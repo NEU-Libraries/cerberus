44a433676ebbe1f8ad571d877d481cab6c36a21e
wjackson <wjackson64@gmail.com>
2013-08-01 14:22:20 -0400

    Adds the roughest beginnings of a collection model and adds a single field to northeastern_dublin_core_datastream.rb

---
+++ app/models/collection.rb
@@ -0,0 +1,7 @@
+class Collection < ActiveFedora::Base
+  include Hydra::ModelMixins::RightsMetadata  
+
+  has_metadata name: 'oaidc', type: NortheasternDublinCoreDatastream 
+  has_metadata name: 'rightsMetadata', type: ParanoidRightsDatastream
+  # has_metadata name: 'MODS', type: NeuModsDatastream <--Need to implement this datastream type
+end
