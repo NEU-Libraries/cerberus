cf383016084b50fd98f432a06fcc15181eb7bcfa
wjackson <wjackson64@gmail.com>
2013-08-02 13:50:04 -0400

    More test work.

--- spec/features/single_file_upload_spec.rb
+++ spec/features/single_file_upload_spec.rb
@@ -4,4 +4,22 @@ feature "Uploading a single file" do
   let(:user) { FactoryGirl.create(:user) }
 
   before { sign_in user }
+  before { visit '/' } 
+
+  it "Upload page is accessible from home page" do 
+    page.should have_link("contribute_link")  
+  end
+
+  describe "Navigating to file upload" do 
+    before :all do
+      visit '/'
+      page.click_link('contribute_link') 
+    end 
+
+    it "Has a properly formed upload screen" do
+      expect(page).to have_content "Upload"  
+    end
+  end
+
+  #TODO: Finish post page refactor
 end 
--- spec/models/northeastern_dublin_core_datastream_spec.rb
+++ spec/models/northeastern_dublin_core_datastream_spec.rb
@@ -1,2 +1,43 @@
 require 'spec_helper' 
 
+describe NortheasternDublinCoreDatastream do 
+
+  before do 
+    @ndbc = NortheasternDublinCoreDatastream.new
+  end
+
+  subject { @ndbc } 
+
+  it { should respond_to(:to_xml) } 
+  it { should respond_to(:nu_title) } 
+  it { should respond_to(:nu_description) } 
+  it { should respond_to(:nu_type) } 
+  it { should respond_to(:nu_identifier) } 
+
+  describe "Setting fields" do 
+
+    before do 
+      @ndbc.nu_title = "My title" 
+      @ndbc.nu_description = "My description" 
+      @ndbc.nu_type = "My type" 
+      @ndbc.nu_identifer = "My identifier" 
+    end
+
+    it "Has the assigned nu_description" do 
+      @ndbc.nu_title.should == "My title" 
+    end
+
+    it "Has the assigned description" do 
+      @ndbc.nu_description.should == "My description" 
+    end
+
+    it "Has the assigned nu_type" do 
+      @ndbc.nu_type.should == "My type" 
+    end
+
+    it "Has the assigned nu_identifier" do 
+      @ndbc.nu_identifier.should == "My identifier" 
+    end
+  end
+end
+
