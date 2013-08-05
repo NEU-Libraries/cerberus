295b6e4c0f9a44636ed63840e0563c2354116ef3
wjackson <wjackson64@gmail.com>
2013-08-05 14:56:41 -0400

    Added an abstract entry field to the catalog form.  Also added relationships to the model.

--- app/models/nu_collection.rb
+++ app/models/nu_collection.rb
@@ -8,6 +8,11 @@ class NuCollection < ActiveFedora::Base
   has_metadata name: 'mods', type: NuModsCollectionDatastream
   has_metadata name: 'crud', type: CrudDatastream
 
-  delegate_to :oaidc, [:nu_title, :nu_description] 
+  delegate_to :oaidc, [:nu_title, :nu_description, :nu_identifier]
+  delegate_to :mods, [:mods_title, :mods_abstract, :mods_identifier] 
+
+  has_many :generic_files, property: :is_part_of 
+  has_many :nu_collections, property: :is_part_of 
+  # belongs_to :nu_collections, property: #What is?
   
 end
