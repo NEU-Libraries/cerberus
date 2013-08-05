bec7b83a143a7f5b7b3ca1d46b6b257a47226b68
David Cliff <d.cliff@neu.edu>
2013-08-05 18:10:47 +0000

    Able to create basic (and not very useful) fedora collection object

--- app/controllers/nu_collections_controller.rb
+++ app/controllers/nu_collections_controller.rb
@@ -1,7 +1,18 @@
 class NuCollectionsController < ApplicationController
+  def index
+  end
+
   def new
+    @nu_collection = NuCollection.new
   end
 
   def create
+    #render text: params[:nu_collection].inspect
+    @nu_collection = NuCollection.new
+    @nu_collection.nu_title = params[:nu_collection][:nu_title]
+    @nu_collection.nu_description = params[:nu_collection][:nu_description]
+    @nu_collection.save!
+    redirect_to(@nu_collection, :notice => 'Collection was successfully created.')
   end
+
 end
--- app/models/nu_collection.rb
+++ app/models/nu_collection.rb
@@ -1,7 +1,12 @@
 class NuCollection < ActiveFedora::Base
   include Hydra::ModelMixins::RightsMetadata  
 
+  attr_accessor :nu_title, :nu_description
+
   has_metadata name: 'oaidc', type: NortheasternDublinCoreDatastream 
   has_metadata name: 'rightsMetadata', type: ParanoidRightsDatastream
   # has_metadata name: 'MODS', type: NeuModsDatastream <--Need to implement this datastream type
+  has_metadata name: 'crud', type: CrudDatastream
+
+  delegate_to :oaidc, [:nu_title, :nu_description] 
 end
