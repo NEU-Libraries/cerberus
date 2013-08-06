648b07560e91261d85862b1b871af4e467015a5b
David Cliff <d.cliff@neu.edu>
2013-08-06 14:54:20 +0000

    Simple pre-save to fix PID issue

--- app/controllers/nu_collections_controller.rb
+++ app/controllers/nu_collections_controller.rb
@@ -2,15 +2,24 @@ class NuCollectionsController < ApplicationController
   def index
   end
 
-  def new
-    @nu_collection = NuCollection.new
+  def new    
   end
 
   def create
     #render text: params[:nu_collection].inspect
     @nu_collection = NuCollection.new
+
+    #to get a correct pid
+    @nu_collection.save!
+
     @nu_collection.nu_title = params[:nu_collection][:nu_title]
     @nu_collection.nu_description = params[:nu_collection][:nu_description]
+    @nu_collection.nu_identifier = @nu_collection.id
+
+    @nu_collection.mods_abstract = params[:nu_collection][:mods_abstract]
+    @nu_collection.mods_title = params[:nu_collection][:nu_title]
+    @nu_collection.mods_identifier = @nu_collection.id
+
     @nu_collection.save!
     redirect_to(@nu_collection, :notice => 'Collection was successfully created.')
   end
--- app/models/nu_collection.rb
+++ app/models/nu_collection.rb
@@ -1,14 +1,16 @@
 class NuCollection < ActiveFedora::Base
+  include Hydra::ModelMethods
+  include Hydra::ModelMixins::CommonMetadata  
   include Hydra::ModelMixins::RightsMetadata  
 
   attr_accessor :nu_title, :nu_description
 
-  has_metadata name: 'oaidc', type: NortheasternDublinCoreDatastream 
+  has_metadata name: 'DC', type: NortheasternDublinCoreDatastream 
   has_metadata name: 'rightsMetadata', type: ParanoidRightsDatastream
   has_metadata name: 'mods', type: NuModsCollectionDatastream
   has_metadata name: 'crud', type: CrudDatastream
 
-  delegate_to :oaidc, [:nu_title, :nu_description, :nu_identifier]
+  delegate_to :DC, [:nu_title, :nu_description, :nu_identifier]
   delegate_to :mods, [:mods_title, :mods_abstract, :mods_identifier] 
 
   has_many :generic_files, property: :is_part_of 
