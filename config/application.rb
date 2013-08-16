df19b92f2f8de4bb4779390dc76afe3e8ee487f0
Steven Bassett <steven.j.bassett@gmail.com>
2013-08-16 13:32:28 -0400

    Working on the homepage tempates and SCSS some more

--- config/application.rb
+++ config/application.rb
@@ -33,11 +33,11 @@ module DrsSufiaApp
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+     config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -69,8 +69,8 @@ module DrsSufiaApp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # Namespace is now neu 
-    config.id_namespace = 'neu' 
+    # Namespace is now drs 
+    config.id_namespace = 'drs' 
 
     #Add polyfills in the pipline so that Modernizr can pull in js polyfills
     
