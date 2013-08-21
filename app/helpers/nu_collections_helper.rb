42f8788758a2102dc180fd3b8cd9aaa3183f7472
wjackson <wjackson64@gmail.com>
2013-08-21 11:44:11 -0400

    Moving logic for filtering out the depositor/registered group/public group from view partial to helper in NuCollectionsHelper.  This is cleaner and eliminates a few potential bugs.

--- app/helpers/nu_collections_helper.rb
+++ app/helpers/nu_collections_helper.rb
@@ -1,2 +1,14 @@
 module NuCollectionsHelper
+
+  # Removes the following from the permissions list
+  # - The depositing user, who cannot have their edit privileges revoked through the frontend 
+  # - The group level permission for 'public' and 'registered' groups.  
+  def filtered_permissions(collection)
+    perms = collection.permissions
+    depositor = collection.depositor
+
+    perms.select! { |coll| coll[:name] != depositor && coll[:name] != 'public' && coll[:name] != 'registered' } 
+
+    return perms 
+  end
 end
