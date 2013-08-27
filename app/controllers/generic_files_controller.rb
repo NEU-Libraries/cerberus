aa38b45424f34d68e84b16eb92aa53e88b22b26d
David Cliff <d.cliff@neu.edu>
2013-08-27 18:33:02 +0000

    Updated redirect and routes for post

--- app/controllers/generic_files_controller.rb
+++ app/controllers/generic_files_controller.rb
@@ -25,8 +25,9 @@ class GenericFilesController < ApplicationController
   end
 
   def process_metadata
-    #TODO
-    puts "warble blarble"
+    Sufia.queue.push(BatchUpdateJob.new(current_user.user_key, params))
+    flash[:notice] = 'Your files are being processed by ' + t('sufia.product_name') + ' in the background. The metadata and access controls you specified are being applied. Files will be marked <span class="label label-important" title="Private">Private</span> until this process is complete (shouldn\'t take too long, hang in there!). You may need to refresh your dashboard to see these updates.'
+    redirect_to sufia.dashboard_index_path    
   end
 
   # routed to /files/new
