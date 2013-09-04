6661a75887ef80be85e8a29749721adf6797e2f5
wjackson <wjackson64@gmail.com>
2013-09-04 10:49:50 -0400

    Modifies the zip job to install to a saner directory, and clean away that directory if it exists before writing a new .zip file.

--- app/jobs/zip_compilation_job.rb
+++ app/jobs/zip_compilation_job.rb
@@ -2,13 +2,14 @@ class ZipCompilationJob
   include Hydra::PermissionsQuery
   include Rails.application.routes.url_helpers
 
-  attr_accessor :title, :comp_pid, :entry_ids
+  attr_accessor :title, :comp_pid, :entry_ids, :nuid
 
   def queue_name 
     :zip_compilation 
   end
 
-  def initialize(compilation)
+  def initialize(user, compilation)
+    self.nuid = user.nuid 
     self.title = compilation.title  
     self.comp_pid = compilation.pid 
     self.entry_ids = compilation.entry_ids 
@@ -21,6 +22,13 @@ class ZipCompilationJob
   def run
     puts "Job executing take two"
     puts self.entry_ids
+
+    # Removes any stale zip files that might still be sitting around. 
+    if File.directory?("#{Rails.root}/tmp/#{self.comp_pid}") 
+      FileUtils.remove_dir ("#{Rails.root}/tmp/#{self.comp_pid}")
+    end 
+
+    FileUtils.mkdir_p ("#{Rails.root}/tmp/#{self.comp_pid}") 
 
     zipfile_name = safe_zipfile_name
 
@@ -50,6 +58,6 @@ class ZipCompilationJob
       safe_title = self.title.gsub(/\s+/, "")
       timestamp = DateTime.now.strftime("%Y-%m-%d-%M-%s")
 
-      return "#{Rails.root}/tmp/#{safe_title}_archived_#{timestamp}.zip" 
+      return "#{Rails.root}/tmp/#{self.comp_pid}/#{safe_title}_archived_#{timestamp}.zip" 
     end
 end
