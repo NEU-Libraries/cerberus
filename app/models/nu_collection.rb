a309ff4b40ad8cc696b41f3bd5b3918c3a675f0c
David Cliff <d.cliff@neu.edu>
2013-09-06 16:48:13 +0000

    Abstracted mods helper implementaiton to a module so its DRY for collection and core file

--- app/models/nu_collection.rb
+++ app/models/nu_collection.rb
@@ -3,6 +3,7 @@ class NuCollection < ActiveFedora::Base
   include Hydra::ModelMixins::CommonMetadata  
   include Hydra::ModelMixins::RightsMetadata
   include ActiveModel::MassAssignmentSecurity
+  include NuModelHelper
   include ModsSetterHelpers
 
   attr_accessible :title, :description, :date_of_issue, :keywords, :parent, :mass_permissions 
@@ -51,68 +52,6 @@ class NuCollection < ActiveFedora::Base
     else
       nil
     end
-  end
-
-  def title=(string)
-    self.mods_title = string
-    self.nu_title = string 
-  end
-
-  def title
-    self.mods_title 
-  end
-
-  def identifier=(string)
-    self.nu_identifier = string 
-    self.mods_identifier = string 
-  end
-
-  def identifier
-    self.mods_identifier 
-  end
-
-  def description=(string)
-    self.nu_description = string 
-    self.mods_abstract = string 
-  end
-
-  def description 
-    self.mods_abstract 
-  end
-
-  def date_of_issue=(string) 
-    self.mods_date_issued = string 
-  end
-
-  def date_of_issue
-    self.mods_date_issued 
-  end
-
-  def keywords=(array_of_strings) 
-    self.mods_keyword = array_of_strings 
-  end
-
-  def keywords 
-    self.mods_keyword 
-  end
-
-  def corporate_creators=(array_of_strings) 
-    self.mods_corporate_creators = array_of_strings
-  end
-
-  def corporate_creators
-    self.mods_corporate_creators 
-  end
-
-  def personal_creators=(hash)
-    first_names = hash['creator_first_names'] 
-    last_names = hash['creator_last_names']  
-
-    self.set_mods_personal_creators(first_names, last_names) 
-  end
-
-  def personal_creators 
-    self.mods_personal_creators 
   end
 
   def embargo_release_date=(string) 
