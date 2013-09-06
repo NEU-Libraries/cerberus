d941552cc15bdcfcbe4223227a99ce7769674e7d
David Cliff <d.cliff@neu.edu>
2013-09-06 15:24:49 +0000

    Changed belongs_to to the singular, and added helper methods from compilation

--- lib/nu_file.rb
+++ lib/nu_file.rb
@@ -7,8 +7,32 @@ module NuFile
   	has_metadata name: 'rightsMetadata', type: ParanoidRightsDatastream
     has_file_datastream :name => "content", :type => FileContentDatastream
     
-    belongs_to :nu_core_files, :property => :is_part_of, :class_name => 'NuCoreFile'
+    belongs_to :nu_core_file, :property => :is_part_of, :class_name => 'NuCoreFile'
 
     delegate_to :DC, [:nu_title, :nu_description, :nu_identifier]
   end
+
+  def title=(value)
+    self.DC.nu_title = value 
+  end
+
+  def title 
+    self.DC.nu_title.first 
+  end
+
+  def description=(value)
+    self.DC.nu_description = value 
+  end
+
+  def description 
+    self.DC.nu_description.first 
+  end
+
+  def identifier=(value) 
+    self.DC.nu_identifier = value 
+  end
+
+  def identifier
+    self.DC.nu_identifier.first 
+  end  
 end
