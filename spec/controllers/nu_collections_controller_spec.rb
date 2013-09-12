63aa4ec48aa701a730abdd600ad994e9aea395f0
wjackson <wjackson64@gmail.com>
2013-09-12 10:51:52 -0400

    Adding basic GET tests to the NuCollectionsController spec.

--- spec/controllers/nu_collections_controller_spec.rb
+++ spec/controllers/nu_collections_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe NuCollectionsController do
+  let(:bill) { FactoryGirl.create(:bill) } 
+  let(:root) { FactoryGirl.create(:root_collection) } 
 
+  describe "GET #new" do 
+    it "redirects to the index page if no parent is set" do 
+      sign_in bill 
+
+      get :new 
+
+      expect(response).to redirect_to(nu_collections_path) 
+    end
+
+    it "renders the new page when a parent is set" do 
+      sign_in bill 
+
+      get :new, { parent: root.identifier } 
+
+      expect(response).to render_template('nu_collections/new') 
+    end
+
+    it "requests signin from unauthenticated users" do 
+
+      get :new, { parent: root.identifier } 
+
+      expect(response).to redirect_to(new_user_session_path) 
+    end
+  end
 end
