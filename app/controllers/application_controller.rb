add17c265e1dd928313262c7c8310e76d2385081
wjackson <wjackson64@gmail.com>
2013-09-13 13:07:04 -0400

    Fixes deprecation warning that render_403 was throwing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,8 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
-  def render_403
-    respond_to do |format| 
-      format.html { render :file => "#{Rails.root}/public/403.html", layout: false, status: '403' } 
-    end
+  def render_403 
+    render :file => "#{Rails.root}/public/403", formats: [:html], layout: false, status: '403' 
   end
 
   def mint_unique_pid 
