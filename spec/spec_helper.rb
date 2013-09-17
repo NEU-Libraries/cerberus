d95692b66cea6a8db15d89bdbe2553eefc88a8f2
wjackson <wjackson64@gmail.com>
2013-09-17 12:20:08 -0400

    adds .each to the cleanup methods.  Woops.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,7 +43,7 @@ RSpec.configure do |config|
   # Cleans everything.  Should figure out how to get Fedora to do this 
   # automatically. 
   config.before(:suite) do 
-    ActiveFedora::Base.find(:all) do |file| 
+    ActiveFedora::Base.find(:all).each do |file| 
       file.destroy 
     end
   end
@@ -51,7 +51,7 @@ RSpec.configure do |config|
   # Clean everything.  Should figure out how to get Fedora to do this
   # automatically. 
   config.after(:suite) do 
-    ActiveFedora::Base.find(:all) do |file| 
+    ActiveFedora::Base.find(:all).each do |file| 
       file.destroy 
     end
   end
