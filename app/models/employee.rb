90310e3cedd6b6eb83ace10e7ef161a611d9b35f
David Cliff <d.cliff@neu.edu>
2013-09-17 14:00:06 +0000

    Should be class name Department not collection

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,7 +6,7 @@ class Employee < ActiveFedora::Base
 
   has_metadata name: 'details', type: DrsEmployeeDatastream
 
-  belongs_to :parent, :property => :is_member_of, :class_name => 'NuCollection'
+  belongs_to :parent, :property => :is_member_of, :class_name => 'Department'
 
   def name=(string)
     self.details.name = string
