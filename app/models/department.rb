821d157989817d3101254091d99334ca9cf44878
David Cliff <d.cliff@neu.edu>
2013-09-19 16:25:41 +0000

    Replacing the symbol with new drs one

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,7 +2,7 @@ class Department < NuCollection
 
   before_create :tag_as_NuCollection
 
-  has_many :employees, property: :is_member_of, class_name: "Employee"
+  has_many :employees, property: :has_affiliation, class_name: "Employee"
 
   def tag_as_NuCollection
     self.RELS_EXT.add_relationship("hasModel", "info:fedora/afmodel:NuCollection") 
