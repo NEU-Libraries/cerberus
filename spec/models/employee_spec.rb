3b0a46e5a347adda93f61f8b6dd616e6dccf96f1
wjackson <wjackson64@gmail.com>
2013-09-23 11:04:26 -0400

    Breaks test for job logic out into its own spec that doesn't touch Resque and updates the employee model spec to ensure that the uniqueness over NUID validation is being enforced.

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe Employee do
 
+  before :each do 
+    Employee.destroy_all 
+  end
+
   describe "validation" do 
     it "disallows multiple employees with the same nuid" do 
-      a = Employee.create(nuid: "multiples@employeespec.com") 
-      b = Employee.create(nuid: "multiples@employeespec.com") 
+      a = Employee.create(nuid: "multiples@employeespec.com")
 
       Employee.find_by_nuid(a.nuid).pid.should == a.pid 
       Employee.new(nuid: "multiples@employeespec.com").valid?.should be false
+    end
+
+    it "allows updating of employee objects" do 
+      a = Employee.create(nuid: "updated@updated.com") 
+      a.nuid = "actuallyupdated@updated.com" 
+      a.save! 
+
+      a.nuid.should == "actuallyupdated@updated.com"
     end
   end
 
