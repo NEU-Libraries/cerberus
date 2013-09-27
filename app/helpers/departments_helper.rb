515f1fde977b8cc885c56a33ff5ea8224f0ce67f
David Cliff <d.cliff@neu.edu>
2013-09-27 19:17:22 +0000

    Added helper and button, temporarily for dev and showing

---
+++ app/helpers/departments_helper.rb
@@ -0,0 +1,14 @@
+module DepartmentsHelper
+
+  # Render a button for creating a new collection within this collection
+  # if the current user has edit permissions. 
+  def render_create_department_button(parent)
+    if current_user_can_edit?(parent) 
+      if request.original_fullpath == departments_path
+        link_to("Create a child department off this node", new_department_path(:parent => Rails.configuration.root_department_id))
+      else 
+        link_to("Create a child department off this node", new_department_path(parent: parent.identifier))
+      end  
+    end
+  end
+end
