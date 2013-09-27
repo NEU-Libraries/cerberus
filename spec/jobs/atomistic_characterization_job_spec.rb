0334975e444e96228c2f0dbfbaa868f9da684c8d
wjackson <wjackson64@gmail.com>
2013-09-27 13:57:57 -0400

    Refactor characterization job test into something more manageable.

--- spec/jobs/atomistic_characterization_job_spec.rb
+++ spec/jobs/atomistic_characterization_job_spec.rb
@@ -1,35 +1,42 @@
 require 'spec_helper'
 
 describe AtomisticCharacterizationJob do
-  let(:image) { FactoryGirl.create(:image_master_file) } 
 
-  describe "Images" do
+  describe "on images" do 
 
-    # Note that this does nothing for verifying the correctness
-    # of the thumbnail in any detail.  
-    it "builds an ImageThumbnailFile object when run" do
-      a = AtomisticCharacterizationJob.new(image.pid) 
-      a.run
-
-      image.core_record.content_objects.length.should == 2
-
-      b = image.core_record.content_objects
-          .find { |e| e.instance_of? ImageThumbnailFile } 
-
-      puts image.title
-      puts image.keywords
-      puts image.description
-      
-      # Check that this has done something. 
-      b.should_not be nil 
-      b.content.content.should_not be nil
-      b.characterization.should_not be nil   
-      b.depositor.should == image.depositor
-      b.keywords.should =~ image.keywords 
-      b.title.should == 'test_pic_thumb.jpeg'
-      b.description.should == "Thumb nail for #{image.pid}"
-      b.permissions.should == image.permissions
-      puts b.DC.content
+    before :all do
+      @image = FactoryGirl.create(:image_master_file)
+      @core = @image.core_record 
+      a = AtomisticCharacterizationJob.new(@image.pid).run
+      @thumb = @image.core_record.content_objects.find { |e| e.instance_of? ImageThumbnailFile } 
     end
-  end
+
+    after :all do 
+      ActiveFedora::Base.destroy_all 
+    end
+
+    it "builds one and only one thumbnail" do 
+      thumbs = @core.content_objects.count { |o| o.instance_of? ImageThumbnailFile }
+
+      thumbs.should be 1
+    end
+
+    it "assigns keywords correctly" do
+      @thumb.keywords.should =~ @image.keywords 
+    end 
+
+    it "assigns description correctly" do 
+      @thumb.description.should == "Thumbnail for #{@image.pid}" 
+    end
+
+    it "mirrors the permissions of the creating object" do 
+      @thumb.permissions.should == @image.permissions 
+    end
+
+    it "assigns content to the content datastream of the thumb" do 
+      @thumb.content.content.should_not be nil 
+    end
+    
+    pending "write custom matcher for thumby titles"
+  end 
 end
