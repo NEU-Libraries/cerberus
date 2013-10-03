1d002153d41357595731869b2d5dd3b9f76bbf7a
wjackson <wjackson64@gmail.com>
2013-10-03 12:15:31 -0400

    Eliminate extraneous 'return' statements to better comply with ruby style guides.

--- lib/drs/employee/faculty_folders.rb
+++ lib/drs/employee/faculty_folders.rb
@@ -6,55 +6,55 @@ module Drs::Employee::FacultyFolders
   end
 
   def root_folder 
-    return find_by_folder_type('user root')  
+    find_by_folder_type('user root')  
   end
 
   def research_publications 
-    return find_by_folder_type('research publications')  
+    find_by_folder_type('research publications')  
   end
 
   def all_research_publications 
-    return research_publications.all_descendent_files 
+    research_publications.all_descendent_files 
   end
 
   def other_publications 
-    return find_by_folder_type('other publications')
+    find_by_folder_type('other publications')
   end
 
   def all_other_publications 
-    return other_publications.all_descendent_files
+    other_publications.all_descendent_files
   end
 
   def data_sets 
-    return find_by_folder_type('data sets') 
+    find_by_folder_type('data sets') 
   end
 
   def all_data_sets 
-    return data_sets.all_descendent_files
+    data_sets.all_descendent_files
   end
 
   def presentations 
-    return find_by_folder_type('presentations') 
+    find_by_folder_type('presentations') 
   end
 
   def all_presentations 
-    return presentations.all_descendent_files
+    presentations.all_descendent_files
   end
 
   def learning_objects
-    return find_by_folder_type('learning objects') 
+    find_by_folder_type('learning objects') 
   end
 
   def all_learning_objects 
-    return learning_objects.all_descendent_files
+    learning_objects.all_descendent_files
   end
 
   def sorted_folders 
-    return [research_publications, other_publications, data_sets, presentations, learning_objects] 
+    [research_publications, other_publications, data_sets, presentations, learning_objects] 
   end
 
   def personal_folders 
-    return self.folders.select { |f| f.personal_folder_type == 'miscellany' && f.parent.pid == self.root_folder.pid } 
+    self.folders.select { |f| f.personal_folder_type == 'miscellany' && f.parent.pid == self.root_folder.pid } 
   end
 
   private
