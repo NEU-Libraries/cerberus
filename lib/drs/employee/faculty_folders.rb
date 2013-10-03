c3252802172909e2deae66820ead4bee5aa1abd3
wjackson <wjackson64@gmail.com>
2013-10-03 12:11:40 -0400

    Adds query methods that return all files off a certain pregenerated root.

--- lib/drs/employee/faculty_folders.rb
+++ lib/drs/employee/faculty_folders.rb
@@ -13,20 +13,40 @@ module Drs::Employee::FacultyFolders
     return find_by_folder_type('research publications')  
   end
 
+  def all_research_publications 
+    return research_publications.all_descendent_files 
+  end
+
   def other_publications 
     return find_by_folder_type('other publications')
+  end
+
+  def all_other_publications 
+    return other_publications.all_descendent_files
   end
 
   def data_sets 
     return find_by_folder_type('data sets') 
   end
 
+  def all_data_sets 
+    return data_sets.all_descendent_files
+  end
+
   def presentations 
     return find_by_folder_type('presentations') 
   end
 
+  def all_presentations 
+    return presentations.all_descendent_files
+  end
+
   def learning_objects
     return find_by_folder_type('learning objects') 
+  end
+
+  def all_learning_objects 
+    return learning_objects.all_descendent_files
   end
 
   def sorted_folders 
