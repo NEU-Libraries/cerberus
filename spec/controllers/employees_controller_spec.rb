2891f61d04a10778fe3cc6a309540a7dbc01638a
wjackson <wjackson64@gmail.com>
2013-10-07 09:35:15 -0400

    Updates the employees controller spec to reference the personal_graph action that the tests ought to be executing now.

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe EmployeesController do  
 
-  describe "GET #show" do 
+  describe "GET #personal_graph" do 
 
     it "doesn't allow unauthenticated access" do 
-      get :show 
+      get :personal_graph 
 
       expect(response).to redirect_to(new_user_session_path) 
     end
 
-    it "Renders the show page for authenticated users" do 
+    it "Renders the users personal folders for this authenticated user." do 
       user = FactoryGirl.create(:user)
       employee = Employee.create(nuid: user.nuid)  
 
       sign_in user 
 
-      get :show 
+      get :personal_graph
 
       assigns(:employee).pid.should == employee.pid 
-      expect(response).to render_template('employees/show')
+      expect(response).to render_template('employees/personal_graph')
     end
   end
 end
