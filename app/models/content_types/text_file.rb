4c534837c17a93233bd1c3c3f51c0f4e7743c3a5
wjackson <wjackson64@gmail.com>
2013-10-07 14:49:28 -0400

    Breaks out the nontrivial amount of code required to get file type working into its own module.  Deletes the two XML content file types and adds Zip/Text File in their place.

---
+++ app/models/content_types/text_file.rb
@@ -0,0 +1,3 @@
+class TextFile < ActiveFedora::Base
+  include Drs::NuFile
+end
--- lib/drs/nu_file.rb
+++ lib/drs/nu_file.rb
@@ -7,6 +7,7 @@ module Drs
     include Drs::MetadataAssignment
     include Drs::Rights::MassPermissions
     include Drs::NuFile::Characterizable
+    include Drs::NuFile::MasterCreator
     include Hydra::Derivatives 
 
     included do
@@ -23,29 +24,7 @@ module Drs
     end
 
     def self.create_master_content_object(core_file, file, datastream_id, user)
-      if NuFile.virus_check(file) != 0 
-        raise "#{file.original_filename} cannot be processed" 
-      end
-
-      content_object = NuFile.instantiate_appropriate_content_object(file)
-
-      content_object.add_file(file, datastream_id, file.original_filename) 
-      content_object.core_record =  core_file
-      content_object.title       =  file.original_filename 
-      content_object.identifier  =  content_object.pid
-      content_object.depositor   =  user.nuid
-      content_object.canonize
-
-      begin
-        content_object.save!
-      rescue RSolr::Error::Http => error
-        logger.warn "GenericFilesController::create_and_save_generic_file Caught RSOLR error #{error.inspect}"
-        save_tries+=1
-        # fail for good if the tries is greater than 3
-        raise error if save_tries >=3
-        sleep 0.01
-        retry
-      end
+      Drs::NuFile::MasterCreator.create(core_file, file, datastream_id, user)
     end
 
     def self.virus_check(file)
@@ -58,63 +37,5 @@ module Drs
         0
       end
     end
-
-    private
-
-      def self.instantiate_appropriate_content_object(file)
-
-        file_name = file.original_filename 
-
-        if MIME::Types.of(file_name).length == 0
-          raise "#{file.original_filename} does not appear to have a MIME type."
-        end 
-
-        mime = MIME::Types.of(file_name).first
-        pid = Sufia::Noid.namespaceize(Sufia::IdService.mint)
-
-
-        if mime.raw_media_type == 'image' 
-          return ImageMasterFile.new(pid: pid)
-        elsif mime.raw_sub_type == 'pdf' 
-          return PdfFile.new(pid: pid) 
-        elsif mime.raw_sub_type == 'xml+xslt' 
-          return XmlXsltFile.new(pid: pid)
-        elsif mime.raw_sub_type == 'xml'
-          return XmlEadFile.new(pid: pid)
-        elsif Drs::NuFile.matches_msword?(file_name)
-          return MswordFile.new(pid: pid)
-        elsif Drs::NuFile.matches_msexcel?(file_name) 
-          return MsexcelFile.new(pid: pid)
-        elsif Drs::NuFile.matches_msppt?(file_name) 
-          return MspowerpointFile.new(pid: pid) 
-        else
-          raise "#{file.original_filename} cannot be processed." 
-        end   
-      end
-
-      def self.matches_msexcel?(filename)
-        matches = ['xls', 'xlt', 'xla', 'xlsx', 'xltx', 'xlsm', 'xltm', 'xlam']
-
-        return self.match_extension(filename, matches)
-      end
-
-      def self.matches_msword?(filename) 
-        matches = ['doc', 'dot', 'docx', 'dotx', 'docm', 'dotm'] 
-
-        return self.match_extension(filename, matches)
-      end
-
-      def self.matches_msppt?(filename) 
-        matches = ['ppt', 'pot', 'pps', 'ppa', 'pptx', 'potx', 'ppsx', 'ppam',
-                   'pptm', 'potm', 'ppsm'] 
-
-        return self.match_extension(filename, matches) 
-      end
-
-      def self.match_extension(filename, matches) 
-        extension = filename.split(".").last 
-
-        return matches.include?(extension) 
-      end
   end
 end
