d50bda60c41fb0f9d1a35c1527aa9e41f3ea342c
wjackson <wjackson64@gmail.com>
2013-10-17 14:33:28 -0400

    Redirects on signin should go to the requesting page now, instead of always kicking you back to the home page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,16 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
+  # Allows us to redirect to the current page on signin, instead of always back to root. 
+  def after_sign_in_path_for(resource)
+    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
+
   def render_403 
     render :file => "#{Rails.root}/public/403", formats: [:html], layout: false, status: '403' 
   end
