93eda30a5127ab36122cd0da8c14f2fcb0ed126f
wjackson <wjackson64@gmail.com>
2013-10-18 16:00:07 -0400

    Collection/department creation should be working again.

--- app/helpers/departments_helper.rb
+++ app/helpers/departments_helper.rb
@@ -4,8 +4,8 @@ module DepartmentsHelper
   # if the current user has edit permissions. 
   def render_create_department_button(parent)
     if current_user_can_edit?(parent) 
-      if request.original_fullpath == departments_path
-        link_to("Create a child department off this node", new_department_path(:parent => Rails.configuration.root_department_id))
+      if request.original_fullpath == departments_path # Handle the case where a department is being created off root. 
+        link_to("Create a child department off this node", new_department_path(parent: Rails.configuration.root_department_id))
       else 
         link_to("Create a child department off this node", new_department_path(parent: parent.identifier))
       end  
--- app/helpers/nu_collections_helper.rb
+++ app/helpers/nu_collections_helper.rb
@@ -15,20 +15,16 @@ module NuCollectionsHelper
   # Render a button for creating a new collection within this collection
   # if the current user has edit permissions. 
   def render_create_collection_button(parent)
-    if current_user_can_edit?(parent)       
-      if parent.instance_of?(NuCollection)
-        link_to("Create a child collection off this node", new_nu_collection_path(parent: parent.identifier))
-      else
-        link_to("Create a child collection off this node", new_nu_collection_path(department_parent: parent.identifier))
-      end
+    if current_user_can_edit?(parent)
+      link_to("Create a child collection off this node", new_nu_collection_path(parent: parent.identifier))
     end
   end
 
-  # Render a button for uploading files within this collection 
-  # if the current user has edit permissions. 
+  # Render a button for uploading files within this collection
+  # if the current user has edit permissions.
   def render_upload_files_button(parent)
-    if current_user_can_edit?(parent) && !(request.original_fullpath == nu_collections_path) 
-      link_to("Upload files to this collection", new_nu_core_file_path(parent: parent.identifier)) 
+    if current_user_can_edit?(parent) && !(request.original_fullpath == nu_collections_path)
+      link_to("Upload files to this collection", new_nu_core_file_path(parent: parent.identifier))
     end
   end
 end
