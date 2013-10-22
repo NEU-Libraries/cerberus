cf4da4294ca4d52bfee12c94e6fc40e915ef7b72
wjackson <wjackson64@gmail.com>
2013-10-22 16:12:44 -0400

    Fixing up some changes lost in the merge.

--- app/helpers/nu_collections_helper.rb
+++ app/helpers/nu_collections_helper.rb
@@ -19,4 +19,21 @@ module NuCollectionsHelper
       link_to("Upload files to this collection", new_nu_core_file_path(parent: parent.identifier))
     end
   end
+
+  # Render a button for creating a new collection within this collection
+  # if the current user has edit permissions. 
+  def render_create_collection_button(parent)
+    if current_user_can_edit?(parent)
+      link_to("Create a child collection off this node", new_nu_collection_path(parent: parent.identifier))
+    end
+  end
+
+  # Render a button for deleting this object if the user 
+  # has edit permissions over the object being deleted
+  def render_delete_object_button(obj, delete_text) 
+    if current_user_can_edit?(obj) 
+      confirm_copy = "This destroys the object and all of its descendents.  Are you sure?" 
+      link_to(delete_text, obj, method: :delete, confirm: confirm_copy) 
+    end
+  end
 end
