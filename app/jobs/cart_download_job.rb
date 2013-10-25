5bd4c1ab37ea5b94f822d3d3155f47d6437a88ed
wjackson <wjackson64@gmail.com>
2013-10-25 15:11:13 -0400

    Adds a job for zipping shopping carts.

---
+++ app/jobs/cart_download_job.rb
@@ -0,0 +1,35 @@
+class CartDownloadJob 
+
+  attr_accessor :pids, :nuid, :user, :path
+
+  def initialize(pids, nuid) 
+    self.pids = pids 
+    self.nuid = nuid
+  end
+
+  def queue_name 
+    :cart_download 
+  end
+
+  def run
+    self.user = User.find_by_email(nuid)
+    self.path = "#{Rails.root}/tmp/#{nuid}/cart"
+
+    # Clean out any files that already exist in the cart directory 
+    # For this user. 
+    FileUtils.rm_rf(Dir.glob("#{path}/*")) if File.directory?(path) 
+
+    FileUtils.mkdir_p("#{Rails.root}/tmp/#{user.nuid}/cart")
+    full_path = "#{Rails.root}/tmp/#{user.nuid}/cart/cart.zip" 
+
+    Zip::Archive.open(full_path, Zip::CREATE) do |io| 
+      pids.each do |pid| 
+        item = ActiveFedora::Base.find(pid, cast: true)
+
+        if user.can? :read, item 
+          io.add_buffer("downloads/#{item.content.label}", item.content.content) 
+        end
+      end
+    end
+  end
+end
