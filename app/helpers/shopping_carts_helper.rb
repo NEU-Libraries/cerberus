529e4359b8754db02cd9cce3f4a94efc3015cf3b
wjackson <wjackson64@gmail.com>
2013-10-29 10:15:52 -0400

    Eliminate some unnecessary abstraction.

--- app/helpers/shopping_carts_helper.rb
+++ app/helpers/shopping_carts_helper.rb
@@ -1,16 +1,3 @@
 module ShoppingCartsHelper
-  def render_add_to_shopping_cart(pid) 
-    link_to("Add to download", 
-             update_cart_path(add: pid), 
-             :method => 'put', 
-             :remote => true)
-  end
-  
 
-  def render_remove_from_shopping_cart(pid) 
-    link_to("Remove from download",
-             update_cart_path(delete: pid),
-             :method => 'put',
-             :remote => true) 
-  end
 end
