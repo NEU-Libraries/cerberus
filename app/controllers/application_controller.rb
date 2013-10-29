a2f5d574181c2b7d28ad325c369cc2657403fe5b
wjackson <wjackson64@gmail.com>
2013-10-29 15:35:22 -0400

    Fixes infinite redirection on user creation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,9 @@ class ApplicationController < ActionController::Base
   # Allows us to redirect to the current page on signin, instead of always back to root. 
   def after_sign_in_path_for(resource)
     sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-    if request.referer == sign_in_url
+    if params[:controller] = 'sessions' && params[:action] == 'new' || params[:action] == 'create'
+      root_path
+    elsif request.referer == sign_in_url
       super
     else
       stored_location_for(resource) || request.referer || root_path
