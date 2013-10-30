61e4e87cffa4801b3ab1a453153a608fd1bc60a8
David Cliff <d.cliff@neu.edu>
2013-10-30 20:46:11 +0000

    First steps to refactoring community spec with admin in mind

--- spec/controllers/communities_controller_spec.rb
+++ spec/controllers/communities_controller_spec.rb
@@ -1,7 +1,37 @@
 require 'spec_helper'
 
 describe CommunitiesController do
-  let(:bill)             { FactoryGirl.create(:bill) }
-  let(:bo)               { FactoryGirl.create(:bo) } 
-  let(:root_dept)        { FactoryGirl.create(:root_community) }
+  let(:admin)                 { FactoryGirl.create(:admin) }
+  let(:bo)                    { FactoryGirl.create(:bo) } 
+  let(:root_community)        { FactoryGirl.create(:root_community) }
+
+  describe "GET #index" do
+    it "renders the show template for root_community" do
+      sign_in admin
+      get :index
+      response.status.should == 200
+      expect(response).to render_template('shared/sets/show')
+    end  
+  end 
+
+  describe "GET #new" do
+    #
+  end
+
+  describe "POST #create" do 
+    #
+  end
+
+  describe "GET #show" do 
+    #
+  end
+
+  describe "GET #edit" do
+    #
+  end
+
+  describe "PUTS #update" do
+    #
+  end
+
 end
