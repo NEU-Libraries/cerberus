7c53b421d3dc69d1bd2acdeb107403e9803f2d0d
wjackson <wjackson64@gmail.com>
2013-10-30 14:37:58 -0400

    Fixes a bug in the admin controller, adds basic tests.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,14 @@
 class AdminController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :verify_admin
 
   def index 
 
   end
+
+  private 
+
+    def verify_admin 
+      redirect_to root_path unless current_user.try "admin" 
+    end
 end
