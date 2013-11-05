c7f3ce78719b3f3ae0cb46d1726b55b604e13560
David Cliff <d.cliff@neu.edu>
2013-11-05 21:03:52 +0000

    Adding test for descendent collections

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -5,18 +5,29 @@ describe Community do
   describe "Behavior of Parent setter" do 
   end
 
-  describe "Get all child collections" do
+  describe "behavior of decendent getter" do
     let(:root_community) { Community.new }
-    let(:test_collection_a) { NuCollection.new }
-    let(:test_collection_b) { NuCollection.new }
-    let(:test_collection_c) { NuCollection.new }
     
     #all_descendent_collections
 
     before do
       root_community.save! 
-      @root_community = NuCollection.find(root_community.pid) 
+      @root_community = Community.find(root_community.pid) 
     end
+
+    it "responds with all descendent collections" do
+      @test_collection_a = NuCollection.create(parent: @root_community.pid)
+      @test_collection_b = NuCollection.create(parent: @root_community.pid)
+      @test_collection_c = NuCollection.create(parent: @root_community.pid)
+
+      @test_collection_x = NuCollection.create(parent: @test_collection_a.pid)
+      @test_collection_y = NuCollection.create(parent: @test_collection_a.pid)
+      @test_collection_z = NuCollection.create(parent: @test_collection_c.pid)
+
+
+      root_community.all_descendent_collections.count.should == 6
+    end
+
   end
 
 
