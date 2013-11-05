1d64103b7f84588e18d94bc8b28929fe6e5c67bb
David Cliff <d.cliff@neu.edu>
2013-11-05 21:21:18 +0000

    finishing up the recursion testing

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -5,11 +5,9 @@ describe Community do
   describe "Behavior of Parent setter" do 
   end
 
-  describe "behavior of decendent getter" do
+  describe "behavior of recursions" do
     let(:root_community) { Community.new }
-    
-    #all_descendent_collections
-
+        
     before do
       root_community.save! 
       @root_community = Community.find(root_community.pid) 
@@ -28,6 +26,33 @@ describe Community do
       root_community.all_descendent_collections.count.should == 6
     end
 
+    it "responds with all descendent communities" do
+      @test_community_a = Community.create(parent: @root_community.pid)
+      @test_community_b = Community.create(parent: @root_community.pid)
+      @test_community_c = Community.create(parent: @root_community.pid)
+
+      @test_community_x = Community.create(parent: @test_community_a.pid)
+      @test_community_y = Community.create(parent: @test_community_a.pid)
+      @test_community_z = Community.create(parent: @test_community_c.pid)      
+
+      root_community.all_descendent_communities.count.should == 6
+    end
+
+    it "responds with all descendent files" do
+      @test_file_1 = NuCoreFile.create(title: "Core File One", parent: @test_collection_a, depositor: "nobody@nobody.com") 
+      @test_file_2 = NuCoreFile.create(title: "Core File Two", parent: @test_collection_y, depositor: "nobody@nobody.com")
+      @test_file_3 = NuCoreFile.create(title: "Core File Two", parent: @test_collection_z, depositor: "nobody@nobody.com")
+
+      root_community.all_descendent_files == 3
+    end
+
+    it "deletes all collections, communities, and files pertaining to this community" do
+      @root_community.recursive_delete
+
+      Community.find(:all).length.should == 0
+      NuCollection.find(:all).length.should == 0 
+      NuCoreFile.find(:all).length.should == 0      
+    end
   end
 
 
