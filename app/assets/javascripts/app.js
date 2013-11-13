8599fcef10d6c7bc9e052df260ab1c2845cfc658
Steven Bassett <steven.j.bassett@gmail.com>
2013-11-13 16:53:18 +0000

    Moved the apps JS into an object literal sturcture.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,35 @@
-$('select').selectpicker();
+'use strict';
+
+
+var drsApp = {
+ 
+    init: function( settings ) {
+        drsApp.config = {
+            $drsBootstrapSelect: $( "select.bs-select" ),
+            $addToSetLink: $('#addToSet'),
+            breadCrumbMenuContent: $('#addToSetLinks').html(),
+        };
+ 
+        // allow overriding the default config
+        $.extend( drsApp.config, settings );
+ 
+        drsApp.setup();
+    },
+ 
+    setup: function() {
+        drsApp.config.$drsBootstrapSelect.selectpicker();
+        drsApp.breadCrumbMenu();
+    },
+    breadCrumbMenu: function(){
+        drsApp.config.$addToSetLink.popover({
+            html: true,
+            content: drsApp.config.breadCrumbMenuContent,
+        });
+    }
+   
+ 
+};
+ 
+$( document ).ready( drsApp.init({
+    // Config can go here eg: $drsBootstrapSelect: $('select.bs-select'),
+}) );
