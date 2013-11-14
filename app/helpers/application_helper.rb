786f7a3c6216a902db7dbabba6bf6e48e37cec72
bassettsj <steven.j.bassett@gmail.com>
2013-11-14 20:02:43 +0000

    Reworked the layouts for sets a bit, and moved the collection into a well. Added action links to edit or delete the page. Will need to create a modal action to confirm deleting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,17 @@ module ApplicationHelper
       return core_record.canonical.class.to_s
     end
   end
+  
+  # Return a string for NuCollections or Communities, will return the class otherwise.
+  def get_set_class_label(set)
+    if set.instance_of?(Community)
+      return "Community"
+    elsif set.instance_of?(NuCollection)
+      return "Collection"
+    else
+      return set.class
+    end
+  end
 
   # Generates an array of link/li tags that should breadcrumb back to the Root Collection  
   def breadcrumb_to_root(set, breadcrumb = [])    
