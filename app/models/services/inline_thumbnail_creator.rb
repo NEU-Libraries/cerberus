b9d324ff27e3b355585bb55b456393c0e084428a
wjackson <wjackson64@gmail.com>
2013-11-14 10:32:44 -0500

    Inline thumbnail creator now accepts a string as an argument for the file.

--- app/models/services/inline_thumbnail_creator.rb
+++ app/models/services/inline_thumbnail_creator.rb
@@ -8,7 +8,7 @@ class InlineThumbnailCreator
   attr_accessor :set, :file, :dsid
 
   # Takes as arguments a collection object
-  # An HTTP Uploaded File object 
+  # An HTTP Uploaded File object or a full string path to a file. 
   # And the desired datastream ID
   def initialize(set, file, dsid)
     @set = set 
@@ -16,11 +16,41 @@ class InlineThumbnailCreator
     @dsid = dsid 
   end
 
-  #TODO create method 
   def create_thumbnail
-    img = Magick::Image.read(file.tempfile.path).first
+    if file.instance_of? String
+      fname = File.basename(file)
+
+      tmp = Tempfile.new("inline_thumb")
+      tmp.write File.open(file).read 
+
+      puts "fname is #{fname}" 
+      path = tmp.path 
+      puts "tmp path is #{path}"
+    elsif file.instance_of? ActionDispatch::Http::UploadedFile
+      path = file.tempfile.path 
+      fname = file.original_filename 
+    else 
+      raise "Invalid type of #{file.class} passed to create_thumbnail." +
+            "  Must be string or UploadedFile object." 
+    end
+
+    img = Magick::Image.read(path).first
     thumb = img.resize_to_fill(175, 175) 
-    thumb.write file.tempfile.path
-    set.add_file(file, dsid, file.original_filename)
+    thumb.write path
+
+    if file.instance_of? String 
+      data = File.open(tmp).read
+    else
+      data = file 
+    end
+
+    set.add_file(data, dsid, fname)
+
+    tmp.unlink if file.instance_of? String
+  end
+
+  def create_thumbnail_and_save
+    create_thumbnail
+    set.save! 
   end
 end
