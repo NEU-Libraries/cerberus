159392811fae66edc129e1545740452fc670df97
wjackson <wjackson64@gmail.com>
2013-11-14 11:06:25 -0500

    Refactor inline_thumbnail_creator to be less of a mess.

--- app/models/services/inline_thumbnail_creator.rb
+++ app/models/services/inline_thumbnail_creator.rb
@@ -7,7 +7,7 @@ include Magick
 class InlineThumbnailCreator
   attr_accessor :set, :file, :dsid
 
-  # Takes as arguments a collection object
+  # Takes as arguments an ActiveFedora object.
   # An HTTP Uploaded File object or a full string path to a file. 
   # And the desired datastream ID
   def initialize(set, file, dsid)
@@ -16,41 +16,62 @@ class InlineThumbnailCreator
     @dsid = dsid 
   end
 
-  def create_thumbnail
-    if file.instance_of? String
-      fname = File.basename(file)
-
-      tmp = Tempfile.new("inline_thumb")
-      tmp.write File.open(file).read 
-
-      puts "fname is #{fname}" 
-      path = tmp.path 
-      puts "tmp path is #{path}"
-    elsif file.instance_of? ActionDispatch::Http::UploadedFile
-      path = file.tempfile.path 
-      fname = file.original_filename 
-    else 
+  def create_thumbnail 
+    if file.instance_of? String 
+      process_string
+    elsif file.instance_of? ActionDispatch::Http::UploadedFile 
+      process_uploaded_file 
+    else
       raise "Invalid type of #{file.class} passed to create_thumbnail." +
             "  Must be string or UploadedFile object." 
     end
-
-    img = Magick::Image.read(path).first
-    thumb = img.resize_to_fill(175, 175) 
-    thumb.write path
-
-    if file.instance_of? String 
-      data = File.open(tmp).read
-    else
-      data = file 
-    end
-
-    set.add_file(data, dsid, fname)
-
-    tmp.unlink if file.instance_of? String
   end
 
   def create_thumbnail_and_save
     create_thumbnail
     set.save! 
   end
+
+  private 
+
+    # Creates a Tempfile with the content at the specified 
+    # path and modifies it to create a scaled thumbnail. 
+    # Avoids overwriting fixture data, which will usually be 
+    # what string paths are pointing at. 
+    def process_string
+      begin
+        fname = File.basename(file)
+
+        tmp = Tempfile.new("inline_thumb") 
+        tmp.write File.open(file).read 
+        path = tmp.path 
+
+        generate_thumbnail(path)
+
+        thumbnail = File.open(tmp).read 
+
+        set.add_file(thumbnail, dsid, fname)
+      ensure 
+        tmp.unlink
+      end
+    end
+
+    def process_uploaded_file
+      path = file.tempfile.path 
+      fname = file.original_filename 
+
+      generate_thumbnail(path)
+
+      thumbnail = file 
+
+      set.add_file(thumbnail, dsid, fname) 
+    end
+
+    # Modify this if changes in the actual thumbnail dimensions/ppi/whatever
+    # are required in the future. 
+    def generate_thumbnail(path) 
+      img = Magick::Image.read(path).first 
+      thumb = img.resize_to_fill(175, 175) 
+      thumb.write path 
+    end
 end
