99ddf383e6762b9a232cd6ab72843780b70e1d06
wjackson <wjackson64@gmail.com>
2013-11-19 17:10:37 -0500

    Fixes typo in migration.

--- db/migrate/20131119220315_remove_community_add_department.rb
+++ db/migrate/20131119220315_remove_community_add_department.rb
@@ -1,8 +1,8 @@
 class RemoveCommunityAddDepartment < ActiveRecord::Migration
   def up
     User.reset_column_information
-    remove_column(:user, :community) if User.column_names.include?('community')
-    add_column(:user, :department, :string) unless User.column_names.include?('department')
+    remove_column(:users, :community) if User.column_names.include?('community')
+    add_column(:users, :department, :string) unless User.column_names.include?('department')
   end
 
   def down
