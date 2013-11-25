1a5613f88f804bb56063f7d59b7d596e3bb0a6d7
David Cliff <d.cliff@neu.edu>
2013-11-25 22:10:32 +0000

    Fleshing out shib value transfer

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def shibboleth
-    shibInfo = request.env["omniauth.auth"]
-    puts "SHIB INFO"
-    puts shibInfo
+    @user = User.find_for_shib(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Shibboleth") if is_navigational_format?
+    else
+      #Flash message, YOU SHALL NOT PASS
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   # Connects this user object to Blacklights Bookmarks. 
   include Blacklight::User
 
-  after_create :link_to_drs
+  #after_create :link_to_drs
   before_destroy :remove_drs_object
 
   # Include default devise modules. Others available are:
@@ -20,18 +20,35 @@ class User < ActiveRecord::Base
 
   ROLES = %w[admin employee] 
 
+  def self.find_for_shib(auth, signed_in_resource=nil)
+    user = User.where(:nuid => auth.info.nuid).first
+    unless user      
+      user = User.create(name:auth.info.name,                           
+                           nuid:auth.info.nuid,
+                           email:auth.uid,
+                           password:Devise.friendly_token[0,20]
+                           )
+      if auth.info.employee == "staff"
+        user.role = 'employee'
+        user.save!
+        Sufia.queue.push(EmployeeCreateJob.new(self.nuid))              
+      end      
+    end
+    user
+  end
+
   # Method added by Blacklight; Blacklight uses #to_s on your
   # user class to get a user-displayable login/identifier for
   # the account. 
   def to_s
-    email
+    name
   end
 
   # When we get that Shibboleth stuff sorted we can figure out how to get
   # this to actually be a user's nuid.  For now it's just their email address 
-  def nuid 
-    email 
-  end
+  # def nuid 
+  #   email 
+  # end
 
   # Currently using group_list attribute as though it will someday contain the grouper information
   # pulled in from Shibboleth
@@ -41,9 +58,9 @@ class User < ActiveRecord::Base
   end
 
   private
-    def link_to_drs
-      Sufia.queue.push(EmployeeCreateJob.new(self.nuid))
-    end
+    # def link_to_drs
+    #   Sufia.queue.push(EmployeeCreateJob.new(self.nuid))
+    # end
 
     def remove_drs_object
       if Employee.exists_by_nuid?(self.nuid)
