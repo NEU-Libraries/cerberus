84b4ff42715a2c72c0246d985b771fd69a29d990
bassettsj <steven.j.bassett@gmail.com>
2013-11-25 17:50:25 +0000

    fixes issue #177 :grinning:

--- spec/features/edit_collection_spec.rb
+++ spec/features/edit_collection_spec.rb
@@ -13,10 +13,6 @@ feature "Editing collections" do
   end
 
   # Assign reused lookup code to clean up test appearance a bit. 
-  let(:creator_firsts) { page.all('input#nu_collection_creator_first_name') } 
-  let(:creator_lasts) { page.all('input#nu_collection_creator_last_name') } 
-  let(:corporate_creators) { page.all('input#nu_collection_corporate_creators') }
-  let(:keywords) { page.all('input#nu_collection_keywords') }
   let(:perms) { page.all('div.permission') }
   let(:first_perm) { page.all('div.permission').first }    
 
@@ -34,25 +30,7 @@ feature "Editing collections" do
     #Verify data prefills correctly
     find_field('Title *').value.should == 'Bills Collection'
     find_field('Description *').value.should == 'Bills new collection' 
-    find_field('Date of Issuance').value.should == Date.yesterday.to_s 
 
-    # Personal creator names 
-    creator_firsts.at(0).value.should == "David" 
-    creator_firsts.at(1).value.should == "Steven" 
-    creator_firsts.at(2).value.should == "Will" 
-    creator_lasts.at(0).value.should == "Cliff"
-    creator_lasts.at(1).value.should == "Bassett" 
-    creator_lasts.at(2).value.should == "Jackson" 
-
-    # Corporate creator names
-    corporate_creators.at(0).value.should == "Corp One" 
-    corporate_creators.at(1).value.should == "Corp Two" 
-    corporate_creators.at(2).value.should == "Corp Three"
-
-    # Keywords 
-    keywords.at(0).value.should == "kw one"  
-    keywords.at(1).value.should == "kw two" 
-    keywords.at(2).value.should == "kw three" 
 
     find_field('Choose Mass Permissions:').value.should == 'public'   
 
@@ -64,7 +42,6 @@ feature "Editing collections" do
     first_perm.find_field('Enter NUID or group name').value.should == 'billsfriend@example.com' 
     first_perm.all('select').last.value.should == 'read' 
 
-    find_field('Embargo Date:').value.should == Date.yesterday.to_s 
   end
 
   # Objects instantiated in before :all hooks aren't cleaned up by rails transactional behavior.
