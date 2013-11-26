b3bda94849b8a0da3e1cc953f6d1ce953a728f19
wjackson <wjackson64@gmail.com>
2013-11-26 15:29:21 -0500

    Add scale specific multiple thumbnail datastreams to the ImageThumbnailFile object.

--- app/models/content_types/image_thumbnail_file.rb
+++ app/models/content_types/image_thumbnail_file.rb
@@ -1,3 +1,13 @@
 class ImageThumbnailFile < ActiveFedora::Base
   include Drs::NuFile
+
+  # Rather than create 6 'meaningless' entries in the archive for thumbnails 
+  # used exclusively by the front-end, we collapse the various sizes of thumbnail
+  # we might want into a single object.
+  has_file_datastream name: 'thumbnail_2',    type: FileContentDatastream
+  has_file_datastream name: 'thumbnail_2_2x', type: FileContentDatastream
+  has_file_datastream name: 'thumbnail_4',    type: FileContentDatastream
+  has_file_datastream name: 'thumbnail_4_2x', type: FileContentDatastream 
+  has_file_datastream name: 'thumbnail_10',   type: FileContentDatastream 
+  has_file_datastream name: 'thumbnail_10_2x' type: FileContentDatastream
 end
