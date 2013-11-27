b8c98db475f920ea466bf6f36f475998a2831360
David Cliff <d.cliff@neu.edu>
2013-11-27 18:54:47 +0000

    Getting kind of absurd

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,13 +3,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     auth = request.env["omniauth.auth"]
     puts auth
 
-    @user = User.find_for_shib(request.env["omniauth.auth"], current_user)
+    # @user = User.find_for_shib(request.env["omniauth.auth"], current_user)
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Shibboleth") if is_navigational_format?
-    else
-      #Flash message, YOU SHALL NOT PASS
-    end
+    # if @user.persisted?
+    #   sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+    #   set_flash_message(:notice, :success, :kind => "Shibboleth") if is_navigational_format?
+    # else
+    #   #Flash message, YOU SHALL NOT PASS
+    # end
   end
 end
