42167b2dfef8874ff78ace253fe295d11796659a
wjackson <wjackson64@gmail.com>
2013-12-03 16:40:15 -0500

    Adds queries for getting all alert uploads with a given significant content type and a notified status of false.

--- app/models/upload_alert.rb
+++ app/models/upload_alert.rb
@@ -1,3 +1,33 @@
 class UploadAlert < ActiveRecord::Base
-  attr_accessible :depositor_email, :depositor_name, :title, :type
+  after_initialize :not_notified
+  attr_accessible :depositor_email, :depositor_name, :title, :type, :pid, :notified
+
+  def self.withheld_research_publications
+    unknown_content_query('research publications') 
+  end
+
+  def self.withheld_theses
+    unknown_content_query('theses')
+  end
+
+  def self.withheld_datasets
+    unknown_content_query('datasets') 
+  end
+
+  def self.withheld_learning_objects
+    unknown_content_query('learning objects') 
+  end
+
+  def self.withheld_presentations
+    unknown_content_query('presentations') 
+  end
+
+  private 
+    def not_notified 
+      self.notified = false if self.notified.nil?
+    end
+
+    def self.unknown_content_query(content_type)
+      UploadAlert.where('content_type = ? AND notified = ?', content_type, false).find_all
+    end 
 end
