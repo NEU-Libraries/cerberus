975b189af4908ecb49fedb06e385388576478279
David Cliff <d.cliff@neu.edu>
2013-12-04 17:45:41 +0000

    Missing bracket this time...

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -245,7 +245,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 
-  config.omniauth :shibboleth, {:uid_field => 'eppn',
-                                :info_fields => {:name => 'cn', :last_name => 'sn', :nuid => 'neuEduNUID', :employee => 'unscoped-affiliation', :email => 'mail'}
+  config.omniauth :shibboleth, {:uid_field => 'eppn', :info_fields => {:name => 'cn', :last_name => 'sn',
+                                :nuid => 'neuEduNUID', :employee => 'unscoped-affiliation', :email => 'mail'}}
 
 end
