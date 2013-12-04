6fa4d97b05796004298bb04de615974a2d4e17ea
wjackson <wjackson64@gmail.com>
2013-12-04 11:25:13 -0500

    Update configuration info so that we can actually send emails.

--- app/mailers/metadata_mailer.rb
+++ app/mailers/metadata_mailer.rb
@@ -1,9 +1,28 @@
 class MetadataMailer < ActionMailer::Base
-  default from: "wjackson64@gmail.com"
+  include AbstractController::Callbacks
+
+  default from: "drstestmailer@gmail.com"
+ # after_filter :tag_as_notified
 
 
   # Generate the email 
   def daily_alert_email
-
+    @research = UploadAlert.withheld_research_publications
+    @theses = UploadAlert.withheld_theses 
+    @datasets = UploadAlert.withheld_datasets 
+    @learning_objects = UploadAlert.withheld_learning_objects
+    @presentations = UploadAlert.withheld_presentations
+    mail(to: "William Jackson <wjackson64@gmail.com>", subject: 'Welcome to My Awesome Site')
   end
+
+  private 
+
+    # def tag_as_notified 
+    #   all = @research + @theses + @datasets + @learning_objects + @presentations 
+
+    #   all.each do |alert| 
+    #     alert.notified = true 
+    #     alert.save
+    #   end
+    # end
 end
