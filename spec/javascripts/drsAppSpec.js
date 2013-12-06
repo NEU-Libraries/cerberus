6e2703a65df67f494d45f67258122a789ea4df35
bassettsj <steven.j.bassett@gmail.com>
2013-12-06 17:56:34 +0000

    Testing the collapse plugin

--- spec/javascripts/drsAppSpec.js
+++ spec/javascripts/drsAppSpec.js
@@ -1,28 +1,28 @@
-describe('The drsApp global functions using the applications global functions', function() {
+describe('The drsApp should provide global app functions', function() {
   
-  describe('a user interaction that has a span apply a class to the target object on click ', function(){
-
-    it('should apply a class on click and toggle the class of the icon-font with the link' , function() {
+  it('it should attach a click listener to a[data-toggle] with an [href] target to toggle the ellipsis style by adding the `in` event class, it should toggle the icon-font class from expand to collapse'  , function() {
+    
+    
+    var fixtures = loadFixtures('drsApp-ellipsis.html');
+    
+      drsApp.init();
       
-      
-      var fixtures = loadFixtures('drsApp-ellipsis.html');
-      $('#jasmine-fixtures').ready(function(){
-        drsApp.init();
-        console.log('drsApp inited');
-          var icon = $('i');
-          var link = $('#abstract5Toggle');
-          expect(icon).toHaveClass('icon-expand-alt');
-
-          $(link).trigger('click');
+        var icon = $('i');
+        var link = $('#abstract5Toggle');
         
-          expect(icon).toHaveClass('icon-collapse-alt');
-      });
+        expect(icon).toHaveClass('icon-expand-alt');
+        expect(icon).not.toHaveClass('icon-collapse-alt');
+        
+        expect($('#abstract5').find('.ellipsis')).not.toHaveClass('in'); 
+        
+        $(link).trigger('click');
       
-      
-    });
-  
+        expect(icon).toHaveClass('icon-collapse-alt');
+        expect(icon).not.toHaveClass('icon-expand-alt');
+        expect($('#abstract5').find('.ellipsis')).toHaveClass('in'); 
+    
+    
+    
   });
-  
-
-
 });
+  
