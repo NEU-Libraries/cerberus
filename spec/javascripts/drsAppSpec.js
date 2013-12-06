27ded2eb6390dcf147d7c8ec4bc721f0992d1d98
bassettsj <steven.j.bassett@gmail.com>
2013-12-06 17:38:59 +0000

    Texting the event for the ellipsis collapse action

--- spec/javascripts/drsAppSpec.js
+++ spec/javascripts/drsAppSpec.js
@@ -1,6 +1,28 @@
-describe("drsApp", function() {
+describe('The drsApp global functions using the applications global functions', function() {
   
-  it("Should return true when you call its method returnTrue.", function() {
-    expect(drsApp.returnTrue()).toBe(true);
+  describe('a user interaction that has a span apply a class to the target object on click ', function(){
+
+    it('should apply a class on click and toggle the class of the icon-font with the link' , function() {
+      
+      
+      var fixtures = loadFixtures('drsApp-ellipsis.html');
+      $('#jasmine-fixtures').ready(function(){
+        drsApp.init();
+        console.log('drsApp inited');
+          var icon = $('i');
+          var link = $('#abstract5Toggle');
+          expect(icon).toHaveClass('icon-expand-alt');
+
+          $(link).trigger('click');
+        
+          expect(icon).toHaveClass('icon-collapse-alt');
+      });
+      
+      
+    });
+  
   });
+  
+
+
 });
