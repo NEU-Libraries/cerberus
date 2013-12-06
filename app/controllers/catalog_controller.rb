2a22a70c41ce87841b89a8d8f4dda9e8079888fe
wjackson <wjackson64@gmail.com>
2013-12-06 15:10:49 -0500

    Remove desc_metadata dependent facets and replace them with facets that we want, sans the creator one.  Which I have to figure out.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -93,14 +93,18 @@ class CatalogController < ApplicationController
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
-    config.add_facet_field solr_name("desc_metadata__resource_type", :facetable), :label => "Resource Type", :limit => 5
-    config.add_facet_field solr_name("desc_metadata__creator", :facetable), :label => "Creator", :limit => 5
-    config.add_facet_field solr_name("desc_metadata__tag", :facetable), :label => "Keyword", :limit => 5
-    config.add_facet_field solr_name("desc_metadata__subject", :facetable), :label => "Subject", :limit => 5
-    config.add_facet_field solr_name("desc_metadata__language", :facetable), :label => "Language", :limit => 5
-    config.add_facet_field solr_name("desc_metadata__based_near", :facetable), :label => "Location", :limit => 5
-    config.add_facet_field solr_name("desc_metadata__publisher", :facetable), :label => "Publisher", :limit => 5
-    config.add_facet_field solr_name("file_format", :facetable), :label => "File Format", :limit => 5
+    config.add_facet_field solr_name("creation_year", :facetable), label: "Year", limit: 5
+    config.add_facet_field solr_name("drs_department", :symbol), label: "Department", limit: 5
+    config.add_facet_field solr_name("drs_degree", :symbol), label: "Degree Level", limit: 5
+    config.add_facet_field solr_name("subject", :facetable), label: "Subject", limit: 5
+    config.add_facet_field solr_name("type", :facetable), label: "Type", limit: 5
+
+    # Creator (Mods)
+    # Department (Extension) 
+    # Year (mods) (done) 
+    # Degree (Extension) (done) 
+    # Authorized Subjects (Mods) (done) 
+    # Type (DC) (done)
 
     # Have BL send all facet field names to Solr, which has been the default
     # previously. Simply remove these lines if you'd rather use Solr request
