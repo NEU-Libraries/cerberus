0dff51b2a30d088183e7e5b3a4228dfc37e7ee2f
bassettsj <steven.j.bassett@gmail.com>
2013-12-10 18:13:40 +0000

    Trying to add my own controller here

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,25 @@ class UsersController < ApplicationController
   include Sufia::UsersControllerBehavior
 
   def update
+    # Only when view_pref parameter is present
     if params[:view_pref].present?
-      #do stuff
+      view_pref  = params[:view_pref]
+      
+      #make sure it is only one of these strings
+      if view_pref !== "grid" || view_pref !== "list"
+        if user_signed_in?
+          @user.update_attribute(:only_one_field, view_pref)
+        else
+          session[:view_pref] = view_pref
+        end
+        respond_to do | format |
+          format.json
+        end
+
+      else
+        flash[:error] = "Preference wasn't saved, please try again."
+      end
     end
+  
   end
 end
