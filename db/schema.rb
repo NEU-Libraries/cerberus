fcb66efd4502770916cc9f60e87b981871fe0de0
bassettsj <steven.j.bassett@gmail.com>
2013-12-10 17:08:50 +0000

    Adding a new column to the users table to store their prefered view for the app in listing objects.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131203210300) do
+ActiveRecord::Schema.define(:version => 20131210165421) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -197,6 +197,7 @@ ActiveRecord::Schema.define(:version => 20131203210300) do
     t.string   "role"
     t.string   "nuid"
     t.string   "full_name"
+    t.string   "view_pref"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
