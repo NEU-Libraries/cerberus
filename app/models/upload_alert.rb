b7c835b0506c271d0b2b96cdbf45b20d151fa679
wjackson <wjackson64@gmail.com>
2013-12-13 11:38:33 -0500

    Add a 'create_from_core_file' class method to UploadAlert.  Test it.

--- app/models/upload_alert.rb
+++ app/models/upload_alert.rb
@@ -1,6 +1,8 @@
 class UploadAlert < ActiveRecord::Base
   after_initialize :not_notified
-  attr_accessible :depositor_email, :depositor_name, :title, :content_type, :pid, :notified
+
+  attr_accessible :depositor_email, :depositor_name, :title, :content_type
+  attr_accessible :pid, :notified, :change_type
 
   def self.withheld_research_publications
     unknown_content_query('research publications') 
@@ -21,6 +23,24 @@ class UploadAlert < ActiveRecord::Base
   def self.withheld_presentations
     unknown_content_query('presentations') 
   end
+
+  def self.create_from_core_file(core_file, change_type) 
+    if !([:edit, :create].include? change_type)
+      raise %Q(Passed #{change_type.class} #{change_type} to create_from_core_file, 
+               which takes either symbol :edit or :create)
+    end
+
+    u = UploadAlert.new 
+    user = User.find_by_nuid(core_file.depositor) 
+
+    u.depositor_email = user.email 
+    u.depositor_name  = user.full_name 
+    u.title           = core_file.title 
+    u.content_type    = core_file.category.first
+    u.pid             = core_file.pid 
+    u.change_type     = change_type 
+    u.save! ? u : false
+  end 
 
   private 
     def not_notified 
--- spec/models/upload_alert_spec.rb
+++ spec/models/upload_alert_spec.rb
@@ -1,5 +1,68 @@
 require 'spec_helper'
 
 describe UploadAlert do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+
+  describe "creation from core file" do  
+    before :all do 
+      @user = FactoryGirl.create(:bill) 
+      @core = FactoryGirl.create(:bills_complete_file)
+
+      @core.category =  "Theses and Dissertations" 
+      @core.save!
+    end
+
+    after(:all) { @user.destroy ; @core.destroy } 
+
+    def create_alert(change_type) 
+      @alert = UploadAlert.create_from_core_file(@core, change_type) 
+    end
+
+    context "on core file creation" do 
+      before(:each) { create_alert(:create) }
+
+      it "has a title" do 
+        @alert.title.should == @core.title 
+      end
+
+      it "has a category" do 
+        @alert.depositor_email.should == @user.email 
+      end
+
+      it "has a full name" do 
+        @alert.depositor_name.should == @user.full_name 
+      end
+
+      it "has a pid" do 
+        @alert.pid.should == @core.pid 
+      end
+
+      it "has a content type" do 
+        @alert.content_type.should == @core.category.first
+      end
+
+      it "has a change type" do 
+        @alert.change_type.should == :create 
+      end
+    end
+
+    context "on core file update" do 
+      before(:each) { create_alert(:edit) } 
+      
+      it "has a change type" do 
+        @alert.change_type.should == :edit 
+      end
+    end
+
+    context "with invalid change type" do 
+
+      it "raises an error" do 
+        expect{ UploadAlert.create_from_core_file(@core, 'edit') }.to raise_error 
+      end
+
+      it "raises an error" do 
+        expect{ UploadAlert.create_from_core_file(@core, :edi) }.to raise_error 
+      end
+    end
+  end
 end
