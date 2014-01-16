8bc39d8544e62f67f80880c674bc65a9e5ec78e7
dgcliff <d.cliff@neu.edu>
2014-01-16 18:47:56 +0000

    Attempting to fix caching

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -30,10 +30,10 @@ Drs::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Do not serve static assets
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
