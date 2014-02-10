be572e0c1148ee0213c445df0cac3d10a6af4257
dgcliff <d.cliff@neu.edu>
2014-02-10 21:44:10 +0000

    Work in progress...

--- app/controllers/nu_core_files_controller.rb
+++ app/controllers/nu_core_files_controller.rb
@@ -75,6 +75,10 @@ class NuCoreFilesController < ApplicationController
 
   def process_metadata
     Sufia.queue.push(MetadataUpdateJob.new(current_user.user_key, params))
+    @nu_core_file = NuCoreFile.users_in_progress_files(current_user).first
+    update_metadata if params[:nu_core_file]
+    puts "FUNKY DORY"
+    puts params[:nu_core_file]
     flash[:notice] = 'Your files are being processed by ' + t('sufia.product_name') + ' in the background. The metadata and access controls you specified are being applied. Files will be marked <span class="label label-important" title="Private">Private</span> until this process is complete (shouldn\'t take too long, hang in there!). You may need to refresh your dashboard to see these updates.'
     redirect_to nu_collection_path(params[:collection_id])
   end
@@ -217,7 +221,6 @@ class NuCoreFilesController < ApplicationController
     nu_core_file.date_uploaded = Date.today 
     nu_core_file.date_modified = Date.today
     nu_core_file.creator = user.name
-
     
     collection = !collection_id.blank? ? NuCollection.find(collection_id) : nil
     nu_core_file.set_parent(collection, user) if collection
