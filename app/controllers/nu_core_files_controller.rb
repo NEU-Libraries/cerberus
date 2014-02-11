81922965821c150b52ad76a4d1d1f512afb79b5f
dgcliff <d.cliff@neu.edu>
2014-02-11 22:09:55 +0000

    Fix for skip to update

--- app/controllers/nu_core_files_controller.rb
+++ app/controllers/nu_core_files_controller.rb
@@ -54,8 +54,8 @@ class NuCoreFilesController < ApplicationController
 
   def provide_metadata
     # Feeding through an incomplete file if there is one
-    @nu_core_file = NuCoreFile.users_in_progress_files(current_user).first || NuCoreFile.new
-    
+    @nu_core_file = NuCoreFile.new
+    @incomplete_file = NuCoreFile.users_in_progress_files(current_user).first
     # With the move to single file upload, incomplete files (plural) is a misnomer.
     # but worthwhile to keep if we reimplement batch uploads. In the meantime only
     # NuCoreFile.users_in_progress_files(x).first should ever occur (not more than one at a time).
@@ -80,13 +80,9 @@ class NuCoreFilesController < ApplicationController
   end
 
   def process_metadata
-    puts "HERPY DERP"
-    puts params[:collection_id]
     Sufia.queue.push(MetadataUpdateJob.new(current_user.user_key, params))
     @nu_core_file = NuCoreFile.users_in_progress_files(current_user).first
     update_metadata if params[:nu_core_file]
-    puts "FUNKY DORY"
-    puts params[:nu_core_file]
     flash[:notice] = 'Your files are being processed by ' + t('sufia.product_name') + ' in the background. The metadata and access controls you specified are being applied. Files will be marked <span class="label label-important" title="Private">Private</span> until this process is complete (shouldn\'t take too long, hang in there!). You may need to refresh your dashboard to see these updates.'
     redirect_to nu_collection_path(params[:collection_id])
   end
