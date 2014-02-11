26e11eada1ff62755b4947a1a26d3a161b223a8e
dgcliff <d.cliff@neu.edu>
2014-02-11 03:15:26 +0000

    Good progress. Default form almost essentially finished.

---
+++ lib/drs/rights/permissions_assignment_helper.rb
@@ -0,0 +1,20 @@
+module Drs
+  module Rights
+    module PermissionsAssignmentHelper  
+      # Accepts a hash of the following form:
+      # ex. {'permissions1' => {'identity_type' => val, 'identity' => val, 'permission_type' => val }, 'permissions2' => etc. etc. }
+      # Tosses out param sets that are missing an identity.  Which is nice.   
+      def permissions=(params)
+        params.each do |perm_hash| 
+          identity_type = perm_hash[1]['identity_type']
+          identity = perm_hash[1]['identity']
+          permission_type = perm_hash[1]['permission_type'] 
+
+          if identity != 'public' && identity != 'registered' 
+            self.rightsMetadata.permissions({identity_type => identity}, permission_type)
+          end 
+        end
+      end
+    end
+  end
+end  
