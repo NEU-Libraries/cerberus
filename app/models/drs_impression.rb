d2bb8b8e2ec1ec94ca53edd9c24ba7e115d9efd1
wjackson <wjackson64@gmail.com>
2014-02-12 11:35:28 -0500

    Update model to scope on new attribute action, update impression count to discern between action types.

--- app/models/drs_impression.rb
+++ app/models/drs_impression.rb
@@ -1,4 +1,4 @@
 class DrsImpression < ActiveRecord::Base 
-  attr_accessible :pid, :session_id
-  validates_uniqueness_of :session_id, :scope => :pid
+  attr_accessible :pid, :session_id, :action
+  validates_uniqueness_of :session_id, :scope => [:pid, :action]
 end
