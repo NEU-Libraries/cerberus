6ca297a17a8a3deee2755cfc4f26909b5a2d36f9
wjackson <wjackson64@gmail.com>
2014-02-12 11:44:22 -0500

    Make controller helper account for action type.

--- lib/drs/controller_helpers/view_logger.rb
+++ lib/drs/controller_helpers/view_logger.rb
@@ -2,10 +2,10 @@
 # Uses session hash to determine uniqueness, only writes unique views. 
 
 module Drs::ControllerHelpers::ViewLogger 
-  def view_logger
+  def log_view
     id = params[:id] 
     session = request.session_options[:id] 
 
-    DrsImpression.create(pid: id, session_id: session)
+    DrsImpression.create(pid: id, session_id: session, action: 'view') 
   end
 end
