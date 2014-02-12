0f9003a86115d5b628061560aaba8aeb5fbf603b
wjackson <wjackson64@gmail.com>
2014-02-12 11:35:28 -0500

    Update model to scope on new attribute action, update impression count to discern between action types.

--- lib/drs/impression_count.rb
+++ lib/drs/impression_count.rb
@@ -5,8 +5,13 @@ module Drs::ImpressionCount
     # Return the number of unique* views this object has according to the
     # DrsImpressions table. 
     # * Unique as determined by viewing session_id 
-    def impression_count 
-      DrsImpression.where("pid = ?", self.pid).count
+    def impression_views 
+      DrsImpression.where("pid = ? AND action = ?", self.pid, 'view').count
+    end
+
+    # Same as above, but with recorded download actions
+    def impression_downloads
+      DrsImpression.where("pid = ? AND action = ?", self.pid, 'download').count 
     end
   end
 end
--- spec/lib/drs/impression_count_spec.rb
+++ spec/lib/drs/impression_count_spec.rb
@@ -13,20 +13,24 @@ describe "ImpressionCount" do
   let(:imp) { ImpTest.new("1") }
 
   it "Returns zero impressions when none have been recorded" do 
-    imp.impression_count.should == 0 
+    imp.impression_views.should == 0 
+    imp.impression_downloads.should == 0
   end
 
   it "Returns a single impression when one has been recorded" do 
-    DrsImpression.create(pid: "1", session_id: "abc") 
+    DrsImpression.create(pid: "1", session_id: "abc", action: "view") 
 
-    imp.impression_count.should == 1 
+    imp.impression_views.should == 1 
   end
 
   it "Knows how to distinguish between impressions" do 
-    DrsImpression.create(pid: "1", session_id: "def")
-    DrsImpression.create(pid: "1", session_id: "ghi") 
-    DrsImpression.create(pid: "2", session_id: "def") 
+    DrsImpression.create(pid: "1", session_id: "def", action: "view")
+    DrsImpression.create(pid: "1", session_id: "ghi", action: "view")
+    DrsImpression.create(pid: "1", session_id: "def", action: "view") # This should not be counted
+    DrsImpression.create(pid: "1", session_id: "def", action: "download")  
+    DrsImpression.create(pid: "2", session_id: "def", action: "view")  
 
-    imp.impression_count.should == 2 
+    imp.impression_views.should == 2 
+    imp.impression_downloads.should == 1
   end
 end
