c20c11c043ece8c6fe01010776d117ea2d84edec
wjackson <wjackson64@gmail.com>
2014-02-19 13:19:57 -0500

    Add extra method to View Logger helper.

--- lib/drs/controller_helpers/view_logger.rb
+++ lib/drs/controller_helpers/view_logger.rb
@@ -2,10 +2,21 @@
 # Uses session hash to determine uniqueness, only writes unique views. 
 
 module Drs::ControllerHelpers::ViewLogger 
-  def log_view
-    id = params[:id] 
-    session = request.session_options[:id] 
 
-    DrsImpression.create(pid: id, session_id: session, action: 'view') 
+  def log_view 
+    log_action('view') 
   end
+
+  def log_download 
+    log_action('download') 
+  end
+
+  private 
+
+    def log_action(action)
+      id = params[:id] 
+      session = request.session_options[:id] 
+
+      DrsImpression.create(pid: id, session_id: session, action: action) 
+    end
 end
