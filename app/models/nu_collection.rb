3ece09156de7c4d623cadc7eb8667052e022d3e4
dgcliff <d.cliff@neu.edu>
2014-02-28 15:16:10 +0000

    Woops

--- app/models/nu_collection.rb
+++ app/models/nu_collection.rb
@@ -44,7 +44,7 @@ class NuCollection < ActiveFedora::Base
   # Override parent= so that the string passed by the creation form can be used.
   def parent=(val)
     unique_assign_by_string(val, :is_member_of, [NuCollection, Community], allow_nil: true)
-    self.properties.parent_id = val.pid
+    self.properties.parent_id = val
   end
 
   # Override user_parent= so that the string passed by the creation form can be used.
