46311c5b7468a66bd2e8aaf95748e8e7a97c1280
dgcliff <d.cliff@neu.edu>
2014-02-28 15:38:29 +0000

    First pass at solr q

--- lib/drs/find.rb
+++ lib/drs/find.rb
@@ -15,11 +15,16 @@ module Drs
         return obj
       end
 
-      def self.find_children
+      def self.find_children(filter = "none")
         # Search SOLR for all objects that have this object set as its Parent in the
         # properties datastream.
 
-        # TODO
+        # query = "{!lucene q.op=AND df=#{category_field}}#{category}"
+        query = "{!lucene q.op=AND df=parent_id_tesim}#{self.pid}"
+        (response, document_list) = get_search_results(:q => query)
+        docs = response.docs.map { |x| SolrDocument.new(x) }
+        return docs
+
       end
     end
 
