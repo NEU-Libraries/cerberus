c960e7ac37ef6f58cf1e3f05cae19687c4771450
dgcliff <d.cliff@neu.edu>
2014-03-04 20:22:57 +0000

    Much progress

--- app/controllers/sets_controller.rb
+++ app/controllers/sets_controller.rb
@@ -1,21 +1,21 @@
-class SetsController < ApplicationController 
-  include Drs::ControllerHelpers::EditableObjects 
+class SetsController < ApplicationController
+  include Drs::ControllerHelpers::EditableObjects
 
   helper_method :readable_child_files,
                 :readable_child_collections,
                 :readable_child_communities
 
-  private 
+  private
 
-    def readable_child_files(set) 
-      return set.child_files.keep_if { |f| current_user_can_read? f } 
+    def readable_child_files(set)
+      return set.child_files.keep_if { |f| current_user.can? :read, f }
     end
 
     def readable_child_collections(set)
-      return set.child_collections.keep_if { |c| current_user_can_read? c }
+      return set.child_collections.keep_if { |c| current_user.can? :read, c }
     end
 
     def readable_child_communities(set)
-      return set.child_communities.keep_if { |c| current_user_can_read? c } 
+      return set.child_communities.keep_if { |c| current_user.can? :read, c }
     end
 end
