6971bc15a24378ba2796815b7e2cc5b66ab44886
dgcliff <d.cliff@neu.edu>
2014-03-10 19:06:06 +0000

    Fix for broken url

--- app/jobs/metadata_update_job.rb
+++ app/jobs/metadata_update_job.rb
@@ -1,6 +1,6 @@
 class MetadataUpdateJob
   include Hydra::PermissionsQuery
-  include Rails.application.routes.url_helpers 
+  include Rails.application.routes.url_helpers
 
   def queue_name
     :metadata_update
@@ -10,8 +10,8 @@ class MetadataUpdateJob
 
   def initialize(login, params)
     self.login = login
-    self.title = params[:title] 
-    self.nu_title = params[:title] 
+    self.title = params[:title]
+    self.nu_title = params[:title]
     self.file_attributes = params[:nu_core_file]
     self.visibility = params[:visibility]
   end
@@ -25,14 +25,14 @@ class MetadataUpdateJob
     NuCoreFile.users_in_progress_files(user).each do |gf|
       update_file(gf, user)
     end
-    
+
     job_user = User.batchuser()
-    
+
     message = 'The file(s) '+ file_list(@saved)+ " have been saved." unless @saved.empty?
     job_user.send_message(user, message, 'Metadata upload complete') unless @saved.empty?
- 
+
     message = 'The file(s) '+ file_list(@denied)+" could not be updated.  You do not have sufficient privileges to edit it." unless @denied.empty?
-    job_user.send_message(user, message, 'Metadata upload permission denied') unless @denied.empty?   
+    job_user.send_message(user, message, 'Metadata upload permission denied') unless @denied.empty?
   end
 
   def update_file(gf, user)
@@ -43,7 +43,7 @@ class MetadataUpdateJob
     end
 
     gf.title = title[gf.pid] if title[gf.pid] rescue gf.label
-    gf.nu_title = nu_title[gf.pid] if nu_title[gf.pid] rescue gf.label  
+    gf.nu_title = nu_title[gf.pid] if nu_title[gf.pid] rescue gf.label
     gf.attributes=file_attributes
     gf.set_visibility(visibility)
     gf.tag_as_completed
@@ -51,10 +51,10 @@ class MetadataUpdateJob
 
     begin
       gf.save!
-      # If this core record is being uploaded into a 'best bits' bucket, 
+      # If this core record is being uploaded into a 'best bits' bucket,
       # we want to add an UploadAlert entry for the sake of the daily metadata
       # emailing.  Note that all of the actual significant content type metadata
-      # is applied before this is reached.    
+      # is applied before this is reached.
       if !gf.category.first.blank?
         UploadAlert.create_from_core_file(gf, :create)
       end
@@ -69,9 +69,9 @@ class MetadataUpdateJob
     Sufia.queue.push(ContentUpdateEventJob.new(gf.pid, login))
     @saved << gf
   end
-  
+
   def file_list (files)
-    return files.map {|gf| '<a href="'+nu_core_files_path+'/'+gf.noid+'">'+gf.to_s+'</a>'}.join(', ')   
+    return files.map {|gf| '<a href="'+nu_core_files_path+'/'+gf.pid+'">'+gf.to_s+'</a>'}.join(', ')
   end
-  
+
 end
