f1f5d01f77a125e7266890f552df473adf3a79c4
dgcliff <d.cliff@neu.edu>
2014-03-11 20:24:31 +0000

    Should fix issue

--- lib/drs/nu_file.rb
+++ lib/drs/nu_file.rb
@@ -8,29 +8,33 @@ module Drs
     include Drs::Rights::MassPermissions
     include Drs::NuFile::Characterizable
     include Hydra::Derivatives
-    include Drs::Find 
+    include Drs::Find
     include Drs::ImpressionCount
 
     included do
       attr_accessible :title, :description, :keywords, :identifier
-      attr_accessible :depositor, :date_of_issue, :core_record 
+      attr_accessible :depositor, :date_of_issue, :core_record
       attr_accessible :creators
 
-      has_metadata name: 'DC', type: NortheasternDublinCoreDatastream 
+      has_metadata name: 'DC', type: NortheasternDublinCoreDatastream
       has_metadata name: 'rightsMetadata', type: ParanoidRightsDatastream
       has_metadata name: 'properties', type: DrsPropertiesDatastream
       has_file_datastream name: "content", type: FileContentDatastream
-      
+
       belongs_to :core_record, property: :is_part_of, class_name: 'NuCoreFile'
     end
 
     def self.create_master_content_object(core_file, file, datastream_id, user)
       Drs::NuFile::MasterCreator.create(core_file, file, datastream_id, user)
     end
-    
+
     # Provides a string for labeling the UI
     def type_label
       self.class.name
+    end
+
+    def public?
+      self.rightsMetadata.permissions({group: 'public'}, 'read')
     end
 
     def self.virus_check(file)
