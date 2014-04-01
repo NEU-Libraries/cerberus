e1607519acdc679cd8d1e32277ae005f779db66d
David Cliff <d.cliff@neu.edu>
2014-04-01 17:33:14 -0400

    Able to write to the poster datastream, having mighty troubles making thumbnails from that though...

--- app/models/content_types/video_file.rb
+++ app/models/content_types/video_file.rb
@@ -1,5 +1,6 @@
-class VideoFile < ActiveFedora::Base 
+class VideoFile < ActiveFedora::Base
   include Drs::NuFile
+  has_file_datastream name: 'poster', type: FileContentDatastream
   def type_label
     "Video File"
   end
