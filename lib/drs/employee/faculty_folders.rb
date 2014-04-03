ea67fb275169c980eeeb28ce446981e009108f3c
David Cliff <d.cliff@neu.edu>
2014-04-03 14:38:47 +0000

    Fixes #331

--- lib/drs/employee/faculty_folders.rb
+++ lib/drs/employee/faculty_folders.rb
@@ -1,66 +1,66 @@
 module Drs::Employee::FacultyFolders
   extend ActiveSupport::Concern
 
-  included do 
+  included do
     after_destroy :purge_personal_graph
   end
 
-  def root_folder 
-    find_by_folder_type('user root')  
+  def root_folder
+    find_by_folder_type('user root')
   end
 
-  def research_publications 
-    find_by_folder_type('research publications')  
+  def research_publications
+    find_by_folder_type('research publications')
   end
 
-  def all_research_publications 
-    research_publications.all_descendent_files 
+  def all_research_publications
+    research_publications.all_descendent_files
   end
 
-  def other_publications 
+  def other_publications
     find_by_folder_type('other publications')
   end
 
-  def all_other_publications 
+  def all_other_publications
     other_publications.all_descendent_files
   end
 
-  def data_sets 
-    find_by_folder_type('data sets') 
+  def data_sets
+    find_by_folder_type('data sets')
   end
 
-  def all_data_sets 
+  def all_data_sets
     data_sets.all_descendent_files
   end
 
-  def presentations 
-    find_by_folder_type('presentations') 
+  def presentations
+    find_by_folder_type('presentations')
   end
 
-  def all_presentations 
+  def all_presentations
     presentations.all_descendent_files
   end
 
   def learning_objects
-    find_by_folder_type('learning objects') 
+    find_by_folder_type('learning objects')
   end
 
-  def all_learning_objects 
+  def all_learning_objects
     learning_objects.all_descendent_files
   end
 
-  def sorted_folders 
-    [research_publications, other_publications, data_sets, presentations, learning_objects] 
+  def sorted_folders
+    [research_publications, other_publications, data_sets, presentations, learning_objects]
   end
 
-  def personal_folders 
-    self.folders.select { |f| f.personal_folder_type == 'miscellany' && f.parent.pid == self.root_folder.pid } 
+  def personal_folders
+    self.folders.select { |f| f.personal_folder_type == 'miscellany' && f.parent.pid == self.root_folder.pid }
   end
 
   private
 
-    def find_by_folder_type(string) 
-      return self.folders.find{ |f| f.personal_folder_type == string } 
+    def find_by_folder_type(string)
+      return self.folders.find{ |f| f.personal_folder_type == string && f.parent.pid == self.root_folder.pid }
     end
 
     def purge_personal_graph
