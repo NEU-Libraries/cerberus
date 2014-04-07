8d0d15eba9bf932dfd6875a4a9bbb636acf61639
David Cliff <d.cliff@neu.edu>
2014-04-07 17:55:05 +0000

    Very basic list of non-filtered user's stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
+require 'blacklight/catalog'
+require 'blacklight_advanced_search'
+require 'parslet'
+require 'parsing_nesting/tree'
+
 class UsersController < ApplicationController
+
+  include Blacklight::Catalog
+  include Blacklight::Configurable # comply with BL 3.7
+  include ActionView::Helpers::DateHelper
+  # This is needed as of BL 3.7
+  self.copy_blacklight_config_from(CatalogController)
+
+  include BlacklightAdvancedSearch::ParseBasicQ
+  include BlacklightAdvancedSearch::Controller
+
   extend ActiveSupport::Concern
 
   included do
-    layout "sufia-one-column"
+    # layout "sufia-one-column"
     prepend_before_filter :find_user, :except => [:index, :search, :notifications_number]
     before_filter :authenticate_user!, only: [:edit, :update]
     before_filter :user_is_current_user, only: [:edit, :update]
@@ -21,16 +36,20 @@ class UsersController < ApplicationController
       format.html
       format.json { render json: @users.to_json }
     end
-    
+
   end
 
   # Display user profile
   def show
     @user = User.find_by_nuid(params[:id])
     if @user.respond_to? :profile_events
-      @events = @user.profile_events(100) 
-    else 
+      @events = @user.profile_events(100)
+    else
       @events = []
+    end
+    if user_signed_in? && current_user.nuid == @user.nuid
+      (_, @recent_user_documents) = get_search_results(:q =>filter_mine,
+                                        :sort=>sort_field, :rows=>3)
     end
   end
 
@@ -46,17 +65,17 @@ class UsersController < ApplicationController
 
     if params[:view_pref].present?
       view_pref  = params[:view_pref]
-      
-      #make sure it is only one of these strings
-      if view_pref == 'grid' || view_pref == 'list' 
 
-        if @user 
+      #make sure it is only one of these strings
+      if view_pref == 'grid' || view_pref == 'list'
+
+        if @user
           unless @user.view_pref == view_pref
             @user.view_pref = view_pref
             @user.save!
           end
         end
-        
+
       else
         flash[:error] = "Preference wasn't saved, please try again."
       end
@@ -72,26 +91,39 @@ class UsersController < ApplicationController
 
     @user.populate_attributes if params[:update_directory]
     @user.avatar = nil if params[:delete_avatar]
-    
+
     unless @user.save
       redirect_to sufia.edit_profile_path(@user), alert: @user.errors.full_messages
       return
     end
-    
+
     Sufia.queue.push(UserEditProfileEventJob.new(@user.user_key))
-    
+
     respond_to do |format|
       format.html {  redirect_to sufia.profile_path(@user), notice: "Your profile has been updated" }
       format.js   { render json: @user.to_json }
       format.json { render json: @user.to_json }
     end
 
-   
-  end 
+
+  end
 
   protected
 
-  # You can override base_query to return a list of arguments 
+  def depositor
+    #Hydra.config[:permissions][:owner] maybe it should match this config variable, but it doesn't.
+    Solrizer.solr_name('depositor', :stored_searchable, type: :string)
+  end
+
+  def filter_mine
+    "{!lucene q.op=AND df=#{depositor}}#{current_user.user_key}"
+  end
+
+  def sort_field
+    "#{Solrizer.solr_name('system_create', :sortable)} desc"
+  end
+
+  # You can override base_query to return a list of arguments
   def base_query
     [nil]
   end
