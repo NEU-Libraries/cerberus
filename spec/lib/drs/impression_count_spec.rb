ecce2cba6df61d6edb71e077f7fb3e94b0198ed4
Will Jackson <wjackson64@gmail.com>
2014-04-17 20:01:31 +0000

    Updates the impression count spec to account for the required ip_address field.

--- spec/lib/drs/impression_count_spec.rb
+++ spec/lib/drs/impression_count_spec.rb
@@ -1,36 +1,36 @@
 require 'spec_helper'
 
-describe "ImpressionCount" do 
+describe "ImpressionCount" do
   class ImpTest
     include Drs::ImpressionCount
     attr_accessor :pid
 
-    def initialize(pid) 
-      self.pid = pid 
+    def initialize(pid)
+      self.pid = pid
     end
   end
 
   let(:imp) { ImpTest.new("1") }
 
-  it "Returns zero impressions when none have been recorded" do 
-    imp.impression_views.should == 0 
+  it "Returns zero impressions when none have been recorded" do
+    imp.impression_views.should == 0
     imp.impression_downloads.should == 0
   end
 
-  it "Returns a single impression when one has been recorded" do 
-    DrsImpression.create(pid: "1", session_id: "abc", action: "view") 
+  it "Returns a single impression when one has been recorded" do
+    DrsImpression.create(pid: "1", session_id: "abc", action: "view", ip_address: "121.10.20.1")
 
-    imp.impression_views.should == 1 
+    imp.impression_views.should == 1
   end
 
-  it "Knows how to distinguish between impressions" do 
-    DrsImpression.create(pid: "1", session_id: "def", action: "view")
-    DrsImpression.create(pid: "1", session_id: "ghi", action: "view")
-    DrsImpression.create(pid: "1", session_id: "def", action: "view") # This should not be counted
-    DrsImpression.create(pid: "1", session_id: "def", action: "download")  
-    DrsImpression.create(pid: "2", session_id: "def", action: "view")  
+  it "Knows how to distinguish between impressions" do
+    DrsImpression.create(pid: "1", session_id: "def", action: "view", ip_address: "1")
+    DrsImpression.create(pid: "1", session_id: "ghi", action: "view", ip_address: "2")
+    DrsImpression.create(pid: "1", session_id: "def", action: "view", ip_address: "3") # This should not be counted
+    DrsImpression.create(pid: "1", session_id: "def", action: "download", ip_address: "4")
+    DrsImpression.create(pid: "2", session_id: "def", action: "view", ip_address: "5")
 
-    imp.impression_views.should == 2 
+    imp.impression_views.should == 2
     imp.impression_downloads.should == 1
   end
 end
