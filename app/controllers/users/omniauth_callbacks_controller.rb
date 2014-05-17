f7fd35149c422b3f661e325c429661c6619a0fed
David Cliff <d.cliff@neu.edu>
2014-05-16 21:27:07 -0400

    Reverting shib setup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,19 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def shibboleth
     auth = request.env["omniauth.auth"]
+    puts auth
 
     @user = User.find_for_shib(request.env["omniauth.auth"], current_user)
 
-    begin
+    if @user.persisted?
       flash[:notice] = "Successfully signed in with Shibboleth"
       sign_in @user, :event => :authentication #this will throw if @user is not activated
       redirect_to root_path
-    rescue => error
+    else
       flash[:error] = "Error with Shibboleth login"
       logger.error "SHIB ERROR - #{error.class}\n\t#{error.to_s}\n #{error.backtrace.join("\n")}\n\n"
       logger.info "Shibboleth error - #{auth.inspect} #{@user.inspect}"
       redirect_to root_path
     end
-
   end
 end
