b7ed299eb3dcc21cbdace3c9c40b977615307edf
David Cliff <d.cliff@neu.edu>
2014-05-16 20:53:21 -0400

    Sigh. Rm'ing pidfile if it exists...

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -40,6 +40,7 @@ namespace :deploy do
   task :restart_workers do
     on roles(:app), :in => :sequence, :wait => 5 do
       execute "cd #{release_path} && (RAILS_ENV=staging /tmp/drs/rvm-auto.sh . kill -TERM $(cat /home/drs/config/resque-pool.pid))"
+      execute "cd #{release_path} && (RAILS_ENV=staging /tmp/drs/rvm-auto.sh . rm -f /home/drs/config/resque-pool.pid)"
       execute "cd #{release_path} && (RAILS_ENV=staging /tmp/drs/rvm-auto.sh . resque-pool --daemon -p /home/drs/config/resque-pool.pid)"
     end
   end
