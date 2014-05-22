33bdfab3e4f9304af7baae12d2d0ae37efc00d0a
David Cliff <d.cliff@neu.edu>
2014-05-22 11:05:19 -0400

    Moving to redis for rails caching instead of default flat file storage

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,7 +11,7 @@ Drs::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -43,6 +43,8 @@ Drs::Application.configure do
 
   # Tell Mailer to use repositorydev as the default host
   config.action_mailer.default_url_options = { :host => "repositorydev.neu.edu" }
+
+  config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 12.hours }
 
   # Mailer configuration
   ActionMailer::Base.smtp_settings = {
