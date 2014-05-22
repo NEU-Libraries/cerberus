c63a6e568c3f557d5a83993cfe0d4ca0b78b57ca
David Cliff <d.cliff@neu.edu>
2014-05-22 11:20:06 -0400

    Minor fix for cache clearing on deploy

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -46,8 +46,15 @@ namespace :deploy do
     end
   end
 
-  desc "Resetting data"
+  desc "Clearing cache"
   task :reset_data do
+    on roles(:app), :in => :sequence, :wait => 5 do
+      execute "cd #{release_path} && (RAILS_ENV=staging /tmp/drs/rvm-auto.sh . rake cache:clear)"
+    end
+  end
+
+  desc "Resetting data"
+  task :clear_cache do
     on roles(:app), :in => :sequence, :wait => 5 do
       execute "cd #{release_path} && (RAILS_ENV=staging /tmp/drs/rvm-auto.sh . rake reset_data)"
     end
@@ -108,7 +115,7 @@ after 'deploy:updating', 'deploy:migrate'
 after 'deploy:updating', 'deploy:whenever'
 after 'deploy:updating', 'deploy:assets_kludge'
 after 'deploy:updating', 'deploy:restart'
+after 'deploy:finished', 'deploy:clear_cache'
 #after 'deploy:finished', 'deploy:reset_data'
-after 'deploy:finished', 'cache:clear'
 after 'deploy:finished', 'deploy:start_solrizerd'
 
