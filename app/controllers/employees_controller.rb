f1501522f2a9f5f6d823ab53b0bb64686c8e37b2
David Cliff <d.cliff@neu.edu>
2014-05-27 16:52:37 -0400

    Bleh, not fixed. Will make a ticket. Reverting for now

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -39,14 +39,12 @@ class EmployeesController < ApplicationController
   end
 
   def personal_graph
-    e_pid = current_user.employee_pid
-    @employee = SolrDocument.new(ActiveFedora::SolrService.query("id:\"#{e_pid}\"").first)
+    @employee = SolrDocument.new(ActiveFedora::SolrService.query("id:\"#{current_user.employee_pid}\"").first)
     @page_title = "My DRS"
   end
 
   def personal_files
-    e_pid = current_user.employee_pid
-    @employee = SolrDocument.new(ActiveFedora::SolrService.query("id:\"#{e_pid}\"").first)
+    @employee = SolrDocument.new(ActiveFedora::SolrService.query("id:\"#{current_user.employee_pid}\"").first)
     @nuid = @employee.nuid
 
     self.solr_search_params_logic += [:exclude_unwanted_models]
