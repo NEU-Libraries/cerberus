42e7edee8b3164f829298e028032450f315033d6
David Cliff <d.cliff@neu.edu>
2014-05-27 15:40:12 -0400

    Actually addresses the google specific issue with the downloadscontroller

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -3,4 +3,10 @@ class DownloadsController < ApplicationController
   include Drs::ControllerHelpers::ViewLogger
 
   after_filter :log_download, only: [:show]
+
+  rescue_from ActiveFedora::ObjectNotFoundError do |exception|
+    @obj_type = "Object"
+    ExceptionNotifier.notify_exception(exception)
+    render_404(ActiveFedora::ObjectNotFoundError.new) and return
+  end
 end
