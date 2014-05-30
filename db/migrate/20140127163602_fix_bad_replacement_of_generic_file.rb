3953c4126d08e6bb845abc52f676cb81bec235bf
Will Jackson <=>
2014-05-30 12:53:06 -0400

    Remove migrations that depend on Paperclip, also remove Trophy table.

--- db/migrate/20140127163602_fix_bad_replacement_of_generic_file.rb
+++
@@ -1,5 +0,0 @@
-class FixBadReplacementOfGenericFile < ActiveRecord::Migration
-  def change
-    rename_column :trophies, :nu_core_file_id, :generic_file_id if Trophy.column_names.include?('nu_core_file_id')
-  end
-end
