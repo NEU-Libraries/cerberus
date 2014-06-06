91e7473843b5f9e9d0f6cfff4ac948b694713e76
Will Jackson <=>
2014-06-06 14:04:58 -0400

    Require that referrer and status are always set

--- app/models/drs_impression.rb
+++ app/models/drs_impression.rb
@@ -3,6 +3,7 @@ class DrsImpression < ActiveRecord::Base
 
   # Ensure that all required fields are present
   validates :pid, :session_id, :action, :ip_address, presence: true
+  validates :referrer, :status, presence: true
 
   # Store all IP addresses the first time they read or download an item
   validates_uniqueness_of :session_id, :scope => [:pid, :action]
