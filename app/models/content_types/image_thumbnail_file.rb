6c9fe038729eecfb7892ad26c32a730b998397fd
David Cliff <d.cliff@neu.edu>
2014-08-20 13:13:33 -0400

    Removing strange comment

--- app/models/content_types/image_thumbnail_file.rb
+++ app/models/content_types/image_thumbnail_file.rb
@@ -1,9 +1,5 @@
 class ImageThumbnailFile < ActiveFedora::Base
   include Drs::NuFile
-
-  # Rather than create 6 'meaningless' entries in the archive for thumbnails
-  # used exclusively by the front-end, we collapse the various sizes of thumbnail
-  # we might want into a single object.
   has_file_datastream name: 'thumbnail_1',     type: FileContentDatastream
   has_file_datastream name: 'thumbnail_2',     type: FileContentDatastream
   has_file_datastream name: 'thumbnail_3',  type: FileContentDatastream
