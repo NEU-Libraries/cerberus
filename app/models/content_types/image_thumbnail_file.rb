a4610153f21b8ef8b85d4aa42a3b1ce2daf327e8
David Cliff <d.cliff@neu.edu>
2014-08-20 13:13:09 -0400

    Updating old datastream names for imagethumbnailfile

--- app/models/content_types/image_thumbnail_file.rb
+++ app/models/content_types/image_thumbnail_file.rb
@@ -6,9 +6,7 @@ class ImageThumbnailFile < ActiveFedora::Base
   # we might want into a single object.
   has_file_datastream name: 'thumbnail_1',     type: FileContentDatastream
   has_file_datastream name: 'thumbnail_2',     type: FileContentDatastream
-  has_file_datastream name: 'thumbnail_2_2x',  type: FileContentDatastream
+  has_file_datastream name: 'thumbnail_3',  type: FileContentDatastream
   has_file_datastream name: 'thumbnail_4',     type: FileContentDatastream
-  has_file_datastream name: 'thumbnail_4_2x',  type: FileContentDatastream
-  has_file_datastream name: 'thumbnail_10',    type: FileContentDatastream
-  has_file_datastream name: 'thumbnail_10_2x', type: FileContentDatastream
+  has_file_datastream name: 'thumbnail_5',  type: FileContentDatastream
 end
