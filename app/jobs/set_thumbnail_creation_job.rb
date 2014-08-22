8ac4bf4586b3c16e8ccda3eeddf03cfb8a23bca3
David Cliff <d.cliff@neu.edu>
2014-08-22 11:55:40 -0400

    Put tmp file storage in module, so as to be reused. Fixes "can't dump File" bug

--- app/jobs/set_thumbnail_creation_job.rb
+++ app/jobs/set_thumbnail_creation_job.rb
@@ -4,14 +4,14 @@ include Drs::ThumbnailCreation
 # community and collection objects.  Actual thumbnail creation for
 # content objects is handled elsewhere.
 class SetThumbnailCreationJob
-  attr_accessor :set, :blob
+  attr_accessor :set, :file_path
 
   # Takes as arguments an ActiveFedora object.
   # An HTTP Uploaded File object or a full string path to a file.
   # And the desired datastream ID
-  def initialize(set, blob)
+  def initialize(set, file_path)
     @set = set
-    @blob = blob
+    @file_path = file_path
   end
 
   def queue_name
@@ -19,6 +19,8 @@ class SetThumbnailCreationJob
   end
 
   def run
+    blob = File.open(@file_path)
+
     if !(file.instance_of? (StringIO)) && !(file.instance_of? ActionDispatch::Http::UploadedFile)
       raise "Invalid type of #{file.class} passed to create_thumbnail." +
             "  Must be string or UploadedFile object."
---
+++ lib/drs/temp_file_storage.rb
@@ -0,0 +1,10 @@
+module Drs::TempFileStorage
+  def move_file_to_tmp(file)
+    # We move the file contents to a more permanent location so that our various jobs can access them.
+    # An ensure block in that job handles cleanup of this file.
+    tempdir = Rails.root.join("tmp")
+    new_path = tempdir.join("#{file.original_filename}")
+    FileUtils.mv(file.tempfile.path, new_path.to_s)
+    return new_path.to_s
+  end
+end
