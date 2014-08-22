532fdde289e0f6fbb7c08bb54b92f14ec8e1fe2b
David Cliff <d.cliff@neu.edu>
2014-08-22 10:50:14 -0400

    Moving thumbnail creation to module so that it can be shared between derivative_creator and set thumbnail job

--- app/models/services/derivative_creator.rb
+++ app/models/services/derivative_creator.rb
@@ -1,5 +1,6 @@
 require 'RMagick'
 include Magick
+include Drs::ThumbnailCreation
 
 class DerivativeCreator
 
@@ -62,34 +63,6 @@ class DerivativeCreator
       pdf.add_file(master.pdf.content, 'content', "#{master.content.label.split('.').first}.pdf")
       pdf.original_filename = "#{master.content.label.split('.').first}.pdf"
       pdf.save! ? pdf : false
-    end
-
-    def create_scaled_progressive_jpeg(thumb, blob, thumbnail_list, size, dsid)
-
-      if (master.is_a? ImageMasterFile) && !(master.width.first.to_i >= size[:width])
-        return false
-      end
-
-      img = Magick::Image.from_blob(blob).first
-
-      if size[:height] && size[:width]
-        scaled_img = img.resize_to_fit(size[:height], size[:width])
-        fill = Magick::Image.new(size[:height], size[:width])
-        fill = fill.matte_floodfill(1, 1)
-        end_img = fill.composite!(scaled_img, Magick::CenterGravity, Magick::OverCompositeOp)
-      elsif size[:width]
-        end_img = img.resize_to_fit(size[:width])
-      else
-        raise "Size must be hash containing :height/:width or :width keys"
-      end
-
-      end_img.format = "JPEG"
-      end_img.interlace = Magick::PlaneInterlace
-
-      thumb.add_file(end_img.to_blob, dsid, "#{self.master.content.label.split('.').first}.jpeg")
-      thumb.save!
-
-      thumbnail_list << "/downloads/#{self.core.thumbnail.pid}?datastream_id=#{dsid}"
     end
 
     def update_or_create_with_metadata(title, desc, klass, object = nil)
