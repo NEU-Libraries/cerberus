373d73dec3ae514431bcd5c11dd9a9bd5238aa36
David Cliff <d.cliff@neu.edu>
2014-08-26 15:04:56 -0400

    The actual rename to Cerberus

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Drs::Application.configure do
+Cerberus::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
