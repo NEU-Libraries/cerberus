b93bc906090116a9a70f313a655007a54d0d15a4
David Cliff <d.cliff@neu.edu>
2014-08-26 10:51:26 -0400

    Almost done

--- spec/helpers/nu_collections_helper_spec.rb
+++ spec/helpers/nu_collections_helper_spec.rb
@@ -32,11 +32,14 @@ describe NuCollectionsHelper do
   describe "#render_create_collection_button" do
     it "returns a link to the new collection path if the permissions are correct" do
       helper.stub(:current_user) { bill }
-      expect(helper.render_upload_files_button(parent)).to eq "<a href=\"/files/new?parent=neu%3A#{(parent.pid).split(":").last}\">Upload files to this collection</a>"
+      expect(helper.render_create_collection_button(parent)).to eq "<a href=\"/collections/new?parent=neu%3A#{(parent.pid).split(":").last}\">Create a child collection off this node</a>"
     end
   end
 
   describe "#render_delete_object_button" do
-    true
+    it "returns a link to the new collection path if the permissions are correct" do
+      helper.stub(:current_user) { bill }
+      expect(helper.render_delete_object_button(parent, "Delete")).to eq "<a href=\"/collections/#{parent.pid}\" data-confirm=\"This destroys the object and all of its descendents.  Are you sure?\" data-method=\"delete\" rel=\"nofollow\">Delete</a>"
+    end
   end
 end
