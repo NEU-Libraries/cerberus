a42adaa90d11c2f803427a2c756ad061944727fc
David Cliff <d.cliff@neu.edu>
2014-08-26 10:39:14 -0400

    Stubbed out, filling in method tests now

--- spec/helpers/nu_collections_helper_spec.rb
+++ spec/helpers/nu_collections_helper_spec.rb
@@ -11,5 +11,29 @@ require 'spec_helper'
 #   end
 # end
 describe NuCollectionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:bill) { FactoryGirl.create(:bill) }
+  let(:parent) { FactoryGirl.create(:valid_owned_by_bill) }
+
+  before :all do
+    User.destroy_all
+  end
+
+  describe "#filtered_permissions" do
+    true
+  end
+
+  describe "#render_upload_files_button" do
+    it "returns a link to the new file path if the permissions are correct" do
+      helper.stub(:current_user) { bill }
+      expect(helper.render_upload_files_button(parent)).to eq "<a href=\"/files/new?parent=neu%3A#{(parent.pid).split(":").last}\">Upload files to this collection</a>"
+    end
+  end
+
+  describe "#render_create_collection_button" do
+    true
+  end
+
+  describe "#render_delete_object_button" do
+    true
+  end
 end
