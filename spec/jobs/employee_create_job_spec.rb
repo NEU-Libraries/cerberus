042783ef24f91886bd4ce062a6ac052ed9e50488
Will Jackson <wjackson64@gmail.com>
2014-09-03 12:35:00 -0400

    Update employee create job to send an email as long as an employee is created, even if job fails at some other point.  Update tests to reflect this.

--- spec/jobs/employee_create_job_spec.rb
+++ spec/jobs/employee_create_job_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 
 describe EmployeeCreateJob do
-
   let(:bill) { FactoryGirl.create(:bill) }
   let(:bo) { FactoryGirl.create(:bo) }
 
@@ -12,6 +11,7 @@ describe EmployeeCreateJob do
   end
 
   before(:each) do
+    ActionMailer::Base.deliveries = []
     Employee.all.map { |e| e.destroy }
   end
 
@@ -44,6 +44,11 @@ describe EmployeeCreateJob do
     # Only one employee is created
     expect(Employee.all.length).to eq 1
 
+    # A new employee alert email is sent
+    expect(ActionMailer::Base.deliveries.length).to eq 1
+    subject = "[cerberus] New Employee Created"
+    expect(ActionMailer::Base.deliveries.first.subject).to eq subject
+
     # Run the job again to make sure collections aren't recreated
     run_job
 
@@ -52,5 +57,40 @@ describe EmployeeCreateJob do
 
     # Verify that there are still only six smart collections
     expect(employee.smart_collections.length).to eq 6
+  end
+
+  it "sends a new employee alert email as long as an employee is created" do
+    EmployeeCreateJob.any_instance.stub(:create_personal_collection)
+    .and_raise StandardError
+
+    # Executing the job raises an exception
+    expect{ run_job }.to raise_error StandardError
+
+    # An employee was created
+    expect(Employee.exists_by_nuid? bill.nuid).to be true
+
+    # Hackily ensure all employees are tagged as complete
+    Employee.all.map { |e| e.employee_is_complete ; e.save! }
+
+    employee = Employee.find_by_nuid bill.nuid
+
+    # No collections were created
+    expect(employee.smart_collections).to match_array []
+
+    # An email was sent
+    expect(ActionMailer::Base.deliveries.length).to eq 1
+  end
+
+  it "doesn't send a new employee alert if the employee fails to build" do
+    Employee.any_instance.stub(:create).and_raise StandardError
+
+    # Executing the job raises an exception
+    expect { run_job }.to raise_error StandardError
+
+    # No employee is created
+    expect(Employee.all.length).to eq 0
+
+    # No email is sent
+    expect(ActionMailer::Base.deliveries.length).to eq 0
   end
 end
