6edb3eef8d437c7cfc9b5f4db85b134a3fd7265d
David Cliff <d.cliff@neu.edu>
2014-09-04 17:09:05 -0400

    Fix for travis and test env for mail address

--- spec/mailers/employee_mailer_spec.rb
+++ spec/mailers/employee_mailer_spec.rb
@@ -3,9 +3,9 @@ require "spec_helper"
 describe EmployeeMailer do
   before(:all) { @employee = FactoryGirl.create(:employee) }
   describe "New employee alerts" do
-    it "sends the emails to wjackson in the test environment" do
+    it "sends the emails to bogus test email in the test environment" do
       msg = EmployeeMailer.new_employee_alert(@employee).deliver
-      expect(msg.to).to match_array ["wjackson64@gmail.com"]
+      expect(msg.to).to match_array ["test@test.com"]
       expect(ActionMailer::Base.deliveries.length).to eq 1
     end
 
