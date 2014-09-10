d41a6c2373b577bbb5553f6124ff19f355b3dfc4
David Cliff <d.cliff@neu.edu>
2014-09-10 13:28:02 -0400

    Copy pasta strikes again

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -21,7 +21,7 @@ class Employee < ActiveFedora::Base
 
   def to_solr(solr_doc = Hash.new())
     super(solr_doc)
-    solr_doc["type_sim"] = I18n.t("drs.display_labels.#{item_class}.name")
+    solr_doc["type_sim"] = I18n.t("drs.display_labels.#{self.class}.name")
     return solr_doc
   end
 
