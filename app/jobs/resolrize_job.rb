7d1f29debac1996f7fca7e7fc5514531f0e79001
David Cliff <d.cliff@neu.edu>
2014-09-25 15:27:51 -0400

    Updating solrizerd and commenting out resolrize to fix the ThreadError

--- app/jobs/resolrize_job.rb
+++ app/jobs/resolrize_job.rb
@@ -4,13 +4,16 @@ class ResolrizeJob
   end
 
   def run
-    require 'active_fedora/version'
-    active_fedora_version = Gem::Version.new(ActiveFedora::VERSION)
-    minimum_feature_version = Gem::Version.new('6.4.4')
-    if active_fedora_version >= minimum_feature_version
-      ActiveFedora::Base.reindex_everything("pid~#{Cerberus::Application.config.id_namespace}:*")
-    else
-      ActiveFedora::Base.reindex_everything
-    end
+    # Disabling as a result of SolrService's poor thread handling
+    # which creates #<ThreadError: can't create Thread (11)> errors
+
+    # require 'active_fedora/version'
+    # active_fedora_version = Gem::Version.new(ActiveFedora::VERSION)
+    # minimum_feature_version = Gem::Version.new('6.4.4')
+    # if active_fedora_version >= minimum_feature_version
+    #   ActiveFedora::Base.reindex_everything("pid~#{Cerberus::Application.config.id_namespace}:*")
+    # else
+    #   ActiveFedora::Base.reindex_everything
+    # end
   end
 end
