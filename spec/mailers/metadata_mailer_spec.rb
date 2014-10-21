ab1c4b4d97734da0282e72ee0d2b48861aeed96d
David Cliff <d.cliff@neu.edu>
2014-10-21 12:39:17 -0400

    Fixes #588

--- spec/mailers/metadata_mailer_spec.rb
+++ spec/mailers/metadata_mailer_spec.rb
@@ -1,20 +1,24 @@
 require "spec_helper"
 
 describe MetadataMailer do
-  
-  describe "daily_alert_email" do 
-    before :each do 
-      FactoryGirl.create_list(:theses_alert, 2) 
+
+  describe "daily_alert_email" do
+    before :each do
+      FactoryGirl.create_list(:theses_alert, 2)
     end
 
-    let(:mail) { MetadataMailer.daily_alert_email } 
+    let(:mail) { MetadataMailer.daily_alert_email }
 
-    it "purges sent emails" do 
+    it "purges sent emails" do
       UploadAlert.withheld_theses(:create).size.should == 2
-      mail 
-      UploadAlert.withheld_theses(:create).size.should == 0 
+      mail
+      UploadAlert.withheld_theses(:create).size.should == 0
     end
 
+    it "should have a subject with a correct item count" do
+      mail.deliver!
+      ActionMailer::Base.deliveries.first.subject.should == "Daily Featured Content Uploads and Updates - 2 items"
+    end
 
   end
 end
