604d7ec82a5de1ac41447cc17fc51a7793ee6603
David Cliff <d.cliff@neu.edu>
2014-11-10 11:09:06 -0500

    Cleaning out desc_metadata references, still some to go

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -180,172 +180,23 @@ class CatalogController < ApplicationController
     # of Solr search fields.
     # creator, title, description, publisher, date_created,
     # subject, language, resource_type, format, identifier, based_near,
-    config.add_search_field('contributor') do |field|
-      # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = { :"spellcheck.dictionary" => "contributor" }
 
-      # :solr_local_parameters will be sent using Solr LocalParams
-      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
-      # Solr parameter de-referencing like $title_qf.
-      # See: http://wiki.apache.org/solr/LocalParams
-      solr_name = solr_name("desc_metadata__contributor", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
+    # Commenting out because we no longer have desc_metadata, but want it here for posterity.
 
+    # config.add_search_field('contributor') do |field|
+    #   # solr_parameters hash are sent to Solr as ordinary url query params.
+    #   field.solr_parameters = { :"spellcheck.dictionary" => "contributor" }
 
-
-    config.add_search_field('creator') do |field|
-      field.solr_parameters = { :"spellcheck.dictionary" => "creator" }
-      solr_name = solr_name("desc_metadata__creator", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('title') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "title"
-      }
-      solr_name = solr_name("desc_metadata__title", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('description') do |field|
-      field.label = "Abstract or Summary"
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "description"
-      }
-      solr_name = solr_name("desc_metadata__description", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('publisher') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "publisher"
-      }
-      solr_name = solr_name("desc_metadata__publisher", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('date_created') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "date_created"
-      }
-      solr_name = solr_name("desc_metadata__created", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('subject') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "subject"
-      }
-      solr_name = solr_name("desc_metadata__subject", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('language') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "language"
-      }
-      solr_name = solr_name("desc_metadata__language", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('resource_type') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "resource_type"
-      }
-      solr_name = solr_name("desc_metadata__resource_type", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('format') do |field|
-      field.include_in_advanced_search = false
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "format"
-      }
-      solr_name = solr_name("desc_metadata__format", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('identifier') do |field|
-      field.include_in_advanced_search = false
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "identifier"
-      }
-      solr_name = solr_name("desc_metadata__id", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('based_near') do |field|
-      field.label = "Location"
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "based_near"
-      }
-      solr_name = solr_name("desc_metadata__based_near", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('tag') do |field|
-      field.solr_parameters = {
-        :"spellcheck.dictionary" => "tag"
-      }
-      solr_name = solr_name("desc_metadata__tag", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('depositor') do |field|
-      solr_name = solr_name("desc_metadata__depositor", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
-
-    config.add_search_field('rights') do |field|
-      solr_name = solr_name("desc_metadata__rights", :stored_searchable, type: :string)
-      field.solr_local_parameters = {
-        :qf => solr_name,
-        :pf => solr_name
-      }
-    end
+    #   # :solr_local_parameters will be sent using Solr LocalParams
+    #   # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+    #   # Solr parameter de-referencing like $title_qf.
+    #   # See: http://wiki.apache.org/solr/LocalParams
+    #   solr_name = solr_name("desc_metadata__contributor", :stored_searchable, type: :string)
+    #   field.solr_local_parameters = {
+    #     :qf => solr_name,
+    #     :pf => solr_name
+    #   }
+    # end
 
 
     # "sort results by" select (pulldown)
