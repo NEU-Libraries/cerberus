701628ece5a26e2420d214053f9a2725a40b166a
David Cliff <d.cliff@neu.edu>
2014-11-14 16:38:47 -0500

    Removing copy pasta commented out code

--- app/jobs/core_file_html_validation_job.rb
+++ app/jobs/core_file_html_validation_job.rb
@@ -4,21 +4,6 @@ class CoreFileHtmlValidationJob
   end
 
   def run
-    # Disabling as a result of SolrService's poor thread handling
-    # which creates #<ThreadError: can't create Thread (11)> errors
-
-    # require 'active_fedora/version'
-    # active_fedora_version = Gem::Version.new(ActiveFedora::VERSION)
-    # minimum_feature_version = Gem::Version.new('6.4.4')
-    # if active_fedora_version >= minimum_feature_version
-    #   ActiveFedora::Base.reindex_everything("pid~#{Cerberus::Application.config.id_namespace}:*")
-    # else
-    #   ActiveFedora::Base.reindex_everything
-    # end
-
-    # #{Rails.root}
-    # log/solrizer.log
-
     logger = Logger.new("#{Rails.root}/log/core_file_html_validation.log")
 
     community_model = ActiveFedora::SolrService.escape_uri_for_query "info:fedora/afmodel:CoreFile"
