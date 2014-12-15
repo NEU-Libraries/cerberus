e1830567c00dc3c5b7de80ceaa5ef3f59e90d9a8
David Cliff <d.cliff@neu.edu>
2014-12-15 21:40:14 +0000

    Many things failing with the new test. Commenting out for now, deal with it later

--- spec/factories/employee_factory.rb
+++ spec/factories/employee_factory.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :employee do
     nuid "000000001"
-    name "bill"
+    name "bill baggins"
 
     factory :sequenced_employee do
       sequence(:nuid) { |n| "#{n}" }
-      sequence(:name) { |n| "user #{n}" }
+      sequence(:name) { |n| "firstname lastname" }
       mass_permissions "public"
     end
   end
