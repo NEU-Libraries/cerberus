fa14e194665bac12dbf29ce6e75b66d94818dd8d
David Cliff <d.cliff@neu.edu>
2014-12-18 18:50:30 +0000

    Made into module, and repackaged a little. Still need to make it ajaxy

--- app/controllers/core_files_controller.rb
+++ app/controllers/core_files_controller.rb
@@ -9,6 +9,7 @@ class CoreFilesController < ApplicationController
   include Cerberus::ControllerHelpers::PermissionsCheck
 
   include ModsDisplay::ControllerExtension
+  include XmlValidator
 
   before_filter :authenticate_user!, except: [:show]
 
@@ -199,56 +200,8 @@ class CoreFilesController < ApplicationController
     render :template => 'core_files/ace_xml_editor'
   end
 
-  def xml_valid?(xml_str)
-    # Never trust the user
-    errors = []
-    # Nokogiri xml validation, catch errors
-    doc = Nokogiri::XML(xml_str)
-    if doc.errors != []
-      errors = doc.errors
-      return errors
-    end
-
-    # Dummy corefile for testing
-    doc = CoreFile.new
-    doc.mods.content = xml_str
-
-    # Try rendering html with mods display, catch errors
-    begin
-      CoreFilesController.new.render_mods_display(doc).to_html
-    rescue Exception => error
-      errors << error
-    end
-
-    if errors != []
-      return errors
-    end
-
-    # Does it have a title?
-    if doc.title == nil
-      errors << "No valid title in xml"
-      return errors
-    end
-
-    # Does it have at least one keyword?
-    if doc.keywords == []
-      errors << "No valid keywords in xml"
-      return errors
-    end
-
-    # Can we solrize the core_file if we use this xml?
-    begin
-      doc.to_solr
-    rescue Exception => error
-      errors << error
-    end
-
-    if errors != []
-      return errors
-    end
-
-    # If we've gotten this far, we should be able to return mods_display html
-    return CoreFilesController.new.render_mods_display(doc).to_html
+  def check_xml
+    return xml_valid?(params[:raw_xml])
   end
 
   def update
