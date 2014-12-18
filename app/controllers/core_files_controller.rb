fb38a9cb63878c862d430937783307f41b9c80d7
David Cliff <d.cliff@neu.edu>
2014-12-18 18:36:24 +0000

    Stubbing out the xml validation

--- app/controllers/core_files_controller.rb
+++ app/controllers/core_files_controller.rb
@@ -199,6 +199,58 @@ class CoreFilesController < ApplicationController
     render :template => 'core_files/ace_xml_editor'
   end
 
+  def xml_valid?(xml_str)
+    # Never trust the user
+    errors = []
+    # Nokogiri xml validation, catch errors
+    doc = Nokogiri::XML(xml_str)
+    if doc.errors != []
+      errors = doc.errors
+      return errors
+    end
+
+    # Dummy corefile for testing
+    doc = CoreFile.new
+    doc.mods.content = xml_str
+
+    # Try rendering html with mods display, catch errors
+    begin
+      CoreFilesController.new.render_mods_display(doc).to_html
+    rescue Exception => error
+      errors << error
+    end
+
+    if errors != []
+      return errors
+    end
+
+    # Does it have a title?
+    if doc.title == nil
+      errors << "No valid title in xml"
+      return errors
+    end
+
+    # Does it have at least one keyword?
+    if doc.keywords == []
+      errors << "No valid keywords in xml"
+      return errors
+    end
+
+    # Can we solrize the core_file if we use this xml?
+    begin
+      doc.to_solr
+    rescue Exception => error
+      errors << error
+    end
+
+    if errors != []
+      return errors
+    end
+
+    # If we've gotten this far, we should be able to return mods_display html
+    return CoreFilesController.new.render_mods_display(doc).to_html
+  end
+
   def update
     @core_file = CoreFile.find(params[:id])
 
