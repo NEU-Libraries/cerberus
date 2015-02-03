7f5b258095915bd86789ee7f199627c61ded32de
David Cliff <d.cliff@neu.edu>
2015-02-03 22:35:21 +0000

    Yay, mostly working...

--- app/controllers/api/v1/handles_controller.rb
+++ app/controllers/api/v1/handles_controller.rb
@@ -1,7 +1,27 @@
 module Api
   module V1
     class HandlesController < ApplicationController
-      # 
+
+      def create_handle
+        providedUrl = params[:url]
+        if HandleGenerator::handle_exists?(providedUrl)
+          handle = HandleGenerator::get_handle(providedUrl)
+        else
+          handle = HandleGenerator::make_handle(providedUrl)
+        end
+        render :json => [{:handle => "#{handle}"}]
+      end
+
+      def get_handle
+        puts "DGCDEBUG"
+        providedUrl = params[:url]
+        puts providedUrl
+        puts "DGCDEBUG2"
+        handle = HandleGenerator::get_handle(providedUrl)
+        puts handle
+        puts "DGCDEBUG3"
+        render :json => [{:handle => "#{handle}"}]
+      end
     end
   end
 end
