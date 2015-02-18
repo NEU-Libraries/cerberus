d4bcb95e25bd79b66c4cc62badd8b856ba4e0ca5
David Cliff <d.cliff@neu.edu>
2015-02-18 14:53:39 -0500

    Up and drop of test handle sql for spec

--- spec/helpers/handle_helper_spec.rb
+++ spec/helpers/handle_helper_spec.rb
@@ -1,9 +1,16 @@
 require "spec_helper"
 
 describe HandleHelper do
+
   before :all do
+    sh "mysql -u #{ENV["HANDLE_TEST_USERNAME"]} -p#{ENV["HANDLE_TEST_PASSWORD"]} < #{Rails.root}/spec/fixtures/files/handlesTEST.sql"
+    client = Mysql2::Client.new(:host => "#{ENV["HANDLE_TEST_HOST"]}",
+      :username => "#{ENV["HANDLE_TEST_USERNAME"]}",
+      :password => "#{ENV["HANDLE_TEST_PASSWORD"]}",
+      :database => "#{ENV["HANDLE_TEST_DATABASE"]}")
   end
-  
+
   after :all do
+    client.query("DROP DATABASE #{ENV["HANDLE_TEST_DATABASE"]};")
   end
 end
