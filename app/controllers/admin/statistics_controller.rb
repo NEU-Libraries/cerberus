57d22bc570ff9ac3b4d592a236dd306d0c72bc21
David Cliff <d.cliff@neu.edu>
2015-02-24 13:56:11 -0500

    Ok. This includes the content types (and doesn't include derivatives). Ready for styling

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -8,6 +8,19 @@ class Admin::StatisticsController < ApplicationController
     @community_count = get_count_for_model_type("info:fedora/afmodel:Community")
     @collection_count = get_count_for_model_type("info:fedora/afmodel:Collection")
     @core_file_count = get_count_for_model_type("info:fedora/afmodel:CoreFile")
+
+    # content type counts
+    @audio_count = get_count_for_content_obj("info:fedora/afmodel:AudioFile")
+    # Not including derivatives
+    @image_count = get_count_for_content_obj("info:fedora/afmodel:ImageMasterFile")
+    @excel_count = get_count_for_content_obj("info:fedora/afmodel:MsexcelFile")
+    @ppt_count = get_count_for_content_obj("info:fedora/afmodel:MspowerpointFile")
+    @word_count = get_count_for_content_obj("info:fedora/afmodel:MswordFile")
+    # This will include word derivatives...will look into a way to solve that...
+    @pdf_count = get_count_for_content_obj("info:fedora/afmodel:PdfFile")
+    @text_count = get_count_for_content_obj("info:fedora/afmodel:TextFile")
+    @video_count = get_count_for_content_obj("info:fedora/afmodel:VideoFile")
+    @zip_count = get_count_for_content_obj("info:fedora/afmodel:ZipFile")
   end
 
   private
@@ -18,8 +31,15 @@ class Admin::StatisticsController < ApplicationController
 
     def get_count_for_model_type(model_string)
       model_type = ActiveFedora::SolrService.escape_uri_for_query "#{model_string}"
-      # ignoreing smart collections, so they don't inflate collection count
+      # ignoring smart collections, so they don't inflate collection count
       query_result = ActiveFedora::SolrService.query("has_model_ssim:\"#{model_type}\"  -smart_collection_type_tesim:[* TO *]")
+      return query_result.length
+    end
+
+    def get_count_for_content_obj(model_string)
+      model_type = ActiveFedora::SolrService.escape_uri_for_query "#{model_string}"
+      # only finding canonical objects to avoid derivatives
+      query_result = ActiveFedora::SolrService.query("has_model_ssim:\"#{model_type}\" AND canonical_tesim:[* TO *]")
       return query_result.length
     end
 
