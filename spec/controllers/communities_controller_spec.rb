3fafcfd4c9db01141f738e7ab88f7d073d80d968
David Cliff <d.cliff@neu.edu>
2015-02-25 13:41:56 -0500

    Fix remaining community spec test

--- spec/controllers/communities_controller_spec.rb
+++ spec/controllers/communities_controller_spec.rb
@@ -1,25 +1,14 @@
 require 'spec_helper'
 
 describe CommunitiesController do
-  let(:admin)                 { FactoryGirl.create(:admin) }
-  let(:bo)                    { FactoryGirl.create(:bo) } 
   let(:root_community)        { FactoryGirl.create(:root_community) }
-  let(:test_community)        { FactoryGirl.create(:test_community) }
 
-  describe "GET #index" do
-    it "renders the show template for root_community with no user logged in" do      
-      get :index
-      response.status.should == 302
-      expect(response).to redirect_to(community_path(id: 'neu:1'))
-    end  
-  end
+  describe "GET #show" do
+    it "renders the show template for unauthed users on public collections" do
 
-  describe "GET #show" do 
-    it "renders the show template for unauthed users on public collections" do 
+      get :show, { id: root_community.identifier }
 
-      get :show, { id: root_community.identifier } 
-
-      expect(response).to render_template('shared/sets/show') 
+      expect(response).to render_template('shared/sets/show')
     end
   end
 end
