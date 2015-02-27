813729171c155efe01a9698c2ce10dde38639237
David Cliff <d.cliff@neu.edu>
2015-02-27 11:20:22 -0500

    Fixes #669 - removing an old hack related to #578

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -48,8 +48,6 @@ class CollectionsController < ApplicationController
     @set = fetch_solr_document
     self.solr_search_params_logic += [:limit_to_scope]
 
-    # Kludge because of blacklights assumptions
-    params[:id] = params[:solr_field]
     @pagination = get_facet_pagination(params[:solr_field], params)
 
     respond_to do |format|
--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -40,8 +40,6 @@ class CommunitiesController < ApplicationController
     @set = fetch_solr_document
     self.solr_search_params_logic += [:limit_to_scope]
 
-    # Kludge because of blacklights assumptions
-    params[:id] = params[:solr_field]
     @pagination = get_facet_pagination(params[:solr_field], params)
 
     respond_to do |format|
