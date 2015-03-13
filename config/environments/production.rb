9575de7fdc14df92784019de11ff32a75faca028
David Cliff <d.cliff@neu.edu>
2015-03-13 17:27:41 +0000

    Updating environment vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,73 +1,60 @@
 Cerberus::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = false
+
+  # Show full error reports and enable caching
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.action_mailer.raise_delivery_errors = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
-  # Development Tracking Code
-  # GA.tracker = "UA-4426028-6"
-
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
-
-  # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
-
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 12.hours }
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable threaded mode
-  # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners
+  # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :notify
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Compress assets
+  config.assets.compress = true
+
+  # Serve static assets
+  config.serve_static_assets = true
+
+  # Expands the lines which load the assets
+  config.assets.debug = false
+
+  # Tell Mailer to use SMTP
+  config.action_mailer.delivery_method = :smtp
+
+  # Tell Mailer to use repositorydev as the default host
+  config.action_mailer.default_url_options = { :host => "repository.library.northeastern.edu" }
+
+  config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 12.hours }
+
+  # Mailer configuration
+  ActionMailer::Base.smtp_settings = {
+    address: ENV["MAILER_ADDRESS"],
+    port: ENV["MAILER_PORT"],
+    domain: ENV["MAILER_DOMAIN"],
+    user_name: ENV["MAILER_USERNAME"],
+    password: ENV["MAILER_PASSWORD"],
+    authentication: ENV["MAILER_AUTHENTICATION"],
+    enable_starttls_auto: true
+  }
 
   config.lograge.enabled = true
   config.log_level = :warn
@@ -80,8 +67,10 @@ Cerberus::Application.configure do
     :ignore_crawlers => %w{Googlebot bingbot},
     :email => {
       :email_prefix => "[DRS Production] ",
-      :sender_address => %{"notifier" <notifier@repositorydev.neu.edu>},
+      :sender_address => %{"notifier" <notifier@repository.library.northeastern.edu>},
       :exception_recipients => %w{d.cliff@neu.edu}
     }
 
+  #Google analytics tracking code
+  #GA.tracker = "UA-4426028-12"
 end
