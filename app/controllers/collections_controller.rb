e6e5c4afd84e303b6a7397c1b3e2b22e9e03f30f
Eli Zoller <zoller.eli@gmail.com>
2015-03-18 08:52:43 -0400

    collections fix for issue #320

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -49,7 +49,7 @@ class CollectionsController < ApplicationController
 
     if !params[:q].nil?
       # Fixes #667 - we remove single characters. They're a pretty terrible idea with a strict AND
-      params[:q].gsub!(/(^| ).( |$)/, ' ')      
+      params[:q].gsub!(/(^| ).( |$)/, ' ')
       self.solr_search_params_logic += [:limit_to_scope]
     else
       self.solr_search_params_logic += [:show_children_only]
@@ -69,6 +69,10 @@ class CollectionsController < ApplicationController
   def new
     @page_title = "New Collection"
     @set = Collection.new(parent: params[:parent])
+    # Set public default permissions if parent is public
+    if @set.parent.mass_permissions == 'public'
+      @set.mass_permissions = 'public'
+    end
     render :template => 'shared/sets/new'
   end
 
