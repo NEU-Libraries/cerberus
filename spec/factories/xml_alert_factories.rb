0cedae8e1a3fed750289d64cbb2208c5e68195e9
David Cliff <d.cliff@neu.edu>
2015-04-11 02:24:12 +0000

    Nailed it. Just have to do the model spec, and this is done

--- spec/factories/xml_alert_factories.rb
+++ spec/factories/xml_alert_factories.rb
@@ -6,5 +6,17 @@ FactoryGirl.define do
     sequence(:pid)              { |n| "000#{n}" }
     sequence(:old_file_str)     { |n| "<note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>" }
     sequence(:new_file_str)     { |n| "<note><to>Tove</to><from>George</from><heading>Reminder</heading><body>Don't forget me this weekend!</body></note>" }
+
+
+    trait :diff_made do
+      after(:create) do |upload_alert|
+        upload_alert.diff = Diffy::Diff.new(upload_alert.old_file_str, upload_alert.new_file_str, :include_plus_and_minus_in_html => true, :context => 1).to_s(:html)
+        upload_alert.save!
+      end
+    end
+
+    factory :updated_xml do
+      diff_made
+    end
   end
 end
