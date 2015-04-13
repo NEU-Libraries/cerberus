feaeaac2892a1161047300af6ae4d770063df920
Eli Zoller <zoller.eli@gmail.com>
2015-04-13 15:06:04 -0400

    start of views for tombstoned, incomplete, and in progress for admin panel

---
+++ app/controllers/admin/core_files_controller.rb
@@ -0,0 +1,64 @@
+require 'blacklight/catalog'
+require 'blacklight_advanced_search'
+require 'parslet'
+require 'parsing_nesting/tree'
+
+class Admin::EmployeesController < AdminController
+
+  include Blacklight::Catalog
+  include Blacklight::Configurable # comply with BL 3.7
+  include ActionView::Helpers::DateHelper
+  # This is needed as of BL 3.7
+  self.copy_blacklight_config_from(CatalogController)
+
+  include BlacklightAdvancedSearch::ParseBasicQ
+  include BlacklightAdvancedSearch::Controller
+
+  before_filter :authenticate_user!
+  before_filter :verify_admin
+  #before_filter :load_employee, except: [:index, :update, :filter_list]
+
+  def index
+    self.solr_search_params_logic += [:limit_to_tombstone]
+    (@response, @tombstoned) = get_search_results
+    self.solr_search_params_logic.delete(:limit_to_tombstone)
+    self.solr_search_params_logic += [:limit_to_in_progress]
+    (@response, @in_progress) = get_search_results
+    self.solr_search_params_logic.delete(:limit_to_in_progress)
+    self.solr_search_params_logic += [:limit_to_incomplete]
+    (@response, @incomplete) = get_search_results
+    self.solr_search_params_logic.delete(:limit_to_incomplete)
+
+
+    @page_title = "Administer Core Files"
+  end
+
+
+  def destroy
+    @core_file = CoreFile.find(@core_file.pid)
+    pid = @core_file.pid
+
+    if @core_file.pid
+      redirect_to admin_core_files_path, notice: "Core File #{pid} removed"
+    else
+      redirect_to admin_core_files_path, notice: "Something went wrong"
+    end
+  end
+
+
+  private
+
+    def limit_to_tombstone(solr_parameters, user_parameters)
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << "tombstoned_ssi:\"true\""
+    end
+    def limit_to_in_progress(solr_parameters, user_parameters)
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << "in_progress_tesim:\"true\""
+    end
+    def limit_to_incomplete(solr_parameters, user_parameters)
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << "incomplete_tesim:\"true\""
+    end
+
+end
