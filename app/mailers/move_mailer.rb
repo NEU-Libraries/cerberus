13fa050b1685f97998ac390cb61b56a8840ead2a
Eli Zoller <zoller.eli@gmail.com>
2015-04-21 16:22:03 -0400

    fixes #710

--- app/mailers/move_mailer.rb
+++ app/mailers/move_mailer.rb
@@ -1,10 +1,11 @@
 class MoveMailer < ActionMailer::Base
   default from: "notifier@repository.library.northeastern.edu"
 
-  def move_alert(core_file, reason)
+  def move_alert(core_file, reason, collection_url)
     @title = core_file.title || "No title set.  Uh oh!"
     @pid  = core_file.pid  || "No pid set.  Uh oh!"
     @reason = reason
+    @collection_url = collection_url
     mail(to: pick_receiver,
          subject: "[cerberus] User Requested File Move",
          content_type: "text/html")
