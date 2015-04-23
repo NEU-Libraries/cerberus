0501fbca2161e4d4912b8fffaf29d2f4d5fb5a2c
David Cliff <d.cliff@neu.edu>
2015-04-23 18:29:56 +0000

    Barebones new controllers for loaders

---
+++ app/controllers/loaders/marcom_controller.rb
@@ -0,0 +1,19 @@
+class Loaders::MarcomController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :verify_group
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  private
+
+    def verify_group
+      # if user is not part of the marcom_loader grouper group, bounce them
+    end
+end
