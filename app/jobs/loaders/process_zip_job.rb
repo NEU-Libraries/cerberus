5bf617094f1cd56c1f4bce20bf0888ea48cb533f
David Cliff <d.cliff@neu.edu>
2015-04-23 18:48:20 +0000

    Some pseudocode for the jobs

--- app/jobs/loaders/process_zip_job.rb
+++ app/jobs/loaders/process_zip_job.rb
@@ -4,5 +4,11 @@ class Loaders::ProcessZipJob
   end
 
   def run
+    # unzip zip file to tmp storage
+    # for each file in new dir
+      # start a new image_processing_job like...
+      # result = ImageProcessingJob.new().run
+      # result will be a file_report
+    # when all images are processed, create a load_report
   end
 end
