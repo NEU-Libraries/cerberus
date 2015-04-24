3fa045564d4454d3e490b778fbd4793c1dc02ac8
David Cliff <d.cliff@neu.edu>
2015-04-24 18:44:58 +0000

    Framing

--- lib/helpers/mime_helper.rb
+++ lib/helpers/mime_helper.rb
@@ -1,10 +1,12 @@
 module MimeHelper
 
   def extract_mime_type(file_location)
-    # given
+    # given a files location at the operating system level
+    # use file to extract it's mime type - to be used in
+    # assign_type, but we may have more use for this
   end
 
-  def extract_extension
+  def extract_extension(mime_type)    
   end
 
 end
