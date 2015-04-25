adfb65df7d970e0162f5228b29241f5b64cf0c91
David Cliff <d.cliff@neu.edu>
2015-04-25 20:27:32 +0000

    Replace rack for ext, use /etc/mime.types

--- lib/cerberus/downloads_controller_behavior.rb
+++ lib/cerberus/downloads_controller_behavior.rb
@@ -2,6 +2,7 @@ module Cerberus
   module DownloadsControllerBehavior
     extend ActiveSupport::Concern
     include Hydra::Controller::DownloadBehavior
+    include MimeHelper
 
     included do
       # module mixes in normalize_identifier method
@@ -15,7 +16,7 @@ module Cerberus
       if datastream.dsid == self.class.default_content_dsid
         # params[:filename] || asset.label
         # Fix for #680
-        "neu_#{asset.pid.split(":").last}#{Rack::Mime::MIME_TYPES.invert[asset.characterization.mime_type.first]}"
+        "neu_#{asset.pid.split(":").last}#{extract_extension(asset.characterization.mime_type.first)}"
       else
         params[:datastream_id]
       end
