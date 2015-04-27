33f4ca8dcd03941626e715ca0120a3caa0960d68
Eli Zoller <zoller.eli@gmail.com>
2015-04-27 15:14:30 -0400

    populating upload form with collections from parent pid

--- app/controllers/loaders/marcom_controller.rb
+++ app/controllers/loaders/marcom_controller.rb
@@ -3,8 +3,17 @@ class Loaders::MarcomController < ApplicationController
   before_filter :verify_group
 
   def new
-    @parent = "pid goes here"
-    render :template => 'loaders/new'
+    @parent = Community.find("neu:8s45qc00v")
+    @collections_options = Array.new
+    cols = @parent.child_collections.sort_by{|c| c.title}
+    cols.each do |child|
+      @collections_options.push([child.title, child.pid])
+      children = child.child_collections.sort_by{|c| c.title}
+      children.each do |c|
+        @collections_options.push([" - #{c.title}", c.pid])
+      end
+    end
+    render 'loaders/new', locals: { collections_options: @collections_options }
   end
 
   def create
