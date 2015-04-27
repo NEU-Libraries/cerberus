bf929fdc39314f5a28c8ff7d6d3d63a312ae2f9d
Eli Zoller <zoller.eli@gmail.com>
2015-04-27 16:20:59 -0400

    starting upload zip process

--- app/jobs/loaders/image_processing_job.rb
+++ app/jobs/loaders/image_processing_job.rb
@@ -6,5 +6,22 @@ class Loaders::ImageProcessingJob
   def run
     # extract metadata from iptc
     # if theres an exception, log details to image_report
+    photo = IPTC::JPEG::Image.from_file i, quick=true
+    photo.values.each do |item|
+      puts "#{item.key}\t#{item.value}"
+      if item.key == 'iptc/Headline'
+        core_file.title = item.value
+      # creator (iptc/Credit)
+      #elsif item.key == 'iptc/Credit'
+      #  core_file.corporate_creators = item.value
+      elsif item.key == 'iptc/Caption'
+        core_file.description = item.value
+      # organization (iptc/Source)
+      #elsif item.key == 'iptc/Source'
+      #  core_file.organization = item.value
+      #elsif item.key == 'iptc/Keywords'
+      #  core_file.keywords = item.value
+      end
+    end
   end
 end
