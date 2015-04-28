fe2010da6fc15b9578f3e408b07b1d992ba7160d
Eli Zoller <zoller.eli@gmail.com>
2015-04-28 13:19:11 -0400

    moved jobs out of loaders folder for now becuase i couldnt get the jobs to be called, building out job functionality

--- app/jobs/image_processing_job.rb
+++ app/jobs/image_processing_job.rb
@@ -1,12 +1,18 @@
-class Loaders::ImageProcessingJob
+class ImageProcessingJob
+  attr_accessor :file
+
   def queue_name
     :loader_image_processing
+  end
+
+  def initialize(file)
+    self.file = file
   end
 
   def run
     # extract metadata from iptc
     # if theres an exception, log details to image_report
-    photo = IPTC::JPEG::Image.from_file i, quick=true
+    photo = IPTC::JPEG::Image.from_file file, quick=true
     photo.values.each do |item|
       puts "#{item.key}\t#{item.value}"
       if item.key == 'iptc/Headline'
