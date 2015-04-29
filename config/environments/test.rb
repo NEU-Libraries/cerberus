7d3449cb79ed64ed0ed4392b473ca5b234d94d42
David Cliff <d.cliff@neu.edu>
2015-04-29 10:25:29 -0400

    Using rails root instead...

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,11 +1,7 @@
 Cerberus::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  if !ENV['TRAVIS'].nil? && ENV['TRAVIS'] == 'true'
-    config.fedora_home = "/home/travis/cerberus/jetty/fedora/test/data/datastreamStore/"
-  else
-    config.fedora_home = "/home/vagrant/cerberus/jetty/fedora/test/data/datastreamStore/"
-  end
+  config.fedora_home = "#{Rails.root.to_s}/jetty/fedora/test/data/datastreamStore/"
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
