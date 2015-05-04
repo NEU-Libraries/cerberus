b58d2d06ed995866326b63b591b8ca41d51b6716
Eli Zoller <zoller.eli@gmail.com>
2015-05-04 15:52:54 -0400

    adding image report relation and report creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150504194419) do
+ActiveRecord::Schema.define(:version => 20150504194859) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -78,8 +78,9 @@ ActiveRecord::Schema.define(:version => 20150504194419) do
     t.string   "title"
     t.text     "iptc"
     t.text     "exception"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+    t.integer  "load_report_id"
   end
 
   create_table "impressions", :force => true do |t|
