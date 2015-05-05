2c872325a6f991e6fcc19ae77f2c090315aa83fa
Eli Zoller <zoller.eli@gmail.com>
2015-05-05 13:41:26 -0400

    work on views and routes for load reports and image reports

--- app/models/loaders/load_report.rb
+++ app/models/loaders/load_report.rb
@@ -2,14 +2,14 @@ class Loaders::LoadReport < ActiveRecord::Base
   has_many :image_reports
   attr_accessible :timestamp, :number_of_files, :success_count, :fail_count, :name, :email, :loader
 
-  def self.create_from_strings(user, time, loader_name)
+  def self.create_from_strings(user, time, results, loader_name)
     x = Loaders::LoadReport.new
 
     x.time        = time
     x.name             = user.full_name
     x.email            = user.email
     x.loader_name      = loader_name
-    x.number_of_files  = 0
+    x.number_of_files  = results
     x.save! ? x : false
 
     return x.id
