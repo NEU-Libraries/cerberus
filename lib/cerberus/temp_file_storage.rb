0b34831e800f38051e7652bf9ab8c9d757320895
David Cliff <d.cliff@neu.edu>
2015-05-14 10:58:38 -0400

    Minor refactor for prod space usage

--- lib/cerberus/temp_file_storage.rb
+++ lib/cerberus/temp_file_storage.rb
@@ -4,7 +4,11 @@ module Cerberus::TempFileStorage
     def move_file_to_tmp(file)
       # We move the file contents to a more permanent location so that our various jobs can access them.
       # An ensure block in that job handles cleanup of this file.
-      tempdir = Rails.root.join("tmp")      
+      if Rails.env.production?
+        tempdir = "/tmp/DRStmp"
+      else
+        tempdir = Rails.root.join("tmp")
+      end
       uniq_hsh = Digest::MD5.hexdigest("#{file.original_filename}")[0,2]
       new_path = tempdir.join("#{Time.now.to_i.to_s}-#{uniq_hsh}")
       FileUtils.mv(file.tempfile.path, new_path.to_s)
