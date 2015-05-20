1a2f2414929db9e2fe62dcea15a62e4d26d351e8
David Cliff <d.cliff@neu.edu>
2015-05-20 11:34:05 -0400

    Updating impressions to take user_agent and default to public false

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -1,9 +1,9 @@
 class Impression < ActiveRecord::Base
-  attr_accessible :pid, :session_id, :action, :ip_address, :referrer, :status
+  attr_accessible :pid, :session_id, :action, :ip_address, :referrer, :status, :user_agent, :public
 
   # Ensure that all required fields are present
   validates :pid, :session_id, :action, :ip_address, presence: true
-  validates :referrer, :status, presence: true
+  validates :referrer, :status, :user_agent, presence: true
 
   # Store all IP addresses the first time they read or download an item
   validates_uniqueness_of :session_id, :scope => [:pid, :action]
