3487d524e490de9657ea5d7945fb2fff0a37c920
David Cliff <d.cliff@neu.edu>
2015-05-20 16:58:39 -0400

    Ok, this should suffice

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -103,8 +103,7 @@ before 'deploy:restart_workers', 'rvm1:hook'
 before 'deploy:starting', 'deploy:stop_httpd'
 before 'deploy:starting', 'deploy:update_clamav'
 
-before 'deploy:bundle', 'deploy:nokogiri'
-
+after 'deploy:updating', 'deploy:nokogiri'
 after 'deploy:updating', 'deploy:copy_rvmrc_file'
 after 'deploy:updating', 'deploy:trust_rvmrc'
 after 'deploy:updating', 'bundler:install'
