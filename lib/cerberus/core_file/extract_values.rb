6a450fba3931c278d12349d728b0bf943afa76d9
David Cliff <d.cliff@neu.edu>
2015-06-02 16:03:42 -0400

    Better caching

--- lib/cerberus/core_file/extract_values.rb
+++ lib/cerberus/core_file/extract_values.rb
@@ -4,14 +4,16 @@ module Cerberus
 
       def to_hash
         @core_doc = SolrDocument.new(self.to_solr)
-        mods_json = fetch_mods_json
-        result_hsh = Hash.new
-        result_hsh["pid"] = self.pid
-        result_hsh["thumbnails"] = @core_doc.thumbnail_list.map { |url_string| "#{root_path(:only_path => false)}#{url_string.sub!(/^\//, '')}"}
-        result_hsh["canonical_object"] = @core_doc.canonical_object.map { |doc| doc_to_url(doc) }
-        result_hsh["content_objects"] = @core_doc.content_objects.map { |doc| doc_to_url(doc) }
-        result_hsh["mods"] = JSON.parse(mods_json)
-        return result_hsh
+        Rails.cache.fetch("/api/#{@core_doc.pid}-#{@core_doc.updated_at}", :expires_in => 12.hours) do
+          mods_json = fetch_mods_json
+          result_hsh = Hash.new
+          result_hsh["pid"] = self.pid
+          result_hsh["thumbnails"] = @core_doc.thumbnail_list.map { |url_string| "#{root_path(:only_path => false)}#{url_string.sub!(/^\//, '')}"}
+          result_hsh["canonical_object"] = @core_doc.canonical_object.map { |doc| doc_to_url(doc) }
+          result_hsh["content_objects"] = @core_doc.content_objects.map { |doc| doc_to_url(doc) }
+          result_hsh["mods"] = JSON.parse(mods_json)
+          result_hsh
+        end
       end
 
 
@@ -21,9 +23,7 @@ module Cerberus
         end
 
         def fetch_mods_json
-          Rails.cache.fetch("/mods/json/#{@core_doc.pid}-#{@core_doc.updated_at}", :expires_in => 12.hours) do
-            CoreFilesController.new.render_mods_display(self).to_json
-          end
+          CoreFilesController.new.render_mods_display(self).to_json
         end
     end
   end
