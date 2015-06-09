abc07a907b2b20d6c90e6bc07f060ec088750907
David Cliff <d.cliff@neu.edu>
2015-06-09 14:59:35 -0400

    Adding spec test for api and private files

--- spec/controllers/api/core_files_controller_spec.rb
+++ spec/controllers/api/core_files_controller_spec.rb
@@ -2,13 +2,26 @@ require 'spec_helper'
 
 describe Api::V1::CoreFilesController, :type => :controller do
 
+  let(:file) { FactoryGirl.create(:bills_private_file) }
+
   describe "GET #show" do
-    it "provides an error indicating that an id is required" do
+    it "gives an error indicating if no id given" do
       @expected = {
         :error  => "An id is required for this action."
       }.to_json
       get :show, :id => ""
       response.body.should == @expected
+    end
+
+    it "gives an error if the file isn't public" do
+      @expected = {
+        :error  => "The item you've requested is unavailable."
+      }.to_json
+      get :show, :id => file.pid
+      response.body.should == @expected
+    end
+
+    it "gives an error if the file is embargoed" do
     end
   end
 
