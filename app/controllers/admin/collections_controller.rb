9a2b31060d3e4000e4af2336012507842b1eac91
Eli Zoller <zoller.eli@gmail.com>
2015-06-26 13:36:15 -0400

    changing admin views to just show tombstoned collections or corefiles instead of all tombstoned objects

--- app/controllers/admin/collections_controller.rb
+++ app/controllers/admin/collections_controller.rb
@@ -77,9 +77,7 @@ class Admin::CollectionsController < AdminController
 
     def limit_to_tombstoned(solr_parameters, user_parameters)
       solr_parameters[:fq] ||= []
-      solr_parameters[:fq] << "tombstoned_ssi:\"true\""
-      solr_parameters[:fq] << "active_fedora_model_ssi:\"Collection\""
-
+      solr_parameters[:fq] << "tombstoned_ssi:\"true\" AND active_fedora_model_ssi:\"Collection\""
     end
 
 end
