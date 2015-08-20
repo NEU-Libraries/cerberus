0fc84771e5d7f4327c798f4640d3f4d615af0e62
Eli Zoller <zoller.eli@gmail.com>
2015-08-20 10:31:30 -0400

    fix permissions for coe and cps loader - i had it right before

--- app/controllers/loaders/coe_loads_controller.rb
+++ app/controllers/loaders/coe_loads_controller.rb
@@ -7,7 +7,7 @@ class Loaders::CoeLoadsController < Loaders::LoadsController
   end
 
   def create
-    permissions = {"CoreFile" => {"read"  => ["northeastern:drs:all"], "edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageThumbnailFile" => {"read"  => ["northeastern:drs:all"], "edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageSmallFile" => {"read"  => ["northeastern:drs:all"], "edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageLargeFile" => {"read"  => ["northeastern:drs:all"], "edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageMasterFile" => {"read"  => ["northeastern:drs:all"], "edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}}
+    permissions = {"CoreFile" => {"edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageThumbnailFile" => {"edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageSmallFile" => {"edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageLargeFile" => {"edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}, "ImageMasterFile" => {"edit" => ["northeastern:drs:college_of_engineering:deans_office", "northeastern:drs:repository:staff"]}}
     process_create(permissions, t('drs.loaders.coe.short_name'), "CoeLoadsController")
   end
 
