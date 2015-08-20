6051ee114cd94875e7a86a9d88f1eb588da33908
Eli Zoller <zoller.eli@gmail.com>
2015-08-20 10:01:35 -0400

    updating process zip job spec for permissions structure change

--- spec/jobs/process_zip_job_spec.rb
+++ spec/jobs/process_zip_job_spec.rb
@@ -13,7 +13,7 @@ describe ProcessZipJob, unless: $in_travis do
       parent = FactoryGirl.create(:root_collection).pid
       copyright = "Copyright statement"
       @user = FactoryGirl.create(:user)
-      permissions = {"edit" => ["northeastern:drs:repository:staff"]}
+      permissions = {"CoreFile" => {"read"  => ["northeastern:drs:all"], "edit" => ["northeastern:drs:repository:staff"]}, "ImageThumbnailFile" => {"read"  => ["northeastern:drs:all"], "edit" => ["northeastern:drs:repository:staff"]}, "ImageSmallFile" => {"read"  => ["northeastern:drs:repository:staff"], "edit" => ["northeastern:drs:repository:staff"]}, "ImageLargeFile" => {"read"  => ["northeastern:drs:repository:staff"], "edit" => ["northeastern:drs:repository:staff"]}, "ImageMasterFile" => {"read"  => ["northeastern:drs:repository:staff"], "edit" => ["northeastern:drs:repository:staff"]}}
       ProcessZipJob.new(@loader_name, new_file.to_s, parent, copyright, @user, permissions).run
     end
 
