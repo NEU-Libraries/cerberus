81e8de0af00b8cf46e7953ae6ea5af4caab3eab7
Eli Zoller <zoller.eli@gmail.com>
2015-08-24 10:04:57 -0400

    trying again with travis - this time letting miniexiftool do its own thing

--- app/jobs/image_processing_job.rb
+++ app/jobs/image_processing_job.rb
@@ -21,7 +21,11 @@ class ImageProcessingJob
     # if theres an exception, log details to image_report
     require 'fileutils'
     require 'mini_exiftool'
-    MiniExiftool.command = '/opt/exiftool/exiftool'
+    if !ENV['TRAVIS'].nil? && ENV['TRAVIS'] == 'true'
+      # MiniExiftool.command = '/usr/bin/exiftool/exiftool'
+    else
+      MiniExiftool.command = '/opt/exiftool/exiftool'
+    end
     job_id = "#{Time.now.to_i}-loader-image"
     FileUtils.mkdir_p "#{Rails.root}/log/#{job_id}"
     failed_pids_log = Logger.new("#{Rails.root}/log/#{job_id}/loader-image-process-job.log")
