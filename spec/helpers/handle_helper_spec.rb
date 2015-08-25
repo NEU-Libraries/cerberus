157c749e1c6a57f8e5b48d9dd4cfc4cea910a215
Eli Zoller <zoller.eli@gmail.com>
2015-08-25 12:43:39 -0400

    another pass and travis yml

--- spec/helpers/handle_helper_spec.rb
+++ spec/helpers/handle_helper_spec.rb
@@ -5,7 +5,7 @@ describe HandleHelper do
   let(:bills_file) { FactoryGirl.create(:bills_complete_file) }
   before :all do
     `mysql -u "#{ENV["HANDLE_USERNAME"]}" < "#{Rails.root}"/spec/fixtures/files/handlesTEST.sql`
-    @client = Mysql2::Client.new(:host => "#{ENV["HANDLE_HOST"]}", :username => "#{ENV["HANDLE_USERNAME"]}", :password => "#{ENV["HANDLE_PASSWORD"]}", :database => "#{ENV["HANDLE_DATABASE"]}")
+    @client = Mysql2::Client.new(:host => "#{ENV["HANDLE_TEST_HOST"]}", :username => "#{ENV["HANDLE_TEST_USERNAME"]}", :password => "#{ENV["HANDLE_TEST_PASSWORD"]}", :database => "#{ENV["HANDLE_TEST_DATABASE"]}")
   end
 
   it "makes a handle" do
