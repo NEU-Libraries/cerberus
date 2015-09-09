451da830316c933f9a0ba846914d1350bef0c5b1
David Cliff <d.cliff@neu.edu>
2015-09-09 12:59:53 -0400

    Fleshing out page_file model

--- app/models/content_types/page_file.rb
+++ app/models/content_types/page_file.rb
@@ -1,3 +1,19 @@
 class PageFile < ImageThumbnailFile
   include Cerberus::ContentFile
+
+  def ordinal_value=(ord_val)
+    self.properties.ordinal_value = date_val
+  end
+
+  def ordinal_value
+    self.properties.ordinal_value.first
+  end
+
+  def ordinal_last=(ord_last)
+    self.properties.ordinal_last = ord_last
+  end
+
+  def ordinal_last
+    return self.properties.ordinal_last.first == 'true'
+  end
 end
