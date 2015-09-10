903340ed39cca889896b02b8e631c1c658a43f8d
David Cliff <d.cliff@neu.edu>
2015-09-10 14:11:10 -0400

    Minor refactoring

--- app/controllers/loaders/multipage_loads_controller.rb
+++ app/controllers/loaders/multipage_loads_controller.rb
@@ -1,9 +1,9 @@
-class Loaders::MarcomLoadsController < Loaders::LoadsController
+class Loaders::MultipageLoadsController < Loaders::LoadsController
   before_filter :verify_group
 
   def new
     parent = "neu:6240"
-    process_new(parent, t('drs.loaders.marcom.short_name'))
+    process_new(parent, t('drs.loaders.multipage.short_name'))
   end
 
   def create
@@ -15,6 +15,6 @@ class Loaders::MarcomLoadsController < Loaders::LoadsController
 
     def verify_group
       redirect_to new_user_session_path if current_user.nil?
-      redirect_to root_path unless current_user.marcom_loader?
+      redirect_to root_path unless current_user.multipage_loader?
     end
 end
