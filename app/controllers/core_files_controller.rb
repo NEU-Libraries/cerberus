a4ce1a2a6587d88a70652db9eb2864bf951a82bd
David Cliff <d.cliff@neu.edu>
2015-09-17 16:20:36 -0400

    Fixes #820

--- app/controllers/core_files_controller.rb
+++ app/controllers/core_files_controller.rb
@@ -387,7 +387,7 @@ class CoreFilesController < ApplicationController
 
     def fetch_mods
       Rails.cache.fetch("/mods/#{@core_file.pid}-#{@core_file.updated_at}", :expires_in => 12.hours) do
-        Sanitize.clean(Kramdown::Document.new(render_mods_display(CoreFile.find(@core_file.pid))).to_html, :elements => ['sup', 'sub', 'dt', 'dd', 'br', 'a']).html_safe
+        Sanitize.clean(Kramdown::Document.new(render_mods_display(CoreFile.find(@core_file.pid))).to_html, :elements => ['sup', 'sub', 'dt', 'dd', 'br', 'a'], :attributes => {'a' => ['href']}).html_safe
       end
     end
 
