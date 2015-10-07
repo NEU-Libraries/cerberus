234d65815530e2d35bd042cb239f517b4c52c171
David Cliff <d.cliff@neu.edu>
2015-10-07 13:49:19 -0400

    Accomadate for non standard thumbs

--- lib/cerberus/downloads_controller_behavior.rb
+++ lib/cerberus/downloads_controller_behavior.rb
@@ -15,7 +15,8 @@ module Cerberus
     # overriding hydra-head 6.3.3
     def send_content(asset)
       # Fuzzy thumbnails with send_file for some reason...small kludge. Everything else, don't use Hydra
-      if asset.class != ImageThumbnailFile
+      # Collections and others can have non-standard thumbnails - inlcuding a check to see if it's a "true" content type
+      if asset.class != ImageThumbnailFile && asset.datastreams.keys.include? "content"
         file_name = "neu_#{asset.pid.split(":").last}.#{extract_extension(asset.properties.mime_type.first)}"
         send_file asset.fedora_file_path, :filename =>  file_name, :type => asset.mime_type || extract_mime_type(asset.fedora_file_path), :disposition => 'inline'
       else
