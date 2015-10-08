57a6dc213aa6384af2a4f6727181388ebb60c280
Eli Zoller <zoller.eli@gmail.com>
2015-10-08 15:29:03 -0400

    update spec tests for new mailer

--- spec/mailers/metadata_mailer_spec.rb
+++ spec/mailers/metadata_mailer_spec.rb
@@ -21,4 +21,23 @@ describe MetadataMailer do
     end
 
   end
+
+  describe "daily_nonfeatured_alert_email" do
+    before :each do
+      FactoryGirl.create_list(:collection_alert, 2)
+    end
+    let(:mail){MetadataMailer.daily_nonfeatured_alert_email}
+
+    it "purges sent emails" do
+      UploadAlert.withheld_collections(:create).size.should == 2
+      mail
+      UploadAlert.withheld_collections(:create).size.should == 0
+    end
+
+    it "should have a subject with a correct item count" do
+      mail.deliver!
+      ActionMailer::Base.deliveries.first.subject.should == "Daily Non-Featured Content and Collection Uploads and Updates - 2 items"
+    end
+
+  end
 end
