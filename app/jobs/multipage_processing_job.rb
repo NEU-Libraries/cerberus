4b2ae1a9ad0723b43436d4576589b569fd4a1c01
David Cliff <d.cliff@neu.edu>
2015-10-30 16:29:35 -0400

    Stripping down, for now

--- app/jobs/multipage_processing_job.rb
+++ app/jobs/multipage_processing_job.rb
@@ -1,33 +1,19 @@
 class MultipageProcessingJob
-  attr_accessor :file_values, :core_file, :copyright, :report_id, :permissions, :client
-  include MimeHelper
-  include HandleHelper
+  attr_accessor :file_values, :core_file
 
   def queue_name
     :loader_multipage_processing
   end
 
-  def initialize(file_values, core_file, copyright, report_id, permissions=[], client=nil)
+  def initialize(file_values, core_file)
     self.file_values = file_values
     self.core_file = core_file
-    self.copyright = copyright
-    self.report_id = report_id
-    self.permissions = permissions
-    self.client = client
   end
 
   def run
-    # extract metadata from iptc
-    # if theres an exception, log details to image_report
-    require 'fileutils'
-    require 'mini_exiftool'
-    MiniExiftool.command = "#{Cerberus::Application.config.minitool_path}"
-    job_id = "#{Time.now.to_i}-loader-multipage"
-    FileUtils.mkdir_p "#{Rails.root}/log/#{job_id}"
-    failed_pids_log = Logger.new("#{Rails.root}/log/#{job_id}/loader-multipage-process-job.log")
-    load_report = Loaders::LoadReport.find(report_id)
     begin
-      
+      # Make PageFile objects
+      # create_all_thumbnail_sizes(master_file_path, thumb_pid)
     rescue Exception => error
       # TODO: fill in
     end
