3de895e8f8d297a460589c2b1adc3a6d4613c544
David Cliff <d.cliff@neu.edu>
2015-11-04 17:59:18 -0500

    Proper value, instead of commenting out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Cerberus::Application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
-  # config.active_support.deprecation = :notify
+  config.active_support.deprecation = :silence
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
