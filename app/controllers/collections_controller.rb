7ecc54bb3e95f42031b5d31dc11150c1336be631
Eli Zoller <zoller.eli@gmail.com>
2015-11-20 15:42:20 -0500

    updating functionality for recent deposits and author lists and their links and minor styling updates

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -235,21 +235,48 @@ class CollectionsController < ApplicationController
   end
 
   def recent_deposits
+    @set = fetch_solr_document
+    @page_title = "#{@set.title} Recent Deposits"
     self.solr_search_params_logic += [:limit_to_core_files]
     params[:limit] = 10
     params[:sort] = "#{Solrizer.solr_name('system_create', :stored_sortable, type: :date)} desc"
     (@response, @recent_deposits) = get_search_results
-    render partial:"/shared/sets/recent_deposits"
+    if @response.response['numFound'] > 0
+      respond_to do |format|
+        format.html { render 'shared/sets/show' }
+      end
+    else
+      flash[:notice] = "There are no recent deposits"
+      redirect_to @set and return
+    end
   end
 
   def author_list
     @set = fetch_solr_document
     @page_title = "#{@set.title} Author List"
-    # @pagination = get_facet_pagination(params[:solr_field], params)
     self.solr_search_params_logic += [:limit_to_scope]
 
     (@response, @document_list) = get_search_results
-    render 'shared/sets/author_list', locals:{sort_value:sort_value}
+    # @pagination = paginate_params(@response)
+    solr_fname = "creator_sim"
+    @display_facet = @response.facets.detect {|f| f.name == solr_fname}
+    facet_count = @display_facet.items.length
+    puts "facet count is #{@display_facet.items.length}"
+    if facet_count > 0
+      render 'shared/sets/author_list', locals:{sort_value:sort_value, solr_fname:solr_fname}
+    else
+      flash[:notice] = "There are no authors"
+      redirect_to @set
+    end
+  end
+
+  def title_list
+    @set = fetch_solr_document
+    @page_title = "#{@set.title} Title List"
+    self.solr_search_params_logic += [:limit_to_scope]
+
+    (@response, @document_list) = get_search_results
+    render 'shared/sets/title_list', locals:{sort_value:sort_value}
   end
 
   protected
@@ -287,6 +314,25 @@ class CollectionsController < ApplicationController
 
       fq = query.join(" OR ")
 
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << fq
+    end
+
+    def limit_to_core_files(solr_parameters, user_parameters)
+      descendents = @set.combined_set_descendents
+
+      # Limit query to items that are set descendents
+      # or files off set descendents
+      query = descendents.map do |set|
+        p = set.pid
+        set = "is_member_of_ssim:\"info:fedora/#{p}\""
+      end
+
+      # Ensure files directly on scoping collection are added in
+      # as well
+      query << "is_member_of_ssim:\"info:fedora/#{@set.pid}\""
+      fq = query.join(" OR ")
+      fq = "(#{fq}) AND active_fedora_model_ssi:\"CoreFile\""
       solr_parameters[:fq] ||= []
       solr_parameters[:fq] << fq
     end
