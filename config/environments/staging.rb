e8734752545d22bbbe67a6d08b3dda7815d2072b
David Cliff <d.cliff@neu.edu>
2015-12-02 21:37:39 -0500

    Allow staging to emulate prod caching

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,14 +6,14 @@ Cerberus::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -29,7 +29,7 @@ Cerberus::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Expands the lines which load the assets
   config.assets.debug = true
