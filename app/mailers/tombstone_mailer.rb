0539f0139a2bdfe7227b9cc85e8e67b2ee6c3876
Eli Zoller <zoller.eli@gmail.com>
2015-12-10 07:35:15 -0500

    fixing delete functionality- when a user has edit permissions but is not depositor they can request deletion from sarah

--- app/mailers/tombstone_mailer.rb
+++ app/mailers/tombstone_mailer.rb
@@ -7,10 +7,15 @@ class TombstoneMailer < ActionMailer::Base
     @reason = reason
     @user= user
     @type = object.class
+    @depositor = object.depositor
+    @depositor_name = get_depositor_name(object)
+    @depositor_email = get_depositor_email(object)
     if @type == Collection
       @object_url = collection_url(@pid)
     elsif @type == CoreFile
       @object_url = core_file_url(@pid)
+    elsif @type == Compilation
+      @object_url = compilation_url(@pid)
     end
     mail(to: pick_receiver,
          subject: "[cerberus] User Requested #{@type} Deletion",
@@ -29,5 +34,27 @@ class TombstoneMailer < ActionMailer::Base
           git_config['user']['email']
         end
       end
+    end
+
+    def get_depositor_email(object)
+      if !object.depositor.blank?
+        user = User.find_by_nuid(object.depositor)
+        if !user.email.blank?
+          if !user.email.include? user.nuid
+            return user.email
+          end
+        end
+      end
+      return false
+    end
+
+    def get_depositor_name(object)
+      if !object.depositor.blank?
+        user = User.find_by_nuid(object.depositor)
+        if !user.full_name.blank?
+          return user.full_name
+        end
+      end
+      return false
     end
 end
