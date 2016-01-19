1456dbf4027fb1f20b46d0993a2bbdf03be42a6a
David Cliff <d.cliff@neu.edu>
2016-01-19 11:40:08 -0500

    Use time restriction, instead of session - update spec to match

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -5,6 +5,26 @@ class Impression < ActiveRecord::Base
   validates :pid, :session_id, :action, :ip_address, presence: true
   validates :referrer, :status, :user_agent, presence: true
 
-  # Store all IP addresses the first time they read or download an item
-  validates_uniqueness_of :session_id, :scope => [:pid, :action]
+  # Changing to a check if ip, pid, action have been done in the last hour
+  # session lasts until deployment - lowering stats in a hamfisted way
+  validate :throttle_creation, on: :create
+
+  def throttle_creation
+    # We implement this to prevent outliers - bots that we're unaware of,
+    # hyperactive refreshing of a page etc.
+    results = Impression.where("pid = ? AND action = ? AND ip_address = ?", self.pid, self.action, self.ip_address)
+    if results.count > 0
+      last_hour = false
+
+      results.each do |result|
+        if result.created_at > 1.hour.ago
+          last_hour = true
+        end
+      end
+
+      if last_hour
+        errors.add(:ip_address, "This action for this pid was done by #{self.ip_address} within the last hour")
+      end
+    end
+  end
 end
