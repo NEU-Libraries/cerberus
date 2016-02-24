024f72c2a2322e4dc7e40aa7edd10d942c190ac4
David Cliff <d.cliff@neu.edu>
2016-02-24 13:19:42 -0500

    Aggregate done for cols coms and sets

--- lib/cerberus/impression_count.rb
+++ lib/cerberus/impression_count.rb
@@ -6,16 +6,40 @@ module Cerberus::ImpressionCount
     # Impressions table.
     # * Unique as determined by viewing session_id
     def impression_views
-      Impression.where("pid = ? AND action = ? AND public = ? AND status = 'COMPLETE'", self.pid, 'view', true).count
+      pids = []
+
+      if !self.klass.blank? && (self.klass == "Community" || self.klass == "Collection" || self.klass == "Set")
+        pids = self.all_descendent_files.map{|doc| doc.pid}
+      else
+        pids << self.pid
+      end
+
+      Impression.where("pid IN (?) AND action = ? AND public = ? AND status = 'COMPLETE'", pids, 'view', true).count
     end
 
     # Same as above, but with recorded download actions
     def impression_downloads
+      pids = []
+
+      if !self.klass.blank? && (self.klass == "Community" || self.klass == "Collection" || self.klass == "Set")
+        pids = self.all_descendent_files.map{|doc| doc.pid}
+      else
+        pids << self.pid
+      end
+
       Impression.where("pid = ? AND action = ? AND public = ? AND status = 'COMPLETE'", self.pid, 'download', true).count
     end
 
     # Same as above, but with recorded download actions
     def impression_streams
+      pids = []
+
+      if !self.klass.blank? && (self.klass == "Community" || self.klass == "Collection" || self.klass == "Set")
+        pids = self.all_descendent_files.map{|doc| doc.pid}
+      else
+        pids << self.pid
+      end
+
       Impression.where("pid = ? AND action = ? AND public = ? AND status = 'COMPLETE'", self.pid, 'stream', true).count
     end
   end
