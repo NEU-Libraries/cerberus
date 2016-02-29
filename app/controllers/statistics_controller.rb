3a9b7b2a66985c367988f5dce5bf186bb61e6d0b
David Cliff <d.cliff@neu.edu>
2016-02-29 00:39:37 -0500

    Ajax-ify stats for set/collections

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,20 +1,13 @@
 class StatisticsController < ApplicationController
-  def get_views
-    @count = total_count
+  def all_counts
+    doc = SolrDocument.new(ActiveFedora::Base.find(params[:id], cast: true).to_solr)
+
+    view_count = doc.impression_views
+    download_count = doc.impression_downloads
+    stream_count = doc.impression_streams
+
     respond_to do |format|
-      format.js { render "count", locals:{count:@count}}
-    end
-  end
-  def get_downloads
-    @count = total_count
-    respond_to do |format|
-      format.js { render "count", locals:{count:@count}}
-    end
-  end
-  def get_streams
-    @count = total_count
-    respond_to do |format|
-      format.js { render "count", locals:{count:@count}}
+      format.js { render "shared/statistics/count", locals:{view_count: view_count, download_count: download_count, stream_count: stream_count}}
     end
   end
 end
