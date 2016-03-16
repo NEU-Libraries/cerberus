b4851c9298a32e2f39060b740bfc350fb6b520de
Eli Zoller <zoller.eli@gmail.com>
2016-03-16 14:52:54 -0400

    adding job to add technical reports collection to employee objects - largely a copy/paste from the monographs refactor job

---
+++ app/jobs/technical_reports_refactor_job.rb
@@ -0,0 +1,67 @@
+class TechnicalReportsRefactorJob
+
+  def queue_name
+    :technical_reports_refactor
+  end
+
+  def run
+    # Find all employees that don't have a technical_reports smart collection
+    Employee.find(:all).each do |emp|
+      has_technical_reports = false
+
+      parent = emp.user_root_collection
+      sc = emp.smart_collections
+
+      sc.each do |col|
+        if col.smart_collection_type == "Technical Reports"
+          has_technical_reports = true
+        end
+      end
+
+      if !has_technical_reports
+        create_personal_collection("Technical Reports", emp, parent)
+
+        # Tag the employee as completed and resave to Fedora.
+        emp.employee_is_complete
+        emp.save!
+      end
+    end
+  end
+
+  private
+    def create_personal_collection(title, employee, parent = nil)
+      if title == employee.name
+        smart_collection_type = "User Root"
+        desc = "#{self.name}'s root collection"
+      else
+        smart_collection_type = "#{title}"
+        desc = "#{title} deposited by, or on behalf of, #{employee.pretty_employee_name}"
+      end
+
+      attrs = {
+                title: title,
+                depositor: employee.nuid,
+                parent: parent,
+                user_parent: employee,
+                description: desc,
+                smart_collection_type: smart_collection_type,
+                mass_permissions: 'public',
+              }
+
+      personal_collection = Collection.new(attrs)
+
+      saves = 0
+
+      # Attempt to save the newly created collection
+      begin
+        return personal_collection if personal_collection.save!
+      rescue RSolr::Error::Http => error
+        puts "Save failed"
+        saves += 1
+        logger.warn "EmployeeCreateJob caught RSOLR error on creation of #{employee.nuid}'s #{title} personal_collection: #{error.inspect}"
+        raise error if saves >= 3
+        sleep 0.01
+        retry
+      end
+    end
+end
