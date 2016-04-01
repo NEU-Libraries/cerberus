c765209840c5dade1d60fee530eb7282cb7c9fc2
Eli Zoller <zoller.eli@gmail.com>
2016-04-01 14:26:43 -0400

    fix for user root issue for increasing parent size as well

--- app/jobs/aggregated_statistics_job.rb
+++ app/jobs/aggregated_statistics_job.rb
@@ -194,8 +194,15 @@ class AggregatedStatisticsJob
     end
 
     begin
-      parent = SolrDocument.new ActiveFedora::SolrService.query("id:\"#{set.parent}\"").first
-      increase_parent_size(parent.pid, size)
+      if set.parent
+        parent = SolrDocument.new ActiveFedora::SolrService.query("id:\"#{set.parent}\"").first
+        increase_parent_size(parent.pid, size)
+      elsif set.smart_collection_type == "User Root" #this lets us go past a user root collection and into the commmunities they belong to
+        emp = Employee.find("#{set.is_member_of.split("/").last}")
+        emp.communities.each do |c|
+          increase_parent_size(c.pid, size)
+        end
+      end
     rescue
       #
     end
