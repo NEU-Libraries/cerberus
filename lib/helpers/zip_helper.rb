8220c5b9dc42f84effdd934c7297828d723e833e
David Cliff <d.cliff@neu.edu>
2016-04-05 15:53:49 -0400

    Helper module made, just need to refactor into loaders

---
+++ lib/helpers/zip_helper.rb
@@ -0,0 +1,43 @@
+module ZipHelper
+
+  def safe_unzip(zip_file_path, output_dir)
+    # For certain zip files, the gem we use struggles to open and extract their contents
+    # It seems related to OSX's handling of large zip files, and their zip64 structure
+    # Further reading can be had here - https://bitinn.net/10716/
+
+    # As a result, in the event that the file doesn't open, or opens but only makes an empty
+    # dir, we fall back and shell out to standard unzip at the linux level - this kludge seems
+    # to be a reliable secondary step
+
+    # Making a generalized helper method to re-use throughout the various places we handle
+    # zips in the codebase
+
+    # Make the output path if it doesn't already exist
+    FileUtils.mkdir(output_dir) unless File.exists? output_dir
+
+    # Attempts to extract all files with the gem, our ideal setup
+    begin
+      Zip::File.open(zip_file_path) do |zipfile|
+        zipfile.each do |f|
+          if !f.directory? && File.basename(f.name)[0] != "." # Don't extract directories or mac specific files
+            fpath = File.join(dir_path, f.name)
+            FileUtils.mkdir_p(File.dirname(fpath))
+            zipfile.extract(f, fpath) unless File.exist?(fpath)
+          end
+        end
+      end
+    rescue Exception => error
+      # we'll check for empty dir afterwards
+    end
+
+    # Empty dir?
+    if Dir[output_dir].empty?
+      # Standard gem zip extraction didn't work, let's shell out to unzip
+      `unzip #{zip_file_path} -d #{output_dir}`
+    end
+
+    # returns file list with absolute paths
+    return Dir.glob("#{output_dir}/*")
+  end
+
+end
