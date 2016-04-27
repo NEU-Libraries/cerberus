ddca2635aaf39a3153c2c0910878249fe8f85fd6
Eli Zoller <zoller.eli@gmail.com>
2016-04-27 10:07:24 -0400

    increasing length of size_increase field

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160404172259) do
+ActiveRecord::Schema.define(:version => 20160427134358) do
 
   create_table "aggregated_statistics", :force => true do |t|
     t.string   "object_type"
     t.string   "pid"
-    t.integer  "views",          :default => 0
-    t.integer  "downloads",      :default => 0
-    t.integer  "streams",        :default => 0
-    t.integer  "loader_uploads", :default => 0
-    t.integer  "user_uploads",   :default => 0
-    t.integer  "form_edits",     :default => 0
-    t.integer  "xml_edits",      :default => 0
-    t.integer  "size_increase",  :default => 0
+    t.integer  "views",                       :default => 0
+    t.integer  "downloads",                   :default => 0
+    t.integer  "streams",                     :default => 0
+    t.integer  "loader_uploads",              :default => 0
+    t.integer  "user_uploads",                :default => 0
+    t.integer  "form_edits",                  :default => 0
+    t.integer  "xml_edits",                   :default => 0
+    t.integer  "size_increase",  :limit => 8, :default => 0
     t.datetime "processed_at"
   end
 
