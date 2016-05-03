10db98538ff2936aaf7a15ca8d039289cc2169f4
Eli Zoller <zoller.eli@gmail.com>
2016-05-03 15:32:22 -0400

    add view for preview with comparing two core files MODS records, hard coded for now and at a separate path, will probably want to use same path with preview in the future

--- app/controllers/loaders/mods_spreadsheet_loads_controller.rb
+++ app/controllers/loaders/mods_spreadsheet_loads_controller.rb
@@ -35,10 +35,32 @@ class Loaders::ModsSpreadsheetLoadsController < Loaders::LoadsController
     render 'loaders/preview'
   end
 
+  def preview_compare
+    @core_file = CoreFile.first #TODO: hook this in the with the job, just hardcoded for now
+    old_core = CoreFile.all[2] #TODO: hook this is in with the job
+    @diff = mods_diff(@core_file, old_core)
+    @diff_css = Diffy::CSS
+    @mods_html = render_mods_display(CoreFile.find(@core_file.pid)).to_html.html_safe
+    @report = Loaders::LoadReport.find(params[:id])
+    @user = User.find_by_nuid(@report.nuid)
+    @collection_title = ActiveFedora::SolrService.query("id:\"#{@report.collection}\"", :fl=>"title_tesim")
+    @collection_title = @collection_title[0]['title_tesim'][0]
+    if @collection_title.blank?
+      @collection_title = "N/A"
+    end
+    render 'loaders/preview'
+  end
+
   private
 
     def verify_group
       redirect_to new_user_session_path if current_user.nil?
       redirect_to root_path unless current_user.mods_spreadsheet_loader?
+    end
+
+    def mods_diff(core_file_a, core_file_b)
+      mods_a = Nokogiri::XML(core_file_a.mods.content).to_s
+      mods_b = Nokogiri::XML(core_file_b.mods.content).to_s
+      return Diffy::Diff.new(mods_a, mods_b, :include_plus_and_minus_in_html => true, :context => 1).to_s(:html).html_safe
     end
 end
