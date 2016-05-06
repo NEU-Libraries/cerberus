9e1d6fbb23d20e907214d2678e9063aa627d5bf0
Eli Zoller <zoller.eli@gmail.com>
2016-05-06 16:08:11 -0400

    updating specs

--- spec/jobs/content_attachment_job_spec.rb
+++ spec/jobs/content_attachment_job_spec.rb
@@ -1,30 +1,85 @@
 require 'spec_helper'
+include MimeHelper
+include ChecksumHelper
+include ActionDispatch::TestProcess
+include Cerberus::TempFileStorage
 
 describe ContentAttachmentJob do
   before(:each) do
-
+    @user = FactoryGirl.create(:admin)
+    @core_file = FactoryGirl.create(:complete_file, depositor:"000000000")
+    @core_file.canonical_class = "VideoFile"
+    @core_file.save!
+    @content_object = VideoMasterFile.new(pid: Cerberus::Noid.namespaceize(Cerberus::IdService.mint))
+    file = fixture_file_upload("/files/video.mp4")
+    new_path = move_file_to_tmp(file)
+    @content_object.tmp_path = new_path
+    @content_object.original_filename = file.original_filename
+    @content_object.save!
+    @permissions = {"identity"=>["northeastern:drs:repository:staff", "northeastern:drs:repository:loaders:emsa_emc"], "permission_type"=>["edit", "read"]}
+    @mass_permissions = "public"
+    @job = ContentAttachmentJob.new(@core_file.pid, @content_object.tmp_path, @content_object.pid, @content_object.original_filename, @permissions, @mass_permissions)
+    @job.run
+    @core_file.reload
+    @content_object.reload
   end
 
   after(:each) do
     ActiveFedora::Base.destroy_all
+    User.destroy_all
   end
 
-  # ContentAttachmentJob.new(@core_file.pid, @content_object.tmp_path, @content_object.pid, @content_object.original_filename, true, params[:permissions])
-  # core_file, file_path, content_object, file_name, delete_file=true, permissions=nil
-  # it "gets core_file"
-  # it "gets content_object"
-  # it "adds the file the content_object"
-  # it "sets the core_record for the content_object"
-  # it "sets the title to the filename"
-  # it "sets the identifier to the pid"
-  # it "sets the depositor to core_record depositor"
-  # it "sets the proxy uploader"
-  # it "sets nil permissions to the core_record permissions"
-  # it "sets no nil permissions"
-  # it "sets original_filename"
-  # it "sets md5"
-  # it "sets mime_type"
-  # it "deletes cached content objects"
-  # it "deletes tmp file"
+  it "gets core_file" do
+    @job.core_file_pid.should == @core_file.pid
+    @job.core_record.should == @core_file
+  end
 
+  it "gets content_object" do
+    @job.content_object_pid.should == @content_object.pid
+    @job.content_object.should == @content_object
+  end
+
+  it "adds the file to the content_object" do
+    @content_object.content.should be_kind_of(FileContentDatastream)
+  end
+
+  it "sets the core_record for the content_object" do
+    @core_file.content_objects.should include(@content_object)
+  end
+
+  it "sets the title to the filename" do
+    @content_object.title.should == @content_object.original_filename
+  end
+
+  it "sets the identifier to the pid" do
+    @content_object.identifier.should == @content_object.pid
+  end
+
+  it "sets permissions" do
+    @content_object.permissions.should == [{:type=>"group", :access=>"read", :name=>"northeastern:drs:repository:loaders:emsa_emc"}, {:type=>"group", :access=>"read", :name=>"public"}, {:type=>"group", :access=>"edit", :name=>"northeastern:drs:repository:staff"}]
+  end
+
+  it "sets mass permissions" do
+    @content_object.mass_permissions.should == "public"
+  end
+
+  it "sets original_filename" do
+    @content_object.original_filename.should == @content_object.original_filename
+  end
+
+  it "sets md5" do
+    file = fixture_file_upload("/files/video.mp4")
+    new_path = copy_file_to_tmp(file)
+    @content_object.properties.md5_checksum.should == [new_checksum(new_path)]
+  end
+
+  it "sets mime_type" do
+    file = fixture_file_upload("/files/video.mp4")
+    new_path = copy_file_to_tmp(file)
+    @content_object.properties.mime_type.should == [extract_mime_type(new_path)]
+  end
+
+  it "deletes tmp file" do
+    # TODO
+  end
 end
