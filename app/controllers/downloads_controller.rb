90c3b2e43dd319558e9e3b9ceee655218f8a2acf
David Cliff <d.cliff@neu.edu>
2016-06-15 14:40:37 -0400

    Export, email and download working. Need to create the manifest spreadsheet

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -34,10 +34,6 @@ class DownloadsController < ApplicationController
     render_403 and return
   end
 
-  def mods_download
-    send_file "#{Rails.application.config.tmp_path}/mods/#{params[:session_id]}/mods_export.zip"
-  end
-
   def show
     if asset.class == ImageThumbnailFile && (Rails.env.staging? || Rails.env.production?)
       response.headers['Cache-Control'] = "public"
