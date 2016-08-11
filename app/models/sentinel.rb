783f89cd0f4c2de7b55e3f438f190d05d6ea2a2a
David Cliff <d.cliff@neu.edu>
2016-08-11 17:08:58 -0400

    Revert to erb so that we can exert javascript magic with loops and next

--- app/models/sentinel.rb
+++ app/models/sentinel.rb
@@ -1,3 +1,23 @@
 class Sentinel < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :set_pid, :permanent, :email, :audio_master, :audio, :image_master,
+                  :image_large, :image_medium, :image_small, :mspowerpoint, :msexcel,
+                  :msword, :page, :pdf, :text, :video_master, :video, :zip, :pid_list
+
+  serialize :audio_master, Hash
+  serialize :audio, Hash
+  serialize :image_master, Hash
+  serialize :image_large, Hash
+  serialize :image_medium, Hash
+  serialize :image_small, Hash
+  serialize :mspowerpoint, Hash
+  serialize :msexcel, Hash
+  serialize :msword, Hash
+  serialize :page, Hash
+  serialize :pdf, Hash
+  serialize :text, Hash
+  serialize :video_master, Hash
+  serialize :video, Hash
+  serialize :zip, Hash
+
+  serialize :pid_list, Array
 end
