8adac9e8d28d8fa5d9af26b7f43465e1c77bf4b9
David Cliff <d.cliff@neu.edu>
2016-09-15 15:21:47 -0400

    Adding route, static action

---
+++ app/mailers/large_download_mailer.rb
@@ -0,0 +1,14 @@
+class LargeDownloadMailer < ActionMailer::Base
+  default from: "notifier@repository.library.northeastern.edu"
+
+  def export_alert(pid, nuid, session_id)
+    user = !nuid.blank? ? User.find_by_nuid(nuid) : nil
+    @name = user.pretty_name || "No name set.  Uh oh!"
+    @nuid = nuid || "No nuid set.  Uh oh!"
+    @collection_title = ActiveFedora::Base.find("#{pid}", cast: true).title || "No collection title."
+    @file_url = large_download_url(id: pid, session_id: session_id)  || "No url set.  Uh oh!"
+    mail(to: user.email,
+         subject: "[DRS] Large download link",
+         content_type: "text/html")
+  end
+end
