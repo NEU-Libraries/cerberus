b75010d74cda35083f3b8e4acacb909f305db5e2
David Cliff <d.cliff@neu.edu>
2017-01-04 14:08:55 -0500

    Add barebones metadata to community and collection to start

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,11 @@
 class Collection < ActiveFedora::Base
   include Hydra::Works::CollectionBehavior
   belongs_to :community, class_name: 'Community', predicate: ActiveFedora::RDF::Fcrepo::RelsExt.isPartOf
+
+  property :title, predicate: ::RDF::Vocab::DC.title, multiple: false do |index|
+    index.as :stored_searchable
+  end
+  property :description, predicate: ::RDF::Vocab::DC.description, multiple: false do |index|
+    index.as :stored_searchable
+  end
 end
--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,10 @@
 class Community < ActiveFedora::Base
   has_many :collections
+
+  property :title, predicate: ::RDF::Vocab::DC.title, multiple: false do |index|
+    index.as :stored_searchable
+  end
+  property :description, predicate: ::RDF::Vocab::DC.description, multiple: false do |index|
+    index.as :stored_searchable
+  end
 end
