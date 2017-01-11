8e71dd0fb34b4d100ee25fb8982e7aab34a4b3c8
David Cliff <d.cliff@neu.edu>
2017-01-11 11:01:28 -0500

    Safe unzip only uses linux zip now. Hopefully works better than rubyzip.

--- lib/helpers/zip_helper.rb
+++ lib/helpers/zip_helper.rb
@@ -5,10 +5,6 @@ module ZipHelper
     # It seems related to OSX's handling of large zip files, and their zip64 structure
     # Further reading can be had here - https://bitinn.net/10716/
 
-    # As a result, in the event that the file doesn't open, or opens but only makes an empty
-    # dir, we fall back and shell out to standard unzip at the linux level - this kludge seems
-    # to be a reliable secondary step
-
     # Making a generalized helper method to re-use throughout the various places we handle
     # zips in the codebase
 
@@ -20,50 +16,32 @@ module ZipHelper
     # Make the output path if it doesn't already exist
     FileUtils.mkdir(output_dir) unless File.exists? output_dir
 
-    # Attempts to extract all files with the gem, our ideal setup
-    begin
-      Zip::File.open(zip_file_path) do |zipfile|
-        zipfile.each do |f|
-          if !f.directory? && File.basename(f.name)[0] != "." # Don't extract directories or mac specific files
-            if squash == true
-              # Legacy zip construction for certain loaders forces us to flatten internal
-              # structure, so to do this we give each file a unique name to avoid collision
-              uniq_hsh = Digest::MD5.hexdigest("#{f.name}")[0,2]
-              uniq_filename = "#{Time.now.to_f.to_s.gsub!('.','-')}-#{uniq_hsh}"
-              fpath = File.join(output_dir, uniq_filename) # Names file time and hash string
-              new_names << fpath
-              original_names << f.name
-            elsif f.name.to_s.include?("/") #this does the work if the files are nested within a directory
-              fpath = File.join(output_dir, File.basename(f.name))
-              zipfile.extract(f, fpath) unless File.exists?(fpath)
-            else
-              fpath = File.join(output_dir, f.name)
-            end
-
-            FileUtils.mkdir_p(File.dirname(fpath))
-            zipfile.extract(f, fpath) unless File.exist?(fpath)
-          end
-        end
-      end
-    rescue Exception => error
-      # we'll check for empty dir afterwards
-    end
-
-    # Empty dir?
-    if Dir[output_dir + "/*"].empty?
-      # Standard gem zip extraction didn't work, let's shell out to unzip
-      `unzip #{zip_file_path} -d #{output_dir}`
-    end
+    # Shell out to unzip
+    `unzip #{zip_file_path} -d #{output_dir}`
 
     # Ensure all files have ok permissions
     FileUtils.chmod_R(0777, "#{output_dir}")
 
     dir_list = Dir.glob("#{output_dir}/*")
 
+    Find.find(output_dir) do |path|
+      if squash
+        if !File.directory?(path) && File.basename(path)[0] != "."
+          uniq_hsh = Digest::MD5.hexdigest("#{path}")[0,2]
+          uniq_filename = "#{Time.now.to_f.to_s.gsub!('.','-')}-#{uniq_hsh}"
+          new_path = File.join(output_dir, uniq_filename)
+          FileUtils.mv(path, new_path)
+
+          new_names << new_path
+          original_names << path
+        end
+      end
+    end
+
     # Loop through and remove directories
     dir_list.each do |item|
       if File.directory?(item)
-        logger.info "its a directory"
+        logger.info "removing a directory in safe_unzip"
         FileUtils.rm_rf(item)
       end
     end
