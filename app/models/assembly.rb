ba107306d32e6d5dad7a9eab53719454e2cf00ff
David Cliff <d.cliff@neu.edu>
2017-02-01 10:55:11 -0500

    Hash instead of set to disambiguate collections from works

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -2,5 +2,5 @@ class Assembly < ApplicationRecord
   # wrapper object of a ruby Set of ids so we can bypass fedora and slow rdf
   # for compilations, which are often compared to and edited (and don't require
   # long term preservation)
-  serialize :id_list, Set
+  serialize :id_list, Hash
 end
