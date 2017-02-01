b4698c4897af0d58635c88b45919ffb525bcc5dc
David Cliff <d.cliff@neu.edu>
2017-02-01 14:12:51 -0500

    Stubbing out adding, removing, and querying of compilations

--- app/models/compilation.rb
+++ app/models/compilation.rb
@@ -12,4 +12,26 @@ class Compilation < ActiveFedora::Base
   def assembly
     return Assembly.find(self.assembly_id)
   end
+
+  def add(object_id)
+    # if collection
+    # if work
+  end
+
+  def remove(object_id)
+    # if collection
+    # if work
+  end
+
+  def contains?(id)
+    # find parent if work
+    # compare with assembly collections
+    # if work compare with assembly works
+  end
+
+  def intersects?(id_list)
+    # find collections in list
+    # find collections in assembly
+    # make sets and compare
+  end
 end
