41f8e701b0828a11e34d75469c4411f02b893d69
David Cliff <d.cliff@neu.edu>
2017-02-01 14:57:10 -0500

    Framing out the add method

--- app/models/assembly.rb
+++ app/models/assembly.rb
@@ -1,5 +1,5 @@
 class Assembly < ApplicationRecord
-  # wrapper object of a ruby Set of ids so we can bypass fedora and slow rdf
+  # wrapper object of a ruby Hash of ids so we can bypass fedora and slow rdf
   # for compilations, which are often compared to and edited (and don't require
   # long term preservation)
   serialize :id_list, Hash
--- app/models/compilation.rb
+++ app/models/compilation.rb
@@ -9,13 +9,39 @@ class Compilation < ActiveFedora::Base
     index.as :stored_searchable
   end
 
+  def initialize(attributes={})
+    super
+    if attributes.blank?
+      self.assembly_id = (Assembly.create).id
+    end
+  end
+
   def assembly
     return Assembly.find(self.assembly_id)
   end
 
+  def ids
+    return self.assembly.id_list
+  end
+
   def add(object_id)
-    # if collection
-    # if work
+    h = {}
+    a = self.assembly
+    old_list = a.id_list
+
+    if !self.contains? object_id
+      if ActiveFedora::SolrService.query("id:#{object_id}").first['has_model_ssim'] == ["Collection"]
+        h[object_id] = "collection"
+      elsif ActiveFedora::SolrService.query("id:#{object_id}").first['has_model_ssim'] == ["Hydra::Works::Work"]
+        h[object_id] = "work"
+      else
+        # raise error - only collections and works can be added to compilations
+      end
+      
+      new_list = a.id_list.merge h
+      a.id_list = new_list
+      a.save!
+    end
   end
 
   def remove(object_id)
@@ -27,6 +53,7 @@ class Compilation < ActiveFedora::Base
     # find parent if work
     # compare with assembly collections
     # if work compare with assembly works
+    return false
   end
 
   def intersects?(id_list)
