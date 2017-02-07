def60f4a361f3c50551b295aced3954af1e7e244
David Cliff <d.cliff@neu.edu>
2017-02-07 13:26:06 -0500

    Subsume blacklight layout into cerberus layout. Blacklight 7 has a better process https://github.com/projectblacklight/blacklight/pull/1506/files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   include Hydra::Controller::ControllerBehavior
-  layout 'blacklight'
+  # layout 'blacklight'
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
