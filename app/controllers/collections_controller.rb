bd99f8d69d4f3c645fcde476d012342f2219a399
David Cliff <d.cliff@neu.edu>
2017-02-09 13:05:29 -0500

    community and collection creation with new strong params req

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -4,5 +4,16 @@ class CollectionsController < ApplicationController
   end
 
   def create
+    if Collection.new(collection_params).save!
+      flash[:notice] = "Collection successfully created."
+      redirect_to root_path
+    else
+      flash.now.error = "Error occured creating collection."
+    end
   end
+
+  private
+    def collection_params
+      params.require(:collection).permit(:title, :description)
+    end
 end
--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -4,5 +4,16 @@ class CommunitiesController < ApplicationController
   end
 
   def create
+    if Community.new(community_params).save!
+      flash[:notice] = "Community successfully created."
+      redirect_to root_path
+    else
+      flash.now.error = "Error occured creating community."
+    end
   end
+
+  private
+    def community_params
+      params.require(:community).permit(:title, :description)
+    end
 end
