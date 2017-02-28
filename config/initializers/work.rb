a67e5099bdf05ada9c4e55e207fc24367e639568
David Cliff <d.cliff@northeastern.edu>
2017-02-28 10:15:36 -0500

    Better way of using generic work

--- config/initializers/work.rb
+++
@@ -1,8 +0,0 @@
-Hydra::Works::Work.include Mods
-Hydra::Works::Work.include Parentable
-Hydra::Works::Work.include Noidable
-Hydra::Works::Work.include Hydra::PCDM::ObjectBehavior
-Hydra::Works::Work.include Hydra::AccessControls::Permissions
-
-# Alias
-Work = Hydra::Works::Work
