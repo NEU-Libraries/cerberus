a2655a6657a1d83733d2f4a0811743c11f33824d
David Cliff <d.cliff@neu.edu>
2017-03-27 15:25:17 -0400

    Changing deploy priority

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -113,9 +113,9 @@ end
 before 'deploy:starting', 'deploy:stop_httpd'
 before 'deploy:starting', 'deploy:update_clamav'
 
-after 'deploy:updating', 'deploy:nokogiri'
 after 'deploy:updating', 'deploy:copy_rvmrc_file'
 after 'deploy:updating', 'deploy:trust_rvmrc'
+after 'deploy:updating', 'deploy:nokogiri'
 after 'deploy:updating', 'bundler:install'
 after 'deploy:updating', 'deploy:copy_yml_file'
 after 'deploy:updating', 'deploy:migrate'
