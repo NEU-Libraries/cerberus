4346ab058bfab48673088160fa814066ed3737d9
David Cliff <d.cliff@northeastern.edu>
2017-04-19 16:43:50 -0400

    Standard hydra download

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,8 @@ Rails.application.routes.draw do
     resources :works
   end
 
+  resources :downloads, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
