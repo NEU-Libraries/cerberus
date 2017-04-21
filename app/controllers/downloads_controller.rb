d619d3d16c05212df8f248876b732845befc0ae6
David Cliff <d.cliff@northeastern.edu>
2017-04-21 13:11:57 -0400

    Catch common scripting problem on downloads

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -12,8 +12,16 @@ class DownloadsController < ApplicationController
     # instead of sub_resource "datastreams", we have file sets with files attached
     # Fair to assume that asset_param_key will always be a file set id
     # and :file will always be the datastream label
-    file_reference = params[:file]
-    obj = ActiveFedora::Base.find(params[asset_param_key])
-    obj.send file_reference
+    if !params[:file].blank?
+      file_reference = params[:file]
+      obj = ActiveFedora::Base.find(params[asset_param_key])
+      obj.send file_reference
+    else
+      # After three years of watching bots fail to figure out basic arguments
+      # because script kiddies can't scrape properly, I think we just send them
+      # to the void with a 500. Don't send error emails, it's never a real
+      # person with an honest mistake.
+      render :nothing => true, :status => 500, :content_type => 'text/html' and return
+    end
   end
 end
