7339b29c2c9fc9f5c17d6a0f4bd7125d3240fa50
David Cliff <d.cliff@neu.edu>
2017-11-02 23:08:06 -0400

    Trying manual methods

--- app/controllers/core_files_controller.rb
+++ app/controllers/core_files_controller.rb
@@ -1144,6 +1144,36 @@ class CoreFilesController < ApplicationController
 
     private
 
+      def send_file(path, options = {})
+        if options[:range]
+          send_file_with_range(path, options)
+        else
+          super(path, options)
+        end
+      end
+
+      def send_file_with_range(path, options = {})
+        if File.exist?(path)
+          size = File.size(path)
+          if !request.headers["Range"]
+            status_code = 200 # 200 OK
+            offset = 0
+            length = File.size(path)
+          else
+            status_code = 206 # 206 Partial Content
+            bytes = Rack::Utils.byte_ranges(request.headers, size)[0]
+            offset = bytes.begin
+            length = bytes.end - bytes.begin
+          end
+          response.header["Accept-Ranges"] = "bytes"
+          response.header["Content-Range"] = "bytes #{bytes.begin}-#{bytes.end}/#{size}" if bytes
+
+          send_data IO.binread(path, length, offset), options
+        else
+          raise ActionController::MissingFile, "Cannot read file #{path}."
+        end
+      end
+
       def verify_staff_or_beta
         if !(current_user.repo_staff? || current_user.beta?)
           flash[:error] = "You do not have privileges to use that feature"
