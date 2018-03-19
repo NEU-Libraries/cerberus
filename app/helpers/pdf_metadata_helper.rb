66c2f9cedda84758cc1e884ec2fecf4c435c037b
David Cliff <d.cliff@neu.edu>
2018-03-19 14:35:26 -0400

    Rescue block for pdf metadata

--- app/helpers/pdf_metadata_helper.rb
+++ app/helpers/pdf_metadata_helper.rb
@@ -1,18 +1,22 @@
 module PdfMetadataHelper
   def update_pdf_exif_metadata
-    if !self.core_record.blank? && !self.core_record.pid.blank?
-      require 'mini_exiftool'
-      MiniExiftool.command = "#{Cerberus::Application.config.minitool_path}"
-      pdf = MiniExiftool.new(self.fedora_file_path)
+    begin
+      if !self.core_record.blank? && !self.core_record.pid.blank?
+        require 'mini_exiftool'
+        MiniExiftool.command = "#{Cerberus::Application.config.minitool_path}"
+        pdf = MiniExiftool.new(self.fedora_file_path)
 
-      cf = CoreFile.find(self.core_record.pid)
-      doc = SolrDocument.new cf.to_solr
+        cf = CoreFile.find(self.core_record.pid)
+        doc = SolrDocument.new cf.to_solr
 
-      pdf.title = "#{doc.non_sort} #{doc.title}".strip
-      pdf.author = doc.author
-      pdf.keywords = doc.keyword_list
+        pdf.title = "#{doc.non_sort} #{doc.title}".strip
+        pdf.author = doc.author
+        pdf.keywords = doc.keyword_list
 
-      pdf.save!
+        pdf.save!
+      end
+    rescue Exception => error
+      # PDF Metadata for google search is a nicety - if we error out, move on
     end
   end
 end
