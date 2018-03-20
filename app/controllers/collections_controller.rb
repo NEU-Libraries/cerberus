3f75ee6a0f3f89a9174aaad9e1b37d4d17839179
David Cliff <d.cliff@neu.edu>
2018-03-19 20:47:25 -0400

    Adding timestamp to thumbnail to prevent caching

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -181,6 +181,7 @@ class CollectionsController < ApplicationController
       response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
       response.headers["Pragma"] = "no-cache"
       response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+      @thumbnail_refresh = true
     end
 
     respond_to do |format|
