b8c66b3225ce71b356da0c67fbcf0522abbaa641
David Cliff <d.cliff@neu.edu>
2018-03-22 14:37:47 -0400

    Sitemap improvement

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -9,7 +9,13 @@ SitemapGenerator::Sitemap.create do
       pid = query_result[i]["id"]
       doc = SolrDocument.new ActiveFedora::SolrService.query("id:\"#{pid}\"").first
       if doc.public?
-        add core_file_path(doc.pid), :lastmod => doc.updated_at
+        # Is title unique? - Lets not put in super low value metadata explicitly to google
+        # as it may have a negative affect on our research materials. i.e. IPTC items from
+        # MarCom and D'Amore with the same title over and over for a photoset - Google gets
+        # rightfully mad about canonical items
+        if ActiveFedora::SolrService.count("title_info_title_tesim:\"#{doc.title}\"") <= 1
+          add core_file_path(doc.pid), :lastmod => doc.updated_at
+        end
       end
     rescue Exception => error
       #
