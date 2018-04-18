a798727da083b722cdd596c541d2306c5e7ac9c3
David Cliff <d.cliff@neu.edu>
2018-04-18 12:21:20 -0400

    Add sendfile to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Cerberus::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   config.fedora_home = "/mnt/libraries/cerberusdata/newfedoradata/datastreamStore/"
 
