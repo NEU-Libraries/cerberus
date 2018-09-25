56e4cfdf334f61226bd8c2495b4352544d2578cd
David Cliff <d.cliff@neu.edu>
2018-09-25 10:57:53 -0400

    Minor datetime fixes for agg stats backfill job

--- app/jobs/aggregated_statistics_back_fill_job.rb
+++ app/jobs/aggregated_statistics_back_fill_job.rb
@@ -6,6 +6,8 @@ class AggregatedStatisticsBackFillJob
   attr_accessor :date
 
   def initialize(date)
+    # d = (Time.now - 3.day).midnight - 1.second <- example
+    # Cerberus::Application::Queue.push(AggregatedStatisticsBackFillJob.new(d.to_datetime)) <- example
     self.date = date #start this job with the last friday prior to the friday that you are going to start the scheduled job with. ie if you are starting the scheduled job to run for Fri, 15 Apr 2016 23:59:59 -0400 then initialize this job with Fri, 08 Apr 2016 23:59:59 -0400
   end
 
@@ -13,7 +15,7 @@ class AggregatedStatisticsBackFillJob
     require 'fileutils'
 
     #queue off back fill from given date until the beginning of time
-    if (date > (Impression.order('created_at asc').limit(1).first.created_at))
+    if (date > (Impression.order('created_at asc').limit(1).first.created_at).to_datetime)
       AggregatedStatisticsJob.new(date).run #run the week being kicked off
       prior_week = date.-1.week
       AggregatedStatisticsBackFillJob.new(prior_week).run #run this job again for the week before
