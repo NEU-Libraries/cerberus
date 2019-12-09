8fb9776baf83f6d1f0e8f0e94c2c3db03584dac2
David Cliff <dgcliff@northeastern.edu>
2019-12-09 15:39:50 -0500

    Fixes #1193

--- app/jobs/privatize_collection_job.rb
+++ app/jobs/privatize_collection_job.rb
@@ -16,6 +16,35 @@ class PrivatizeCollectionJob
 
     mailing_list = col_doc.public_descendents
 
+    # sentinel list
+    content_list = [["Audio Master", "audio_master"],
+                    ["Audio", "audio"],
+                    ["Image Master", "image_master"],
+                    ["Image Large", "image_large"],
+                    ["Image Medium", "image_medium"],
+                    ["Image Small", "image_small"],
+                    ["Powerpoint", "mspowerpoint"],
+                    ["Excel", "msexcel"],
+                    ["Word ", "msword"],
+                    ["Pdf", "pdf"],
+                    ["Text", "text"],
+                    ["Video Master", "video_master"],
+                    ["Video", "video"],
+                    ["Zip", "zip"]]
+
+    sentinel = col.sentinel
+
+    # privatize sentinel
+    if !sentinel.blank?
+      sentinel.core_file["mass_permissions"] = "private"
+      # privatize core file and models
+      content_list.each_with_index do |a, i|
+        if !sentinel.send("#{a[1]}".to_sym).blank?
+          sentinel.send("#{a[1]}".to_sym)["mass_permissions"] = "private"
+        end
+      end
+    end
+
     col_doc.public_descendents.each do |v|
       pid = v[0]
       doc = SolrDocument.new ActiveFedora::SolrService.query("id:\"#{pid}\"").first
@@ -23,6 +52,7 @@ class PrivatizeCollectionJob
         x = ActiveFedora::Base.find(doc.pid, cast: true)
         x.mass_permissions = "private"
         x.save!
+        x.propagate_metadata_changes!
       end
     end
 
