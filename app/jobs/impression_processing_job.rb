98d7d1bac46fbe8839803327d762b88b4fdcf32d
David Cliff <dgcliff@northeastern.edu>
2020-04-15 14:12:25 -0400

    Bot checking at end of day impression processing job - filter anything with more than 150 view OR downloads in 24 hr period (cron kickoff)

--- app/jobs/impression_processing_job.rb
+++ app/jobs/impression_processing_job.rb
@@ -4,12 +4,22 @@ class ImpressionProcessingJob
   end
 
   def run
+    # bot list
+    # do IP sweep - greater than 150 downloads OR views in 24hr period (cron) == bot
+    # add to temporary bot list for this job
+    # public false, processed true
+
+    results = Impression.where(processed: false)
+    count_hsh = results.each_with_object(Hash.new {|h, k| h[k] = Hash.new(0) }) { |imp,counts| counts[imp.ip_address][imp.action] += 1}
+    offenders = count_hsh.select{ |k,v| (v["view"].present? && v["view"] > 150) || (v["download"].present? && v["download"] > 150) }
+    bot_ip_list = offenders.map {|k,v| k}
+
     # Only process what hasn't been done already
     Impression.where(processed: false).find_each do |imp|
       ua = imp.user_agent.downcase
       botlist = I18n.t("bots").map(&:downcase)
 
-      if !botlist.any?{|s| ua.include?(s)}
+      if !botlist.any?{|s| ua.include?(s)} || !bot_ip_list.include?(imp.ip_address)
         imp.public = true
       end
 
