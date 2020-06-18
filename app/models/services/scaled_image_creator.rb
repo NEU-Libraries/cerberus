a1cdcf0dc9b5a1c0e66052d38d6ba6517c112339
David Cliff <dgcliff@northeastern.edu>
2020-06-18 01:15:22 -0400

    Large upload fix. Thank god.

--- app/models/services/scaled_image_creator.rb
+++ app/models/services/scaled_image_creator.rb
@@ -119,15 +119,13 @@ class ScaledImageCreator
     private
 
       def large_upload(content_object, file_path, dsid)
-        url = URI("#{ActiveFedora.config.credentials[:url]}")
-        req = Net::HTTP::Post.new("#{ActiveFedora.config.credentials[:url]}/objects/#{content_object.pid}/datastreams/#{dsid}?controlGroup=M&dsLocation=file://#{file_path}")
-        req.basic_auth("#{ActiveFedora.config.credentials[:user]}", "#{ActiveFedora.config.credentials[:password]}")
-        req.add_field("Content-Type", "#{extract_mime_type(file_path)}")
-        req.add_field("Transfer-Encoding", "chunked")
-        res = Net::HTTP.start(url.host, url.port) {|http|
-          http.read_timeout = 600
-          http.request(req)
-        }
+        res = ''
+        uri = URI("#{ActiveFedora.config.credentials[:url]}/objects/#{content_object.pid}/datastreams/#{dsid}?controlGroup=M&dsLocation=file://#{file_path}")
+        Net::HTTP.start(uri.host, uri.port) do |http|
+          request = Net::HTTP::Post.new uri
+          request.basic_auth("#{ActiveFedora.config.credentials[:user]}", "#{ActiveFedora.config.credentials[:password]}")
+          res = http.request request # Net::HTTPResponse object
+        end
         return res
       end
 end
