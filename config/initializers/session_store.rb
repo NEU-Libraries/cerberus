8d8a97959d1159943bc0088d2b3b030d8c68fbcf
David Cliff <david.g.cliff@gmail.com>
2021-01-26 21:49:15 -0500

    Session fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Cerberus::Application.config.session_store :cookie_store, key: '_cerberus_app_session'
-Cerberus::Application.config.session_store :redis_store, key: '_cerberus_app_session'
+Cerberus::Application.config.session_store :redis_store,
+  servers: ["redis://nb9478.neu.edu:6379/0/session"],
+  key: '_cerberus_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
