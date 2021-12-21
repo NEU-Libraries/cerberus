31a5e1c00173cb2844634d56f601c85badeaec24
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2021-12-21 15:45:13 -0500

    Add Ph D to namae suffix list

--- config/initializers/namae.rb
+++ config/initializers/namae.rb
@@ -1,1 +1,1 @@
-Namae::Parser.instance.options[:suffix] = /\s*\b(?i)(Esq|JR|Jr|jr|SR|Sr|sr|[IVX]{2,})(\.|\b)/
+Namae::Parser.instance.options[:suffix] = /\s*\b(?i)(Esq|JR|Jr|jr|SR|Sr|sr|Ph D|[IVX]{2,})(\.|\b)/
