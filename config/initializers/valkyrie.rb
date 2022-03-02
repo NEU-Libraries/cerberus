988dfc53f04ffb167bbd0930977d67b4cdc7bea1
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-03-02 12:03:44 -0500

    Adding charons valkyrie configs

---
+++ config/initializers/valkyrie.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'valkyrie'
+Rails.application.config.to_prepare do
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::Persistence::Postgres::MetadataAdapter.new,
+    :postgres
+  )
+
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::Persistence::Memory::MetadataAdapter.new,
+    :memory
+  )
+
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Disk.new(
+      base_path: Rails.root.join('tmp', 'files'),
+      file_mover: FileUtils.method(:cp)
+    ),
+    :test_disk
+  )
+
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Disk.new(
+      base_path: '/home/charon/storage/valkyrie',
+      file_mover: FileUtils.method(:cp)
+    ),
+    :disk
+  )
+
+  Valkyrie::StorageAdapter.register(
+    Valkyrie::Storage::Memory.new,
+    :memory
+  )
+
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::Persistence::Solr::MetadataAdapter.new(
+      connection: Blacklight.default_index.connection,
+      resource_indexer: Valkyrie::Persistence::Solr::CompositeIndexer.new(
+        Valkyrie::Indexers::AccessControlsIndexer,
+        HumanReadableTypeIndexer
+      )
+    ),
+    :index_solr
+  )
+
+  Valkyrie::MetadataAdapter.register(
+    Valkyrie::AdapterContainer.new(
+      persister: Valkyrie::Persistence::CompositePersister.new(
+        Valkyrie::MetadataAdapter.find(:postgres).persister,
+        Valkyrie::MetadataAdapter.find(:index_solr).persister
+      ),
+      query_service: Valkyrie::MetadataAdapter.find(:postgres).query_service
+    ),
+    :composite_persister
+  )
+end
