ac41c54c9da36e21910e8db9ea8971c4ecb88a59
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-03-14 12:01:35 -0400

    Add in charon file set

---
+++ app/models/file_set.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class FileSet < Resource
+  attribute :type, Valkyrie::Types::String
+  attribute :member_ids, Valkyrie::Types::Set.of(Valkyrie::Types::ID)
+  attribute :a_member_of, Valkyrie::Types::ID
+
+  def files
+    @files ||= member_ids.map { |id| Blob.find(id) }
+  end
+
+  def original_file?
+    files.each do |f|
+      return true if f.use.include? Valkyrie::Vocab::PCDMUse.OriginalFile
+    end
+    false
+  end
+end
