93d7eb706c1cddad6739e1101994953234457cc0
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-03-17 13:50:57 -0400

    Rubocop clean

--- app/models/work.rb
+++ app/models/work.rb
@@ -4,6 +4,9 @@ class Work < Resource
   attribute :a_member_of, Valkyrie::Types::Set.of(Valkyrie::Types::ID).meta(ordered: true)
 
   def mods_xml_path
-    children.find { |c| c.instance_of?(FileSet) && c.type == 'Metadata' }.files.first.file_path
+    # TODO: Fix fragility (first etc.)
+    children.find do |c|
+      c.instance_of?(FileSet) && c.type == 'Metadata'
+    end.files.first.file_path
   end
 end
