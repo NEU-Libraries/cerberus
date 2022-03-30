5076afd2e5f261e5c09e68f040afac3cf92d6e97
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-03-30 13:56:55 -0400

    Clean up dockerfile by removing yarn, and minor BL changes. Going to persist image to docker hub soon to try and get staging server setup

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -47,8 +47,8 @@ class CatalogController < ApplicationController
     # config.add_show_tools_partial(:sms, if: :render_sms_action?, callback: :sms_action, validator: :validate_sms_params)
     # config.add_show_tools_partial(:citation)
 
-    config.add_nav_action(:bookmark, partial: 'blacklight/nav/bookmark', if: :render_bookmarks_control?)
-    config.add_nav_action(:search_history, partial: 'blacklight/nav/search_history')
+    # config.add_nav_action(:bookmark, partial: 'blacklight/nav/bookmark', if: :render_bookmarks_control?)
+    # config.add_nav_action(:search_history, partial: 'blacklight/nav/search_history')
 
     # solr field configuration for document/show views
     # config.show.title_field = 'title_tsim'
