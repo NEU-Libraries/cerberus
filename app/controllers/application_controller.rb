f4c372c1f9222f7e8842e83684fd990e3090a6b4
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-03-31 10:11:37 -0400

    Update devise partials to use bootstrap 5 and internationalization as per https://github.com/kroger/devise-bootstrap5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
   layout :determine_layout if respond_to? :layout
+
+  before_action do
+    I18n.locale = :en
+  end
 end
