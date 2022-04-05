699a5e31c7db3960300c0636046193f391f16430
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-04-05 14:09:29 -0400

    Switch to super layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
-  layout :determine_layout if respond_to? :layout
 
   before_action do
     I18n.locale = :en
