9e50d3003f9cf950061662d5f6e0b10bbbaebc89
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-04-07 15:04:19 -0400

    More breadcrumbs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,18 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
+  include Croutons::Controller
 
   before_action do
     I18n.locale = :en
+  end
+
+  # Best to ask for breadcrumbs everywhere
+  # and just avoid Croutons NotImplementedError
+  def breadcrumbs
+    super
+  rescue NoMethodError, NotImplementedError
+    # Just don't show them
+    logger.info('No breadcrumbs found') && (return)
   end
 end
---
+++ app/models/breadcrumb_trail.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class BreadcrumbTrail < Croutons::BreadcrumbTrail
+end
