87cb2ce16fd548472c870bbe544372b11920f83f
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-04-28 05:32:40 -0900

    Iterating on reset task - mods fixtures for community and collection, and removing stand-in attrs

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,7 +3,5 @@
 class Collection < Resource
   include Modsable
 
-  attribute :title, Valkyrie::Types::String
-  attribute :description, Valkyrie::Types::String
   attribute :a_member_of, Valkyrie::Types::Set.of(Valkyrie::Types::ID).meta(ordered: true)
 end
