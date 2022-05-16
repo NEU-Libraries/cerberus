ff4310ffb2f5c8ac7763056300d4b35f3b06efdf
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-05-16 12:57:28 -0400

    Fix for missing id div

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -7,9 +7,7 @@ class XmlController < ApplicationController
   end
 
   def validate
-    # puts "DGC DEBUG " + params.inspect
-    # @work = Work.new(alternate_ids: [current_user.id])
-    @work = Work.new
+    @work = Work.new(alternate_ids: ["#{Time.now.to_f.to_s.gsub!('.', '')}"])
     @work.mods_json = params[:raw_xml]
     respond_to do |format|
       format.turbo_stream
