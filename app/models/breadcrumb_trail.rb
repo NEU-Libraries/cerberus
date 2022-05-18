a5ecb3d2fdf53fd12ac9d19ea92665ef8adede63
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-05-18 14:16:06 -0400

    Breadcrumb to root

--- app/models/breadcrumb_trail.rb
+++ app/models/breadcrumb_trail.rb
@@ -2,8 +2,23 @@
 
 class BreadcrumbTrail < Croutons::BreadcrumbTrail
   def works_show
-    breadcrumb('Hats', 'http://www.google.com')
-    breadcrumb('Ducks', 'http://www.google.com')
-    breadcrumb('Google', 'http://www.google.com')
+    breadcrumb_to_root(objects[:work]).each do |r|
+      r.decorate
+      breadcrumb(r.plain_title, polymorphic_path(r))
+    end
   end
+
+  private
+    def breadcrumb_to_root(resource)
+      trail = [resource]
+      parent = resource.parent
+      loop do
+        if !parent.nil?
+          trail << parent
+          parent = parent.parent
+        else
+          return trail.reverse
+        end
+      end
+    end
 end
