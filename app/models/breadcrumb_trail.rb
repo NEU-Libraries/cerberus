05ef7c968680a93f4ccb18cfb8a45b60f158913f
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-05-18 14:16:55 -0400

    Stubbing out col and com breadcrumbs

--- app/models/breadcrumb_trail.rb
+++ app/models/breadcrumb_trail.rb
@@ -8,6 +8,20 @@ class BreadcrumbTrail < Croutons::BreadcrumbTrail
     end
   end
 
+  def collections_show
+    breadcrumb_to_root(objects[:collection]).each do |r|
+      r.decorate
+      breadcrumb(r.plain_title, polymorphic_path(r))
+    end
+  end
+
+  def communities_show
+    breadcrumb_to_root(objects[:community]).each do |r|
+      r.decorate
+      breadcrumb(r.plain_title, polymorphic_path(r))
+    end
+  end
+
   private
     def breadcrumb_to_root(resource)
       trail = [resource]
