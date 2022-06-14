085f68397e9354fab95016a7e05e4e01852df4b4
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2022-06-14 13:54:47 -0400

    desc assignment now works with template

--- app/lib/mods_assignment.rb
+++ app/lib/mods_assignment.rb
@@ -3,15 +3,13 @@
 module MODSAssignment
   def plain_title=(title_str)
     mods_obj = Mods::Record.new.from_str(self.mods_xml)
-    # there is no first in a blank xml - need to fix
-    # mods_obj.title_info.title.children.first.content = title_str
     mods_obj.title_info.find { |n| n.attribute("usage")&.value == "primary" }.title.content = title_str
     self.mods_xml = mods_obj.to_xml
   end
 
   def plain_description=(desc_str)
-    # mods_obj = Mods::Record.new.from_str(self.mods_xml)
-    # mods_obj.abstract.first.content = desc_str
-    # self.mods_xml = mods_obj.to_xml
+    mods_obj = Mods::Record.new.from_str(self.mods_xml)
+    mods_obj.abstract.first.content = desc_str
+    self.mods_xml = mods_obj.to_xml
   end
 end
