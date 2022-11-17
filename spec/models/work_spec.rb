f9dc5ce217e0ca404620fe4949cd478e0a8f1ed6
David Cliff <david.g.cliff@gmail.com>
2022-11-17 20:50:43 +0000

    Stubbing out work model

---
+++ spec/models/work_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Generated with `rails generate valkyrie:model FileSet`
+require 'rails_helper'
+require 'valkyrie/specs/shared_specs'
+
+RSpec.describe Work do
+  let(:resource_klass) { described_class }
+#   let(:file_set) { FactoryBot.create_for_repository(:file_set) }
+  it_behaves_like 'a Valkyrie::Resource'
+end
