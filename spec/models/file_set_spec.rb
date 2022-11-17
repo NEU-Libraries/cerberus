b78c3a4f939b9f50e7f2fc55237c1e01552ab589
David Cliff <david.g.cliff@gmail.com>
2022-11-17 15:36:23 +0000

    Starting file set spec, introducing factories

---
+++ spec/models/file_set_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Generated with `rails generate valkyrie:model FileSet`
+require 'rails_helper'
+require 'valkyrie/specs/shared_specs'
+
+RSpec.describe FileSet do
+  let(:resource_klass) { described_class }
+  let(:file_set) { FactoryBot.create_for_repository(:file_set) }
+  it_behaves_like 'a Valkyrie::Resource'
+
+#   it 'returns files associated with the object' do
+#     expect(file_set.files).to include(Blob.find(file_set.member_ids.first))
+#   end
+end
