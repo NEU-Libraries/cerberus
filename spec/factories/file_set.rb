605570c9bcfc23a39cf3d301dcd163979b34d11c
David Cliff <david.g.cliff@gmail.com>
2022-11-17 16:22:08 +0000

    Adding blob factory and fixture files from Charon

--- spec/factories/file_set.rb
+++ spec/factories/file_set.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-    factory :file_set do
-      member_ids { FactoryBot.create_for_repository(:blob).id }
-  
-      to_create do |instance|
-        Valkyrie.config.metadata_adapter.persister.save(resource: instance)
-      end
-  
-      trait :blank do
-        member_ids { [] }
-      end
-  
-      trait :pdf do
-        member_ids { FactoryBot.create_for_repository(:blob, :pdf).id }
-      end
-  
-      trait :png do
-        member_ids { FactoryBot.create_for_repository(:blob, :png).id }
-      end
+  factory :file_set do
+    member_ids { FactoryBot.create_for_repository(:blob).id }
+
+    to_create do |instance|
+      Valkyrie.config.metadata_adapter.persister.save(resource: instance)
+    end
+
+    trait :blank do
+      member_ids { [] }
+    end
+
+    trait :pdf do
+      member_ids { FactoryBot.create_for_repository(:blob, :pdf).id }
+    end
+
+    trait :png do
+      member_ids { FactoryBot.create_for_repository(:blob, :png).id }
     end
   end
+end
