e2c95466759c005f3283e732f81c43ea3ee42437
David Cliff <david.g.cliff@gmail.com>
2022-11-18 16:50:29 +0000

    Stubbing out work controller spec

--- spec/controllers/works_controller_spec.rb
+++ spec/controllers/works_controller_spec.rb
@@ -3,4 +3,16 @@
 require 'rails_helper'
 
 describe WorksController do
+  let(:community) { CommunityCreator.call(mods_xml: File.read('/home/cerberus/web/spec/fixtures/files/community-mods.xml')) }
+  let(:collection) { CollectionCreator.call(parent_id: community.id, mods_xml: File.read('/home/cerberus/web/spec/fixtures/files/collection-mods.xml')) }
+  let(:work) { WorkCreator.call(parent_id: collection.id, mods_xml: File.read('/home/cerberus/web/spec/fixtures/files/work-mods.xml')) }
+
+  describe 'show' do
+    render_views
+    it 'renders the show partial' do
+      get :show, params: { id: work.noid }
+      expect(response).to render_template('works/show')
+      expect(CGI.unescapeHTML(response.body)).to include(collection.plain_title)
+    end
+  end
 end
