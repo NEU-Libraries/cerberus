2bd3b86a93a05226c826162008e00dec50513593
David Cliff <david.g.cliff@gmail.com>
2022-11-30 16:36:57 +0000

    Stubbing BL controller spec

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -3,4 +3,11 @@
 require 'rails_helper'
 
 describe CatalogController do
+  describe 'index' do
+    render_views
+    it 'renders the index partial' do
+      get :index
+      expect(response).to render_template('catalog/index')
+    end
+  end
 end
