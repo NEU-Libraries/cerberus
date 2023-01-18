ad4713a8c9557922f4b6022fa3a72d87fe75a163
David Cliff <david.g.cliff@gmail.com>
2023-01-18 18:22:05 +0000

    Refactor file binary creation to helper

---
+++ app/helpers/file_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module FileHelper
+  def create_file(file_path, resource, original_filename = file_path.split('/').last)
+    Valkyrie.config.storage_adapter.upload(
+      file: File.open(file_path), # tei, png, txt
+      resource: resource,
+      original_filename: original_filename
+    )
+  end
+end
