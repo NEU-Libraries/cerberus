16bc257a39d3d7f710ab2f5203592c84b02a6cfa
David Cliff <david.g.cliff@gmail.com>
2023-01-18 15:43:43 +0000

    Stubbing out requirements for work without metadata

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -10,6 +10,10 @@ class WorksController < ApplicationController
   end
 
   def create
+    puts params.inspect
+    # TODO: create blob for params[:binary]
+    # TODO: Assign the file name to the works title
+    # TODO: Associate the work with the collection
     w = Valkyrie.config.metadata_adapter.persister.save(resource: Work.new)
     redirect_to w
   end
