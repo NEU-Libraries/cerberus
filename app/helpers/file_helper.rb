2dcd8a658dba275a0e35936fc4c6ba09b007407c
David Cliff <david.g.cliff@gmail.com>
2023-01-19 16:19:32 +0000

    Comment for clarity

--- app/helpers/file_helper.rb
+++ app/helpers/file_helper.rb
@@ -4,7 +4,7 @@ module FileHelper
   def create_file(file_path, resource, original_filename = file_path.split('/').last)
     Valkyrie.config.storage_adapter.upload(
       file: File.open(file_path), # tei, png, txt
-      resource: resource,
+      resource: resource, # FileSet
       original_filename: original_filename
     )
   end
