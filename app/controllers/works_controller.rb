369180585f95e04bda762dc292c2930a655f4333
David Cliff <david.g.cliff@gmail.com>
2023-01-25 20:25:58 +0000

    Blob creator and refactor works controller to utilize a create method for the Work model

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -13,23 +13,12 @@ class WorksController < ApplicationController
   end
 
   def create
-    # TODO: create blob for params[:binary]
     file = params[:binary]
-    w = WorkCreator.call(parent_id: Collection.find(params[:collection_id]).id)
-    w.plain_title = file.original_filename
-    w = Valkyrie.config.metadata_adapter.persister.save(resource: w)
-
-    # TODO: Get classification from file path - path -> mime type -> image/video etc.
-    create_blob((file.tempfile.path.presence || file.path))
-
+    w = Work.create(
+      collection_id: Collection.find(params[:collection_id]).id,
+      title: file.original_filename
+    )
+    BlobCreator.call(work_id: w.id, path: (file.tempfile.path.presence || file.path))
     redirect_to w
   end
-
-  private
-
-    def create_blob(path, work_id)
-      fs = FileSetCreator.call(work_id: work_id, classification: Classification.generic)
-      b = Valkyrie.config.metadata_adapter.persister.save(resource: Blob.new).id
-      b.file_identifiers += [create_file(path, fs).id]
-    end
 end
--- app/helpers/classification_helper.rb
+++ app/helpers/classification_helper.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 module ClassificationHelper
-  def assign_type(file_path)
+  def assign_type(_file_path)
     Classification.work
   end
 
-  def extract_mime_type(file_path)
-  end
+  def extract_mime_type(file_path); end
 end
