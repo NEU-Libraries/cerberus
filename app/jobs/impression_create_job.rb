54d68ef072451712fd54ba756025395c105e9b43
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2023-05-09 13:28:59 -0400

    Putting impression creation in retry block

--- app/jobs/impression_create_job.rb
+++ app/jobs/impression_create_job.rb
@@ -17,7 +17,12 @@ class ImpressionCreateJob
   end
 
   def run
-    Impression.create(pid: pid, session_id: session_id, action: action, ip_address: ip_address,
-      referrer: referrer, user_agent: user_agent, status: status)
+    begin
+      retries ||= 0
+      Impression.create(pid: pid, session_id: session_id, action: action, ip_address: ip_address,
+        referrer: referrer, user_agent: user_agent, status: status)
+    rescue Mysql2::Error
+      retry if (retries += 1) < 3
+    end
   end
 end
