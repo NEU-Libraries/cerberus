add6e1b589465e28a77f5a3dd296b8c8089dad0b
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2023-07-25 00:20:44 -0400

    Xi name/suffix fix

--- config/initializers/namae.rb
+++ config/initializers/namae.rb
@@ -1,2 +1,2 @@
-Namae::Parser.instance.options[:suffix] = /\s*\b(?i)(Esq|JR|Jr|jr|SR|Sr|sr|PhD|Phd|Ph D|[IVX]{2,})(\.|\b)/
+Namae::Parser.instance.options[:suffix] = /\s*\b(?i)(Esq|JR|Jr|jr|SR|Sr|sr|PhD|Phd|Ph D)(\.|\b)/
 # Namae::Parser.instance.options[:include_particle_in_family] = true
