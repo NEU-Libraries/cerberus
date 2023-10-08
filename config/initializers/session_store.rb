13807263df76c4b96049a9d48070dbcdc8abbd20
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2023-10-08 14:33:59 -0400

    Nailed the TTL redis issue - after checking on a long enough timeline, comfotable bumping out the value to 1 month from 1 week without concern for memory pressure, for user experience.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -8,10 +8,10 @@
 
 Cerberus::Application.config.session_store :redis_store, {
   servers: [
-    { url: "redis://nb9478.neu.edu:6379/0/session", expires_in: 1.week, timeout: 10.0, reconnect_attempts: 10, tcp_keepalive: 300 },
+    { url: "redis://nb9478.neu.edu:6379/0/session", expires_in: 1.month, timeout: 10.0, reconnect_attempts: 10, tcp_keepalive: 300 },
   ],
   key: '_cerberus_app_session',
-  expire_after: 1.week
+  expire_after: 1.month
 }
 
 # Use the database for sessions instead of the cookie-based default,
