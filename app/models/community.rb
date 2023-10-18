2666734157f6c047fff0e258ea926a83b7754b2b
David Cliff <david.g.cliff@gmail.com>
2023-10-18 18:11:34 +0000

    Remove valkyrie related models

--- app/models/community.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Community < Resource
-  include Modsable
-
-  attribute :a_member_of, Valkyrie::Types::Set.of(Valkyrie::Types::ID).meta(ordered: true)
-end
--- app/models/metadata.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Metadata
-  def self.table_name_prefix
-    'metadata_'
-  end
-end
