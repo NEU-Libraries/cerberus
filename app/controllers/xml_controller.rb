8130c6bf7773c7c9cd488a51ee2040bf4b824549
David Cliff <david.g.cliff@gmail.com>
2024-01-30 20:48:31 +0000

    Xml editor now working with Atlas and a Work. Need to validate it works for Community and Collections, make validate and update work, then add in the UX via a tab on the edit partials

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -2,19 +2,27 @@
 
 class XmlController < ApplicationController
   def editor
-    @work = Work.find(params[:id])
-    @raw_xml = @work.mods_xml
+    item = AtlasRb::Resource.find(params[:id])
+    @resource = item['resource']
+    resource_mods(item['klass'])
   end
 
   def validate
-    @work = Work.new(alternate_ids: [Time.now.to_f.to_s.gsub!('.', '').to_s])
-    @work.mods_json = params[:raw_xml]
+    # @work = Work.new(alternate_ids: [Time.now.to_f.to_s.gsub!('.', '').to_s])
+    # @work.mods_json = params[:raw_xml]
   end
 
   def update
-    work = Work.find(params[:work_id])
-    work.mods_xml = params[:raw_xml]
-    flash[:success] = 'XML updated'
-    redirect_to work
+    # work = Work.find(params[:work_id])
+    # work.mods_xml = params[:raw_xml]
+    # flash[:success] = 'XML updated'
+    # redirect_to work
   end
+
+  private
+
+    def resource_mods(klass)
+      @mods = AtlasRb.const_get(klass).mods(params[:id], 'html')
+      @raw_xml = AtlasRb.const_get(klass).mods(params[:id], 'xml')
+    end
 end
