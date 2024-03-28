7af30eb5888774c84f8334f3a46c4123dcbc6fc0
David Cliff <david.g.cliff@gmail.com>
2024-03-28 18:20:07 +0000

    Working thumbnail code across communities, collections, and works.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class WorksController < ApplicationController
+  include Thumbable
+
   def show
     # @work = Work.find(params[:id])
     @work = AtlasRb::Work.find(params[:id])
@@ -29,9 +31,9 @@ class WorksController < ApplicationController
   end
 
   def update
-    # permitted = params.require(:community).permit(:title, :description).to_h
-    # add_thumbnail(permitted)
-    # AtlasRb::Community.metadata(params[:id], permitted)
-    # redirect_to community_path(params[:id])
+    permitted = params.require(:work).permit(:title, :description).to_h
+    add_thumbnail(permitted)
+    AtlasRb::Work.metadata(params[:id], permitted)
+    redirect_to work_path(params[:id])
   end
 end
