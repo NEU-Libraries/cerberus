0c34466029e22964eae6399352dc0a8f0e7b3211
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2024-05-16 19:53:13 -0400

    Refactored iptc mailer - need to add to cron. Also need to refactor unicode warning out, and add datetime warning in.

--- app/mailers/iptc_mailer.rb
+++ app/mailers/iptc_mailer.rb
@@ -1,11 +1,12 @@
 class IptcMailer < ActionMailer::Base
   default from: "notifier@repository.library.northeastern.edu"
 
-  def iptc_alert(pid)
-    @pid = pid
+  def iptc_alert
+    @item_reports = Loaders::ItemReport.where('validity = TRUE AND modified = TRUE AND created_at > ?', DateTime.yesterday.end_of_day)
+
     if ["production", "secondary"].include? Rails.env
       mail(to: "Library-DRS-Metadata@northeastern.edu",
-           subject: "[DRS] IPTC Creator parse error - #{pid}",
+           subject: "[DRS] IPTC parse error(s) - #{pid}",
            content_type: "text/html")
     end
   end
