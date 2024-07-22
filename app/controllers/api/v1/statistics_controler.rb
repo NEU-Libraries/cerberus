02e88eb5cc5ed1f8e48cb53fa0c71b8d15093845
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2024-07-22 15:21:02 -0400

    Should work - will test

--- app/controllers/api/v1/statistics_controler.rb
+++ app/controllers/api/v1/statistics_controler.rb
@@ -1,7 +1,18 @@
 module Api
   module V1
     class StatisticsController < ApplicationController
-      # TODO: link agg stats to routes for communities/collections/core_files
+
+      def views
+        render json: AggregatedStatistic.where(pid: pid).map{ |x| {"time" => x.processed_at.to_s, "count" => x.views.to_s} }.to_json
+      end
+
+      def downloads
+        render json: AggregatedStatistic.where(pid: pid).map{ |x| {"time" => x.processed_at.to_s, "downloads" => x.views.to_s} }.to_json
+      end
+
+      def streams
+        render json: AggregatedStatistic.where(pid: pid).map{ |x| {"time" => x.processed_at.to_s, "streams" => x.views.to_s} }.to_json
+      end
     end
   end
 end
