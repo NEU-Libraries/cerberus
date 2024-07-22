14c124c68d965cd147c1702340191ccb5557f4f6
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2024-07-22 19:39:02 -0400

    Fix for params

--- app/controllers/api/v1/statistics_controler.rb
+++ app/controllers/api/v1/statistics_controler.rb
@@ -3,15 +3,15 @@ module Api
     class StatisticsController < ApplicationController
 
       def views
-        render json: AggregatedStatistic.where(pid: pid).map{ |x| {"time" => x.processed_at.to_s, "count" => x.views.to_s} }.to_json
+        render json: AggregatedStatistic.where(pid: params[:id]).map{ |x| {"time" => x.processed_at.to_s, "count" => x.views.to_s} }.to_json
       end
 
       def downloads
-        render json: AggregatedStatistic.where(pid: pid).map{ |x| {"time" => x.processed_at.to_s, "count" => x.downloads.to_s} }.to_json
+        render json: AggregatedStatistic.where(pid: params[:id]).map{ |x| {"time" => x.processed_at.to_s, "count" => x.downloads.to_s} }.to_json
       end
 
       def streams
-        render json: AggregatedStatistic.where(pid: pid).map{ |x| {"time" => x.processed_at.to_s, "count" => x.streams.to_s} }.to_json
+        render json: AggregatedStatistic.where(pid: params[:id]).map{ |x| {"time" => x.processed_at.to_s, "count" => x.streams.to_s} }.to_json
       end
     end
   end
