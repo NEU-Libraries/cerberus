28fd574855a8fa8d7639a26c059367ce97bf98eb
George Lawton <glawton03@outlook.com>
2024-09-05 12:44:17 -0400

    Deleted Unnecessary Decorator Files

--- app/decorators/collection_decorator.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module CollectionDecorator
-  include DecoratorHelper
-  include MODSDecoration
-end
--- app/decorators/community_decorator.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module CommunityDecorator
-  include DecoratorHelper
-  include MODSDecoration
-end
