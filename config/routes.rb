da15626a5df33487ecc443d3a6ccb45d92646f89
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2024-10-25 19:45:46 -0400

    Replacement file routes

--- config/routes.rb
+++ config/routes.rb
@@ -218,6 +218,8 @@ Cerberus::Application.routes.draw do
     resources :statistics, only: [:index]
     resources :users, only: [:index, :show]
     resources :compilations, path: "/sets", except: [:show]
+    get "/new_replacement_file/:id" => "core_files#new_replacement_file", as: 'new_replacement_file'
+    post "/create_replacement_file" => "core_files#create_replacement_file", as: 'create_replacement_file'
     get "/statistics/views" => 'statistics#get_views', as: 'views'
     get "/statistics/downloads" => 'statistics#get_downloads', as: 'downloads'
     get "/statistics/streams" => 'statistics#get_streams', as: 'streams'
