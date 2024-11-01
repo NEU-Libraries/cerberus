529522c1bd0fae6d837a21a8e9224fd72f23f97f
david.g.cliff@gmail.com <david.g.cliff@gmail.com>
2024-11-01 09:25:13 -0400

    Fix for errant self. reference

--- app/jobs/zip_compilation_job.rb
+++ app/jobs/zip_compilation_job.rb
@@ -105,7 +105,7 @@ class ZipCompilationJob
 
     def zip_core_file(doc, user, temp_zipfile_name)
       begin
-        self.path = "#{Rails.application.config.tmp_path}/sets/#{self.comp_pid.gsub(":", "_")}"
+        path = "#{Rails.application.config.tmp_path}/sets/#{self.comp_pid.gsub(":", "_")}"
         FileUtils.mkdir_p path
 
         files_path = "#{path}/downloads"
