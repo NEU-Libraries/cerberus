cbec38721d02ccb6822651f6caaec0d8ba66382e
David Cliff <dgcliff@northeastern.edu>
2025-02-26 22:14:53 +0000

    has_search_parameters was too broad - moving to q blank specifically

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -47,13 +47,11 @@ class CatalogController < ApplicationController
   def index
     if ((!params[:q].blank?) && (CGI::unescape(params[:q]) == "*") && (current_user.blank? || current_user.nuid.blank?))
       render :template => '/error/500', :layout => false, :formats => [:html], :status => 500
+    elsif params[:q].blank? && !params[:f].blank? && !not_a_bot?
+      # bots lost in the facet sauce
+      # give them a blank 410 to lower resource usage
+      render :nothing => true, :status => 410, :content_type => 'text/html' and return
     elsif !has_search_parameters?
-      if !not_a_bot? && !params[:f].blank?
-        # bots lost in the facet sauce
-        # give them a blank 410 to lower resource usage
-        render :nothing => true, :status => 410, :content_type => 'text/html' and return
-      end
-
       self.solr_search_params_logic += [:disable_highlighting]
       recent
 
