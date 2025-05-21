abdbe2082da6831a24f4fbc5716afd3cefeaf304
David Cliff <dgcliff@northeastern.edu>
2025-05-21 16:46:30 +0000

    Bring in and override user util links from BL to start patching default devise expectations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include ActiveModel::API
+  include ActiveModel::Validations
+  extend ActiveModel::Callbacks
+  extend Devise::Models
+
+  define_model_callbacks :validation
+
+  attr_accessor :email, :password
+
   devise :custom_authenticatable, authentication_keys: [:email, :password]
 end
