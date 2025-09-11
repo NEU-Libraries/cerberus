f3cff037a8bff815bc33053a461811f48098831a
David Cliff <dgcliff@northeastern.edu>
2025-09-11 02:31:52 +0000

    Variable name fix for content object

--- app/jobs/checksum_job.rb
+++ app/jobs/checksum_job.rb
@@ -14,10 +14,10 @@ class ChecksumJob
     # fetch content object
     content_object = ActiveFedora::Base.find(content_pid, cast: true)
 
-    if File.exists?(co.fedora_file_path)
+    if File.exists?(content_object.fedora_file_path)
       # 2 gig limit, or Azure NFS churns
-      if (File.size(co.fedora_file_path).to_f / 1024000).round(2) < 2000
-        cs = `md5sum #{co.fedora_file_path.shellescape}`.split(" ").first
+      if (File.size(content_object.fedora_file_path).to_f / 1024000).round(2) < 2000
+        cs = `md5sum #{content_object.fedora_file_path.shellescape}`.split(" ").first
         content_object.properties.md5_checksum = cs
         content_object.save!
       else
