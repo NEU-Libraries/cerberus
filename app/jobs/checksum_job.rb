ccd9ccbb5507eaf86a2ba9f777f3078af17a6f38
David Cliff <dgcliff@northeastern.edu>
2025-09-12 16:05:17 +0000

    Re-ordering checksum job to fetch content object after checksum to try and avoid race condition of overwriting data in properties datastream. Copy paste fedora file path into job to self refer to pid without using AF to fetch the obj first

--- app/jobs/checksum_job.rb
+++ app/jobs/checksum_job.rb
@@ -11,13 +11,14 @@ class ChecksumJob
   end
 
   def run
-    # fetch content object
-    content_object = ActiveFedora::Base.find(content_pid, cast: true)
-
-    if File.exists?(content_object.fedora_file_path)
+    if File.exists?(disk_path)
       # 2 gig limit, or Azure NFS churns
-      if (File.size(content_object.fedora_file_path).to_f / 1024000).round(2) < 2000
-        cs = `md5sum #{content_object.fedora_file_path.shellescape}`.split(" ").first
+      if (File.size(disk_path).to_f / 1024000).round(2) < 2000
+        cs = `md5sum #{disk_path.shellescape}`.split(" ").first
+
+        # fetch content object
+        content_object = ActiveFedora::Base.find(content_pid, cast: true)
+
         content_object.properties.md5_checksum = cs
         content_object.save!
       else
@@ -26,5 +27,15 @@ class ChecksumJob
     else
       logger.warn "#{content_pid} fedora file path empty"
     end
+  end
+
+  def disk_path
+    config_path = Rails.application.config.fedora_home
+    datastream_str = "info:fedora/#{content_pid}/content/content.0"
+    escaped_datastream = Rack::Utils.escape(datastream_str)
+    md5_str = Digest::MD5.hexdigest(datastream_str)
+    dir_name = md5_str[0,2]
+    file_path = config_path + dir_name + "/" + escaped_datastream
+    return file_path
   end
 end
