e0119b32c13fc572f682692e38f8d8b83ccd9f03
David Cliff <dgcliff@northeastern.edu>
2025-12-01 15:13:08 +0000

    Remove user model from DB - we're direct to Atlas now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord
+class User
   include ActiveModel::API
   include ActiveModel::Validations
   extend ActiveModel::Callbacks
--- db/schema.rb
+++ db/schema.rb
@@ -143,17 +143,5 @@ ActiveRecord::Schema[8.1].define(version: 2024_09_26_172306) do
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "ingests", "load_reports"
 end
