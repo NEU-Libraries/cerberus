4b2b16e07ed964acd82ac2e565492310b60b211b
David Cliff <dgcliff@northeastern.edu>
2026-01-17 17:17:10 +0000

    Trying cookie clear for shib sticky on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < Devise::SessionsController
+  before_filter :delete_all_app_cookies, :only => :destroy
   after_filter :clear_sign_signout_flash, :only => [:create, :destroy]
 
   protected
@@ -6,6 +7,18 @@ class SessionsController < Devise::SessionsController
     def clear_sign_signout_flash
       if flash.keys.include?(:notice)
         flash.delete(:notice)
+      end
+    end
+
+    def delete_all_app_cookies
+      # Iterate over all cookies sent by the browser to your app's current path/domain
+      cookies.each_key do |cookie_name|
+        # Delete each cookie individually, using default options
+        cookies.delete(cookie_name.to_sym)
+
+        # If any cookies were set with specific options, you need to add logic here
+        # to delete them with those specific options. For example:
+        # cookies.delete(cookie_name.to_sym, domain: 'mydomain.com')
       end
     end
 end
