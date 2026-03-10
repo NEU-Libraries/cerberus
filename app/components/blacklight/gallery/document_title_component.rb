707dae306cf9a1a9800cc8609a9c9b647acc2a71
David Cliff <dgcliff@northeastern.edu>
2026-03-10 18:22:08 +0000

    Blacklight card title ux

---
+++ app/components/blacklight/gallery/document_title_component.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Blacklight
+  module Gallery
+    class DocumentTitleComponent < Blacklight::DocumentTitleComponent
+      def initialize(title = nil, as: :h5, **kwargs)
+        super
+      end
+    end
+  end
+end
