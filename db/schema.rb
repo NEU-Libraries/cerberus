56ade90d77b7c86479abee9dc1ced2e865a4c6d6
David Cliff <dgcliff@northeastern.edu>
2026-03-18 22:18:06 +0000

    Switching to solid q from good job as it's the rails 8 default. Going clean slate on background jobs, with intent to reimplement xml and IPTC loaders in another feature branch(es)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_13_184319) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_18_000002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -28,116 +28,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_13_184319) do
     t.index ["user_id"], name: "index_bookmarks_on_user_id"
   end
 
-  create_table "good_job_batches", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.integer "callback_priority"
-    t.text "callback_queue_name"
-    t.datetime "created_at", null: false
-    t.text "description"
-    t.datetime "discarded_at"
-    t.datetime "enqueued_at"
-    t.datetime "finished_at"
-    t.text "on_discard"
-    t.text "on_finish"
-    t.text "on_success"
-    t.jsonb "serialized_properties"
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "good_job_executions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "active_job_id", null: false
-    t.datetime "created_at", null: false
-    t.interval "duration"
-    t.text "error"
-    t.text "error_backtrace", array: true
-    t.integer "error_event", limit: 2
-    t.datetime "finished_at"
-    t.text "job_class"
-    t.uuid "process_id"
-    t.text "queue_name"
-    t.datetime "scheduled_at"
-    t.jsonb "serialized_params"
-    t.datetime "updated_at", null: false
-    t.index ["active_job_id", "created_at"], name: "index_good_job_executions_on_active_job_id_and_created_at"
-    t.index ["process_id", "created_at"], name: "index_good_job_executions_on_process_id_and_created_at"
-  end
-
-  create_table "good_job_processes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.integer "lock_type", limit: 2
-    t.jsonb "state"
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "good_job_settings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.text "key"
-    t.datetime "updated_at", null: false
-    t.jsonb "value"
-    t.index ["key"], name: "index_good_job_settings_on_key", unique: true
-  end
-
-  create_table "good_jobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "active_job_id"
-    t.uuid "batch_callback_id"
-    t.uuid "batch_id"
-    t.text "concurrency_key"
-    t.datetime "created_at", null: false
-    t.datetime "cron_at"
-    t.text "cron_key"
-    t.text "error"
-    t.integer "error_event", limit: 2
-    t.integer "executions_count"
-    t.datetime "finished_at"
-    t.boolean "is_discrete"
-    t.text "job_class"
-    t.text "labels", array: true
-    t.datetime "locked_at"
-    t.uuid "locked_by_id"
-    t.datetime "performed_at"
-    t.integer "priority"
-    t.text "queue_name"
-    t.uuid "retried_good_job_id"
-    t.datetime "scheduled_at"
-    t.jsonb "serialized_params"
-    t.datetime "updated_at", null: false
-    t.index ["active_job_id", "created_at"], name: "index_good_jobs_on_active_job_id_and_created_at"
-    t.index ["batch_callback_id"], name: "index_good_jobs_on_batch_callback_id", where: "(batch_callback_id IS NOT NULL)"
-    t.index ["batch_id"], name: "index_good_jobs_on_batch_id", where: "(batch_id IS NOT NULL)"
-    t.index ["concurrency_key"], name: "index_good_jobs_on_concurrency_key_when_unfinished", where: "(finished_at IS NULL)"
-    t.index ["cron_key", "created_at"], name: "index_good_jobs_on_cron_key_and_created_at_cond", where: "(cron_key IS NOT NULL)"
-    t.index ["cron_key", "cron_at"], name: "index_good_jobs_on_cron_key_and_cron_at_cond", unique: true, where: "(cron_key IS NOT NULL)"
-    t.index ["finished_at"], name: "index_good_jobs_jobs_on_finished_at", where: "((retried_good_job_id IS NULL) AND (finished_at IS NOT NULL))"
-    t.index ["labels"], name: "index_good_jobs_on_labels", where: "(labels IS NOT NULL)", using: :gin
-    t.index ["locked_by_id"], name: "index_good_jobs_on_locked_by_id", where: "(locked_by_id IS NOT NULL)"
-    t.index ["priority", "created_at"], name: "index_good_job_jobs_for_candidate_lookup", where: "(finished_at IS NULL)"
-    t.index ["priority", "created_at"], name: "index_good_jobs_jobs_on_priority_created_at_when_unfinished", order: { priority: "DESC NULLS LAST" }, where: "(finished_at IS NULL)"
-    t.index ["priority", "scheduled_at"], name: "index_good_jobs_on_priority_scheduled_at_unfinished_unlocked", where: "((finished_at IS NULL) AND (locked_by_id IS NULL))"
-    t.index ["queue_name", "scheduled_at"], name: "index_good_jobs_on_queue_name_and_scheduled_at", where: "(finished_at IS NULL)"
-    t.index ["scheduled_at"], name: "index_good_jobs_on_scheduled_at", where: "(finished_at IS NULL)"
-  end
-
   create_table "groups", force: :cascade do |t|
     t.string "cosmetic"
     t.datetime "created_at", null: false
     t.string "raw"
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "ingests", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.bigint "load_report_id", null: false
-    t.string "pid", null: false
-    t.integer "status", default: 0, null: false
-    t.datetime "updated_at", null: false
-    t.string "xml_filename", null: false
-    t.index ["load_report_id"], name: "index_ingests_on_load_report_id"
-  end
-
-  create_table "load_reports", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "finished_at"
-    t.datetime "started_at"
-    t.integer "status", default: 0, null: false
     t.datetime "updated_at", null: false
   end
 
@@ -149,6 +43,4 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_13_184319) do
     t.string "user_type"
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
-
-  add_foreign_key "ingests", "load_reports"
 end
