6d2cb3e61ea2dd58f17336ae2201f003d35eb094
David Cliff <dgcliff@northeastern.edu>
2026-04-02 22:55:06 +0000

    Skypack CDN fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,7 +12,7 @@ pin "@fortawesome/free-brands-svg-icons", to: "https://ga.jspm.io/npm:@fortaweso
 pin "@fortawesome/free-regular-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-regular-svg-icons@6.1.1/index.es.js"
 pin "@fortawesome/free-solid-svg-icons", to: "https://ga.jspm.io/npm:@fortawesome/free-solid-svg-icons@6.1.1/index.es.js"
 
-pin "@github/auto-complete-element", to: "https://cdn.skypack.dev/@github/auto-complete-element"
+pin "@github/auto-complete-element", to: "https://cdn.jsdelivr.net/npm/@github/auto-complete-element/+esm"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/umd/popper.min.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.js"
 pin "ace-builds", to: "https://ga.jspm.io/npm:ace-builds@1.16.0/src-noconflict/ace.js"
