47c5db0cac3933d0c92e4716ff4c465aa9e10314
David Cliff <dgcliff@northeastern.edu>
2026-04-14 20:15:39 +0000

    Move url construction into if check to avoid burdening faster 503

--- config/initializers/zoo_rack_attack.rb
+++ config/initializers/zoo_rack_attack.rb
@@ -519,15 +519,15 @@ end
 THROTTLE_HTML = ActionView::Base.new.render(file: 'public/429.html')
 
 Rack::Attack.throttled_response = lambda do |env|
-  u = "#{Rails.application.routes.url_helpers.root_url.chomp('/')}#{env["ORIGINAL_FULLPATH"]}"
-  uri = URI(u)
-  params = URI.decode_www_form(uri.query || "") << ["redirect", "js"]
-  uri.query = URI.encode_www_form(params)
-
-  view = ActionView::Base.new(ActionController::Base.view_paths, {})
-  view.assign(redirect_url: uri.to_s)
-
   if env['rack.attack.matched'] == "challenged"
+    u = "#{Rails.application.routes.url_helpers.root_url.chomp('/')}#{env["ORIGINAL_FULLPATH"]}"
+    uri = URI(u)
+    params = URI.decode_www_form(uri.query || "") << ["redirect", "js"]
+    uri.query = URI.encode_www_form(params)
+
+    view = ActionView::Base.new(ActionController::Base.view_paths, {})
+    view.assign(redirect_url: uri.to_s)
+
     [418, {'Content-Type' => 'text/html', 'Cache-Control' => 'no-cache, no-store, max-age=0, must-revalidate', 'Pragma' => 'no-cache'}, [view.render(file: 'public/challenge.html.erb')]]
   else
     [503, {'Set-Cookie' => "_cerberus_app_session=#{Date.today.to_time.to_i}", 'Set-Cookie' => 'cerberus_throttled=true', 'Content-Type' => 'text/html', 'Cache-Control' => 'no-cache, no-store, max-age=0, must-revalidate', 'Pragma' => 'no-cache'}, [THROTTLE_HTML]]
