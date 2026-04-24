c6583436f8555e3efd4ce57efb2d2f7975289aa5
David Cliff <dgcliff@northeastern.edu>
2026-04-24 22:33:30 +0000

    Minor dev environment cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,5 @@ bin/rails db:create
 bin/rails db:migrate
 rm -f /home/cerberus/web/tmp/pids/server.pid
 rails dartsass:watch &
-rails s -p 3000 -b '0.0.0.0'
+# rails s -p 3000 -b '0.0.0.0'
+bundle exec rdbg --open --host=0.0.0.0 --port=12345 --nonstop --command -- bin/rails server -b 0.0.0.0 -p 3000
