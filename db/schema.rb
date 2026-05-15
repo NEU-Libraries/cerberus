8b91492cc09508fd4f15070b5f65b3248f654e31
David Cliff <dgcliff@northeastern.edu>
2026-05-15 21:52:47 +0000

    Errant schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_19_000003) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_15_200000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_19_000003) do
     t.datetime "created_at", null: false
     t.string "raw"
     t.datetime "updated_at", null: false
+    t.index ["raw"], name: "index_groups_on_raw", unique: true
   end
 
   create_table "iptc_ingests", force: :cascade do |t|
