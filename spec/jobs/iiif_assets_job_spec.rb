4149dd655cd394bc7601d9329418e80d552ce845
David Cliff <dgcliff@northeastern.edu>
2026-05-28 22:48:45 +0000

    Fix race condition

--- spec/jobs/iiif_assets_job_spec.rb
+++ spec/jobs/iiif_assets_job_spec.rb
@@ -2,29 +2,29 @@
 
 require 'rails_helper'
 
-RSpec::Matchers.define_negated_matcher :not_have_enqueued_job, :have_enqueued_job
-
 RSpec.describe IiifAssetsJob, type: :job do
-  include ActiveJob::TestHelper
-
   let(:work_id) { 'w-123' }
   let(:tmp) { Dir.mktmpdir }
   let(:source_path) { File.join(tmp, 'image.png') }
   let(:base) { 'http://example.com/iiif/3/123456789.jp2' }
 
-  before { File.write(source_path, 'fake bytes') }
-  after  { FileUtils.rm_rf(tmp) }
+  before do
+    File.write(source_path, 'fake bytes')
+    allow(ThumbnailCreationJob).to receive(:perform_now)
+    allow(DerivativeCreationJob).to receive(:perform_now)
+  end
 
-  it 'writes the JP2 once and fans out to the thumbnail and derivative jobs' do
+  after { FileUtils.rm_rf(tmp) }
+
+  it 'writes the JP2 once and runs the thumbnail and derivative jobs serially' do
     allow(AtlasRb::Work).to receive(:find).with(work_id).and_return(AtlasRb::Mash.new(thumbnail: nil))
     allow(MasterJp2).to receive(:call).with(path: source_path).and_return(base)
 
-    expect do
-      described_class.new.perform(work_id, source_path)
-    end.to have_enqueued_job(ThumbnailCreationJob).with(work_id, base)
-       .and have_enqueued_job(DerivativeCreationJob).with(work_id, base, widths: nil)
+    described_class.new.perform(work_id, source_path)
 
     expect(MasterJp2).to have_received(:call).with(path: source_path).once
+    expect(ThumbnailCreationJob).to have_received(:perform_now).with(work_id, base).ordered
+    expect(DerivativeCreationJob).to have_received(:perform_now).with(work_id, base, widths: nil).ordered
   end
 
   it 'forwards derivative_widths through to DerivativeCreationJob' do
@@ -32,21 +32,20 @@ RSpec.describe IiifAssetsJob, type: :job do
     allow(AtlasRb::Work).to receive(:find).with(work_id).and_return(AtlasRb::Mash.new(thumbnail: nil))
     allow(MasterJp2).to receive(:call).with(path: source_path).and_return(base)
 
-    expect do
-      described_class.new.perform(work_id, source_path, derivative_widths: widths)
-    end.to have_enqueued_job(DerivativeCreationJob).with(work_id, base, widths: widths)
+    described_class.new.perform(work_id, source_path, derivative_widths: widths)
+
+    expect(DerivativeCreationJob).to have_received(:perform_now).with(work_id, base, widths: widths)
   end
 
   it 'noops when the work already has a thumbnail' do
     allow(AtlasRb::Work).to receive(:find).with(work_id).and_return(AtlasRb::Mash.new(thumbnail: 'already'))
     allow(MasterJp2).to receive(:call)
 
-    expect do
-      described_class.new.perform(work_id, source_path)
-    end.to not_have_enqueued_job(ThumbnailCreationJob)
-       .and not_have_enqueued_job(DerivativeCreationJob)
+    described_class.new.perform(work_id, source_path)
 
     expect(MasterJp2).not_to have_received(:call)
+    expect(ThumbnailCreationJob).not_to have_received(:perform_now)
+    expect(DerivativeCreationJob).not_to have_received(:perform_now)
   end
 
   it 'noops when the staged file is missing' do
@@ -54,11 +53,10 @@ RSpec.describe IiifAssetsJob, type: :job do
     allow(MasterJp2).to receive(:call)
     File.delete(source_path)
 
-    expect do
-      described_class.new.perform(work_id, source_path)
-    end.to not_have_enqueued_job(ThumbnailCreationJob)
-       .and not_have_enqueued_job(DerivativeCreationJob)
+    described_class.new.perform(work_id, source_path)
 
     expect(MasterJp2).not_to have_received(:call)
+    expect(ThumbnailCreationJob).not_to have_received(:perform_now)
+    expect(DerivativeCreationJob).not_to have_received(:perform_now)
   end
 end
