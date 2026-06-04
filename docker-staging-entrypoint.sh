161dd54a7eecc753c3a581ea633ff44e14bb1b45
David Cliff <dgcliff@northeastern.edu>
2026-06-04 15:50:04 -0400

    Get jobs running on staging

--- docker-staging-entrypoint.sh
+++ docker-staging-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bin/rails db:migrate RAILS_ENV=staging
 rm -f /home/cerberus/web/tmp/pids/server.pid
-rails assets:precompile
-rails s -p 3000 -b '0.0.0.0'
+RAILS_ENV=staging rails assets:precompile
+RAILS_ENV=staging bundle exec bin/jobs &
+RAILS_ENV=staging rails s -p 3000 -b '0.0.0.0'
