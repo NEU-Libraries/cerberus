938623c7b3cd8c8b27e3404c22861edbb6750f33
David Cliff <dgcliff@northeastern.edu>
2026-06-17 14:07:40 -0400

    Regenerate schema.rb with sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_10_000001) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_17_180550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -120,6 +120,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_10_000001) do
     t.integer "user_id"
     t.string "user_type"
     t.index ["user_id"], name: "index_searches_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "data"
+    t.string "session_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "xml_ingests", force: :cascade do |t|
