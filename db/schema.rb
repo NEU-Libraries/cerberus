7a0f300c63d04dc7a0e41a98ff09dea4faf9a316
David Cliff <dgcliff@northeastern.edu>
2026-06-27 17:09:04 -0400

    Regenerate schema for nullable loader root_collection

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_26_130001) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_27_000001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -107,7 +107,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_26_130001) do
     t.string "display_name", null: false
     t.string "group", null: false
     t.integer "kind", default: 0, null: false
-    t.string "root_collection", null: false
+    t.string "root_collection"
     t.string "slug", null: false
     t.datetime "updated_at", null: false
     t.index ["slug"], name: "index_loaders_on_slug", unique: true
